d5a4bda8ac49ded295216ceedc13db0415b2ca3a
Matteo Collina <hello@matteocollina.com>
2018-07-07 22:02:09 +0200

    Added basic timeout support

---
+++ test/timeout.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const { test } = require('tap')
+const { Client } = require('..')
+const { createServer } = require('http')
+
+test('timeout with pipelining 1', (t) => {
+  t.plan(9)
+
+  const server = createServer()
+
+  server.once('request', (req, res) => {
+    t.pass('first request received, we are letting this timeout on the client')
+
+    server.once('request', (req, res) => {
+      t.strictEqual('/', req.url)
+      t.strictEqual('GET', req.method)
+      res.setHeader('content-type', 'text/plain')
+      res.end('hello')
+    })
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, {
+      pipelining: 1,
+      timeout: 500
+    })
+    t.tearDown(client.close.bind(client))
+
+    client.request({ path: '/', method: 'GET' }, (err, data) => {
+      t.ok(err instanceof Error) // we are expecting an error
+      t.strictEqual(null, data)
+    })
+
+    client.request({ path: '/', method: 'GET' }, (err, { statusCode, headers, body }) => {
+      t.error(err)
+      t.strictEqual(statusCode, 200)
+      t.strictEqual(headers['content-type'], 'text/plain')
+      const bufs = []
+      body.on('data', (buf) => {
+        bufs.push(buf)
+      })
+      body.on('end', () => {
+        t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+      })
+    })
+  })
+})
