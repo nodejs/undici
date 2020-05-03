24d15a0407efb70e0882c70311c248b8186a088d
Robert Nagy <ronagy@icloud.com>
2020-05-03 12:12:14 +0200

    fix: error response on socket failure (#30)

--- test/client-errors.js
+++ test/client-errors.js
@@ -359,3 +359,29 @@ test('client destroy cleanup', (t) => {
     })
   })
 })
+
+test('GET errors body', (t) => {
+  t.plan(2)
+
+  const server = createServer()
+  server.once('request', (req, res) => {
+    res.write('asd')
+    setTimeout(() => {
+      res.destroy()
+    }, 19)
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.tearDown(client.close.bind(client))
+
+    client.request({ path: '/', method: 'GET' }, (err, { statusCode, headers, body }) => {
+      t.error(err)
+      body.resume()
+      body.on('error', err => (
+        t.ok(err)
+      ))
+    })
+  })
+})
