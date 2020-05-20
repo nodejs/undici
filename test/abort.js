76197463515f89396a4029c9d508d8a7a5431efa
delvedor <tommydelved@gmail.com>
2020-05-20 10:37:03 +0200

    test: Decrease timeout time and added request abort with request body

--- test/abort.js
+++ test/abort.js
@@ -4,6 +4,7 @@ const { test } = require('tap')
 const EventEmitter = require('events')
 const { Client, errors } = require('..')
 const { createServer } = require('http')
+const { createReadStream } = require('fs')
 
 test('Abort while sending request - event emitter (no body)', { skip: 'never ending' }, (t) => {
   t.plan(1)
@@ -33,7 +34,7 @@ test('Abort while waiting response - event emitter (no body)', (t) => {
     setTimeout(() => {
       res.setHeader('content-type', 'text/plain')
       res.end('hello world')
-    }, 1000)
+    }, 200)
   })
   t.teardown(server.close.bind(server))
 
@@ -48,7 +49,7 @@ test('Abort while waiting response - event emitter (no body)', (t) => {
 
     setTimeout(() => {
       ee.emit('abort')
-    }, 500)
+    }, 100)
   })
 })
 
@@ -59,7 +60,7 @@ test('Abort while waiting response - event emitter (write headers started) (no b
     res.writeHead(200, { 'content-type': 'text/plain' })
     setTimeout(() => {
       res.end('hello world')
-    }, 1000)
+    }, 200)
   })
   t.teardown(server.close.bind(server))
 
@@ -74,7 +75,7 @@ test('Abort while waiting response - event emitter (write headers started) (no b
 
     setTimeout(() => {
       ee.emit('abort')
-    }, 500)
+    }, 100)
   })
 })
 
@@ -86,7 +87,7 @@ test('Abort while waiting response - event emitter (write headers and write body
     res.write('hello')
     setTimeout(() => {
       res.end('world')
-    }, 1000)
+    }, 200)
   })
   t.teardown(server.close.bind(server))
 
@@ -104,6 +105,106 @@ test('Abort while waiting response - event emitter (write headers and write body
 
     setTimeout(() => {
       ee.emit('abort')
-    }, 500)
+    }, 100)
   })
 })
+
+function waitingWithBody (body, type) {
+  test(`Abort while waiting response - event emitter (with body ${type})`, (t) => {
+    t.plan(1)
+
+    const server = createServer((req, res) => {
+      setTimeout(() => {
+        res.setHeader('content-type', 'text/plain')
+        res.end('hello world')
+      }, 200)
+    })
+    t.teardown(server.close.bind(server))
+
+    server.listen(0, () => {
+      const client = new Client(`http://localhost:${server.address().port}`)
+      const ee = new EventEmitter()
+      t.teardown(client.close.bind(client))
+
+      client.request({ path: '/', method: 'POST', body, signal: ee }, (err, response) => {
+        t.ok(err instanceof errors.RequestAbortedError)
+      })
+
+      setTimeout(() => {
+        ee.emit('abort')
+      }, 100)
+    })
+  })
+}
+
+waitingWithBody('hello', 'string')
+waitingWithBody(createReadStream(__filename), 'stream')
+waitingWithBody(new Uint8Array([42]), 'Uint8Array')
+
+function writeHeadersStartedWithBody (body, type) {
+  test(`Abort while waiting response - event emitter (write headers started) (with body ${type})`, (t) => {
+    t.plan(1)
+
+    const server = createServer((req, res) => {
+      res.writeHead(200, { 'content-type': 'text/plain' })
+      setTimeout(() => {
+        res.end('hello world')
+      }, 200)
+    })
+    t.teardown(server.close.bind(server))
+
+    server.listen(0, () => {
+      const client = new Client(`http://localhost:${server.address().port}`)
+      const ee = new EventEmitter()
+      t.teardown(client.close.bind(client))
+
+      client.request({ path: '/', method: 'POST', body, signal: ee }, (err, response) => {
+        t.ok(err instanceof errors.RequestAbortedError)
+      })
+
+      setTimeout(() => {
+        ee.emit('abort')
+      }, 100)
+    })
+  })
+}
+
+writeHeadersStartedWithBody('hello', 'string')
+writeHeadersStartedWithBody(createReadStream(__filename), 'stream')
+writeHeadersStartedWithBody(new Uint8Array([42]), 'Uint8Array')
+
+function writeBodyStartedWithBody (body, type) {
+  test(`Abort while waiting response - event emitter (write headers and write body started) (with body ${type})`, (t) => {
+    t.plan(2)
+
+    const server = createServer((req, res) => {
+      res.writeHead(200, { 'content-type': 'text/plain' })
+      res.write('hello')
+      setTimeout(() => {
+        res.end('world')
+      }, 200)
+    })
+    t.teardown(server.close.bind(server))
+
+    server.listen(0, () => {
+      const client = new Client(`http://localhost:${server.address().port}`)
+      const ee = new EventEmitter()
+      t.teardown(client.close.bind(client))
+
+      client.request({ path: '/', method: 'POST', body, signal: ee }, (err, response) => {
+        t.error(err)
+        response.body.on('error', err => {
+          t.ok(err instanceof errors.RequestAbortedError)
+        })
+      })
+
+      setTimeout(() => {
+        ee.emit('abort')
+      }, 100)
+    })
+  })
+}
+
+writeBodyStartedWithBody('hello', 'string')
+writeBodyStartedWithBody(createReadStream(__filename), 'stream')
+writeBodyStartedWithBody(new Uint8Array([42]), 'Uint8Array')
