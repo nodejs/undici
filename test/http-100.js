6a1ec05e85eb4ce2e7c1a5144abea3aeb87b53b3
Robert Nagy <ronagy@icloud.com>
2020-05-27 19:03:39 +0200

    fix: error 101 (#204)

--- test/http-100.js
+++ test/http-100.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const { test } = require('tap')
-const { Client } = require('..')
+const { Client, errors } = require('..')
 const { createServer } = require('http')
+const net = require('net')
 
 test('ignore informational response', (t) => {
   t.plan(2)
@@ -31,6 +32,26 @@ test('ignore informational response', (t) => {
       response.body.on('end', () => {
         t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
       })
+    })
+  })
+})
+
+test('error 101', (t) => {
+  t.plan(1)
+
+  const server = net.createServer((socket) => {
+    socket.write('HTTP/1.1 101 Switching Protocols\r\n\r\n')
+  })
+  t.teardown(server.close.bind(server))
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
+
+    client.request({
+      path: '/',
+      method: 'GET'
+    }, (err) => {
+      t.ok(err instanceof errors.NotSupportedError)
     })
   })
 })
