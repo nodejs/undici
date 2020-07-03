798857cf3f286f79ddfbee51261d0f1f92afd3fa
Robert Nagy <ronagy@icloud.com>
2020-07-03 19:38:20 +0200

    feat: add support for unix socket

---
+++ test/unix.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const { test } = require('tap')
+const { Client } = require('..')
+const { createServer } = require('http')
+const pem = require('https-pem')
+
+test('http unix get', (t) => {
+  t.plan(7)
+
+  const server = createServer((req, res) => {
+    t.strictEqual('/', req.url)
+    t.strictEqual('GET', req.method)
+    t.strictEqual('localhost', req.headers.host)
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('hello')
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen('/var/tmp/test3.sock', () => {
+    const client = new Client({
+      socketPath: '/var/tmp/test3.sock',
+      hostname: 'localhost',
+      protocol: 'http:'
+    })
+    t.tearDown(client.close.bind(client))
+
+    client.request({ path: '/', method: 'GET' }, (err, data) => {
+      t.error(err)
+      const { statusCode, headers, body } = data
+      t.strictEqual(statusCode, 200)
+      t.strictEqual(headers['content-type'], 'text/plain')
+      const bufs = []
+      body.on('data', (buf) => {
+        bufs.push(buf)
+      })
+      body.on('end', () => {
+        t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+      })
+    })
+  })
+})
+
+test('https unix get', (t) => {
+  t.plan(7)
+
+  const server = createServer(pem, (req, res) => {
+    t.strictEqual('/', req.url)
+    t.strictEqual('GET', req.method)
+    t.strictEqual('localhost', req.headers.host)
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('hello')
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen('/var/tmp/test4.sock', () => {
+    const client = new Client({
+      socketPath: '/var/tmp/test4.sock',
+      hostname: 'localhost',
+      protocol: 'http:'
+    })
+    t.tearDown(client.close.bind(client))
+
+    client.request({ path: '/', method: 'GET' }, (err, data) => {
+      t.error(err)
+      const { statusCode, headers, body } = data
+      t.strictEqual(statusCode, 200)
+      t.strictEqual(headers['content-type'], 'text/plain')
+      const bufs = []
+      body.on('data', (buf) => {
+        bufs.push(buf)
+      })
+      body.on('end', () => {
+        t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+      })
+    })
+  })
+})
