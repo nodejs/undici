4759b79f9758cc4ceecea55fcaf7f507c6559b0c
Robert Nagy <ronagy@icloud.com>
2020-07-17 13:48:51 +0200

    fix: assertion crash

--- test/pipeline-pipelining.js
+++ test/pipeline-pipelining.js
@@ -40,3 +40,60 @@ test('pipeline pipelining', (t) => {
     })
   })
 })
+
+test('pipeline pipelining retry', (t) => {
+  t.plan(6)
+
+  let count = 0
+  const server = createServer((req, res) => {
+    if (count++ === 0) {
+      res.destroy()
+    } else {
+      res.end()
+    }
+  })
+
+  t.teardown(server.close.bind(server))
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, {
+      pipelining: 3
+    })
+    t.teardown(client.destroy.bind(client))
+
+    client.once('disconnect', () => {
+      t.pass()
+      client.on('disconnect', () => {
+        t.fail()
+      })
+    })
+
+    client[kConnect](() => {
+      client.pipeline({
+        method: 'GET',
+        path: '/'
+      }, ({ body }) => body)
+        .on('error', (err) => {
+          t.ok(err)
+        })
+        .end()
+        .resume()
+      t.strictDeepEqual(client.running, 1)
+
+      client.pipeline({
+        method: 'GET',
+        path: '/'
+      }, ({ body }) => body).end().resume()
+      t.strictDeepEqual(client.running, 2)
+
+      client.pipeline({
+        method: 'GET',
+        path: '/'
+      }, ({ body }) => body).end().resume()
+      t.strictDeepEqual(client.running, 3)
+
+      client.close(() => {
+        t.pass()
+      })
+    })
+  })
+})
