3cb4d1b074c86273a189c9396bd8c09700d19a37
Robert Nagy <ronagy@icloud.com>
2020-07-19 23:03:59 +0200

    fix: always reset connection after HEAD
    Fixes: https://github.com/mcollina/undici/issues/258

--- test/get-head-body.js
+++ test/get-head-body.js
@@ -4,6 +4,7 @@ const { test } = require('tap')
 const { Client } = require('..')
 const { createServer } = require('http')
 const { Readable } = require('stream')
+const { kConnect } = require('../lib/symbols')
 
 test('GET and HEAD with body should reset connection', (t) => {
   t.plan(4 + 2)
@@ -90,5 +91,53 @@ test('GET with body should work when target parses body as request', (t) => {
     t.error(err)
     t.strictEqual(data.statusCode, 200)
     data.body.resume()
+  })
+})
+
+test('HEAD should reset connection', (t) => {
+  t.plan(9)
+
+  const server = createServer((req, res) => {
+    res.end('asd')
+  })
+
+  t.teardown(server.close.bind(server))
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
+
+    client.on('disconnect', () => {
+      t.pass()
+    })
+
+    client.request({
+      path: '/',
+      method: 'HEAD'
+    }, (err, data) => {
+      t.error(err)
+      data.body.resume()
+    })
+    t.strictEqual(client.busy, true)
+
+    client.request({
+      path: '/',
+      method: 'HEAD'
+    }, (err, data) => {
+      t.error(err)
+      data.body.resume()
+      client.on('disconnect', () => {
+        client[kConnect](() => {
+          client.request({
+            path: '/',
+            method: 'HEAD'
+          }, (err, data) => {
+            t.error(err)
+            data.body.resume()
+          })
+          t.strictEqual(client.busy, true)
+        })
+      })
+    })
+    t.strictEqual(client.busy, true)
   })
 })
