8d88217abca41ec73e3bc18ebb46d7e248d1251e
Robert Nagy <ronagy@icloud.com>
2020-07-31 14:16:13 +0200

    test: fix for all platforms

--- test/parser-issues.js
+++ test/parser-issues.js
@@ -2,45 +2,40 @@ const net = require('net')
 const { test } = require('tap')
 const { Client } = require('..')
 
-const nodeMajorVersion = parseInt(process.version.split('.')[0].slice(1))
+test('https://github.com/mcollina/undici/issues/268', (t) => {
+  t.plan(2)
 
-// TODO: Different failure on other platforms...?
-if (process.platform === 'darwin' && nodeMajorVersion > 12) {
-  test('https://github.com/mcollina/undici/issues/268', (t) => {
-    t.plan(2)
-
-    const server = net.createServer(socket => {
-      socket.write('HTTP/1.1 200 OK\r\n')
-      socket.write('Transfer-Encoding: chunked\r\n\r\n')
+  const server = net.createServer(socket => {
+    socket.write('HTTP/1.1 200 OK\r\n')
+    socket.write('Transfer-Encoding: chunked\r\n\r\n')
+    setTimeout(() => {
+      socket.write('1\r\n')
+      socket.write('\n\r\n')
       setTimeout(() => {
         socket.write('1\r\n')
-        socket.write('\n')
-        setTimeout(() => {
-          socket.write('1\r\n')
-          socket.write('\n')
-        }, 500)
+        socket.write('\n\r\n')
       }, 500)
+    }, 500)
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, {
+      headersTimeout: 1e3
     })
-    t.tearDown(server.close.bind(server))
+    t.tearDown(client.destroy.bind(client))
 
-    server.listen(0, () => {
-      const client = new Client(`http://localhost:${server.address().port}`, {
-        headersTimeout: 1e3
-      })
-      t.tearDown(client.destroy.bind(client))
-
-      client.request({
-        method: 'GET',
-        path: '/nxt/_changes?feed=continuous&heartbeat=5000'
-      }, (err, data) => {
-        t.error(err)
-        data.body
-          .resume()
-        setTimeout(() => {
-          t.pass()
-          data.body.on('error', () => {})
-        }, 2e3)
-      })
+    client.request({
+      method: 'GET',
+      path: '/nxt/_changes?feed=continuous&heartbeat=5000'
+    }, (err, data) => {
+      t.error(err)
+      data.body
+        .resume()
+      setTimeout(() => {
+        t.pass()
+        data.body.on('error', () => {})
+      }, 2e3)
     })
   })
-}
+})
