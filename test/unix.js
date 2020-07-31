11f69e522c0225720421e71de7cf8bf1f501ba7c
Tim Suchanek <Tim.Suchanek@gmail.com>
2020-07-31 11:51:32 +0200

    fix: pass socketPath in pool (#282)
    Closes #281

--- test/unix.js
+++ test/unix.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { test } = require('tap')
-const { Client } = require('..')
+const { Client, Pool } = require('..')
 const http = require('http')
 // const https = require('http')
 // const pem = require('https-pem')
@@ -21,6 +21,43 @@ if (process.platform !== 'win32') {
 
     server.listen('/var/tmp/test3.sock', () => {
       const client = new Client({
+        hostname: 'localhost',
+        protocol: 'http:'
+      }, {
+        socketPath: '/var/tmp/test3.sock'
+      })
+      t.tearDown(client.close.bind(client))
+
+      client.request({ path: '/', method: 'GET' }, (err, data) => {
+        t.error(err)
+        const { statusCode, headers, body } = data
+        t.strictEqual(statusCode, 200)
+        t.strictEqual(headers['content-type'], 'text/plain')
+        const bufs = []
+        body.on('data', (buf) => {
+          bufs.push(buf)
+        })
+        body.on('end', () => {
+          t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+        })
+      })
+    })
+  })
+
+  test('http unix get pool', (t) => {
+    t.plan(7)
+
+    const server = http.createServer((req, res) => {
+      t.strictEqual('/', req.url)
+      t.strictEqual('GET', req.method)
+      t.strictEqual('localhost', req.headers.host)
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('hello')
+    })
+    t.tearDown(server.close.bind(server))
+
+    server.listen('/var/tmp/test3.sock', () => {
+      const client = new Pool({
         hostname: 'localhost',
         protocol: 'http:'
       }, {
