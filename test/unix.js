59152047b37abebabf47235551f024d72a4a9c43
Robert Nagy <ronagy@icloud.com>
2020-08-09 23:29:50 +0200

    test: unix https

--- test/unix.js
+++ test/unix.js
@@ -3,8 +3,8 @@
 const { test } = require('tap')
 const { Client, Pool } = require('..')
 const http = require('http')
-// const https = require('http')
-// const pem = require('https-pem')
+const https = require('https')
+const pem = require('https-pem')
 
 if (process.platform !== 'win32') {
   test('http unix get', (t) => {
@@ -81,42 +81,42 @@ if (process.platform !== 'win32') {
     })
   })
 
-  // test('https get with tls opts', (t) => {
-  //   t.plan(6)
+  test('https get with tls opts', (t) => {
+    t.plan(6)
 
-  //   const server = https.createServer(pem, (req, res) => {
-  //     t.strictEqual('/', req.url)
-  //     t.strictEqual('GET', req.method)
-  //     res.setHeader('content-type', 'text/plain')
-  //     res.end('hello')
-  //   })
-  //   t.tearDown(server.close.bind(server))
+    const server = https.createServer(pem, (req, res) => {
+      t.strictEqual('/', req.url)
+      t.strictEqual('GET', req.method)
+      res.setHeader('content-type', 'text/plain')
+      res.end('hello')
+    })
+    t.tearDown(server.close.bind(server))
 
-  //   server.listen('/var/tmp/test8.sock', () => {
-  //     const client = new Client({
-  //       hostname: 'localhost',
-  //       protocol: 'https:'
-  //     }, {
-  //       socketPath: '/var/tmp/test8.sock',
-  //       tls: {
-  //         rejectUnauthorized: false
-  //       }
-  //     })
-  //     t.tearDown(client.close.bind(client))
+    server.listen('/var/tmp/test8.sock', () => {
+      const client = new Client({
+        hostname: 'localhost',
+        protocol: 'https:'
+      }, {
+        socketPath: '/var/tmp/test8.sock',
+        tls: {
+          rejectUnauthorized: false
+        }
+      })
+      t.tearDown(client.close.bind(client))
 
-  //     client.request({ path: '/', method: 'GET' }, (err, data) => {
-  //       t.error(err)
-  //       const { statusCode, headers, body } = data
-  //       t.strictEqual(statusCode, 200)
-  //       t.strictEqual(headers['content-type'], 'text/plain')
-  //       const bufs = []
-  //       body.on('data', (buf) => {
-  //         bufs.push(buf)
-  //       })
-  //       body.on('end', () => {
-  //         t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
-  //       })
-  //     })
-  //   })
-  // })
+      client.request({ path: '/', method: 'GET' }, (err, data) => {
+        t.error(err)
+        const { statusCode, headers, body } = data
+        t.strictEqual(statusCode, 200)
+        t.strictEqual(headers['content-type'], 'text/plain')
+        const bufs = []
+        body.on('data', (buf) => {
+          bufs.push(buf)
+        })
+        body.on('end', () => {
+          t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+        })
+      })
+    })
+  })
 }
