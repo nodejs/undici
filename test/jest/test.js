511be846e3603acf255e5f5e042e44d37154d25e
Tim Suchanek <Tim.Suchanek@gmail.com>
2020-08-13 18:13:54 +0200

    fix: buffer check for jest (#319)

---
+++ test/jest/test.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const { Client } = require('../..')
+const { createServer } = require('http')
+/* global test, expect */
+
+test('should work in jest', async () => {
+  const server = createServer((req, res) => {
+    expect(req.url).toBe('/')
+    expect(req.method).toBe('POST')
+    expect(req.headers.host).toBe('localhost')
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('hello')
+  })
+  await new Promise((resolve, reject) => {
+    server.listen(0, () => {
+      const client = new Client(`http://localhost:${server.address().port}`)
+      client.request({
+        path: '/',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: '{}'
+      }, (err, result) => {
+        server.close()
+        client.close()
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  })
+})
