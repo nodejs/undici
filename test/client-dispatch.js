73863768d949e0ebdad8bf38b7b57bcf37d4b7e0
Robert Nagy <ronagy@icloud.com>
2020-08-17 10:47:33 +0200

    fixup: test

--- test/client-dispatch.js
+++ test/client-dispatch.js
@@ -2,6 +2,7 @@
 
 const { test } = require('tap')
 const { Client, errors } = require('..')
+const http = require('http')
 
 test('dispatch invalid opts', (t) => {
   t.plan(1)
@@ -17,4 +18,111 @@ test('dispatch invalid opts', (t) => {
   } catch (err) {
     t.ok(err instanceof errors.InvalidArgumentError)
   }
+})
+
+test('basic dispatch get', (t) => {
+  t.plan(11)
+
+  const server = http.createServer((req, res) => {
+    t.strictEqual('/', req.url)
+    t.strictEqual('GET', req.method)
+    t.strictEqual('localhost', req.headers.host)
+    t.strictEqual(undefined, req.headers.foo)
+    t.strictEqual('bar', req.headers.bar)
+    t.strictEqual(undefined, req.headers['content-length'])
+    res.end('hello')
+  })
+  t.tearDown(server.close.bind(server))
+
+  const reqHeaders = {
+    foo: undefined,
+    bar: 'bar'
+  }
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.tearDown(client.close.bind(client))
+
+    const bufs = []
+    client.dispatch({
+      path: '/',
+      method: 'GET',
+      headers: reqHeaders
+    }, {
+      onHeaders (statusCode, headers) {
+        t.strictEqual(headers.length, 6)
+        t.strictEqual(statusCode, 200)
+        t.strictEqual(Array.isArray(headers), true)
+      },
+      onData (buf) {
+        bufs.push(buf)
+      },
+      onComplete (trailers) {
+        t.strictEqual(Array.isArray(trailers), true)
+        t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+      },
+      onError () {
+        t.fail()
+      }
+    })
+  })
+})
+
+test('trailers dispatch get', (t) => {
+  t.plan(13)
+
+  const server = http.createServer((req, res) => {
+    t.strictEqual('/', req.url)
+    t.strictEqual('GET', req.method)
+    t.strictEqual('localhost', req.headers.host)
+    t.strictEqual(undefined, req.headers.foo)
+    t.strictEqual('bar', req.headers.bar)
+    t.strictEqual(undefined, req.headers['content-length'])
+    res.addTrailers({ 'Content-MD5': 'test' })
+    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Trailer', 'Content-MD5')
+    res.end('hello')
+  })
+  t.tearDown(server.close.bind(server))
+
+  const reqHeaders = {
+    foo: undefined,
+    bar: 'bar'
+  }
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.tearDown(client.close.bind(client))
+
+    const bufs = []
+    client.dispatch({
+      path: '/',
+      method: 'GET',
+      headers: reqHeaders
+    }, {
+      onHeaders (statusCode, headers) {
+        t.strictEqual(headers.length, 10)
+        t.strictEqual(statusCode, 200)
+        t.strictEqual(Array.isArray(headers), true)
+        {
+          const contentTypeIdx = headers.findIndex(x => x === 'Content-Type')
+          t.strictEqual(headers[contentTypeIdx + 1], 'text/plain')
+        }
+      },
+      onData (buf) {
+        bufs.push(buf)
+      },
+      onComplete (trailers) {
+        t.strictEqual(Array.isArray(trailers), true)
+        {
+          const contentMD5Idx = trailers.findIndex(x => x === 'Content-MD5')
+          t.strictEqual(trailers[contentMD5Idx + 1], 'test')
+        }
+        t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+      },
+      onError () {
+        t.fail()
+      }
+    })
+  })
 })
