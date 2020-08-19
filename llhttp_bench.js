da5bad2a85a1139044a35d83e713b7e12f9b74a9
Robert Nagy <ronagy@icloud.com>
2020-08-19 08:34:12 +0200

    fixup

--- llhttp_bench.js
+++ llhttp_bench.js
@@ -11,11 +11,56 @@ const pool = new undici.Client(`http://localhost:3009`, {
   requestTimeout: 0
 })
 
-for (let n = 0; n < 1e5; ++n) {
-  pool.request(undiciOptions, (err, { body }) => {
-    if (err) {
-      console.error(err)
-    }
-    body.resume()
-  })
-}
+const shoot = async (max = 1e5) => {
+  const promises = [];
+
+  for (let n = 0; n < 1e5; ++n) {
+    const p = new Promise((resolve) => {
+      pool.request(undiciOptions, (err, { body }) => {
+        if (err) {
+          console.error(err)
+          resolve();
+          return;
+        }
+
+        body.resume()
+        body.on('end', resolve);
+      })
+    });
+    promises.push(p);
+  }
+  await Promise.all(promises);
+};
+
+const bench = async () => {
+  let count = 0;
+  let sum = 0;
+  let squaresSum = 0;
+
+  const maxRequests = 1e5;
+
+  for (;;) {
+
+    const start = Date.now();
+    await shoot(count);
+    const end = Date.now();
+
+    const duration = end - start;
+    const rps = maxRequests * 1000 / duration;
+
+    count++;
+    sum += rps;
+    squaresSum += rps ** 2;
+
+    const mean = sum / count;
+    const stddev = Math.sqrt((squaresSum / count) - mean ** 2);
+
+    console.log(
+      `n=${count} mean=${mean.toFixed(3)} stddev=${stddev.toFixed(3)}`);
+  }
+};
+
+bench().catch((err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
