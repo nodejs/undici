f809afe92302625a4fe5bbc78690c18b49be9f60
Robert Nagy <ronagy@icloud.com>
2020-08-23 10:30:31 +0200

    fixup

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const Client = require('./lib/core/client')
 const errors = require('./lib/core/errors')
 const Pool = require('./lib/pool')
 
-Client.prototype.request = require('./lib/client-request').request
-Client.prototype.stream = require('./lib/client-stream').stream
-Client.prototype.pipeline = require('./lib/client-pipeline').pipeline
-Client.prototype.upgrade = require('./lib/client-upgrade').upgrade
-Client.prototype.connect = require('./lib/client-connect').connect
+Client.prototype.request = require('./lib/client-request')
+Client.prototype.stream = require('./lib/client-stream')
+Client.prototype.pipeline = require('./lib/client-pipeline')
+Client.prototype.upgrade = require('./lib/client-upgrade')
+Client.prototype.connect = require('./lib/client-connect')
+
+Pool.prototype.request = require('./lib/client-request')
+Pool.prototype.stream = require('./lib/client-stream')
+Pool.prototype.pipeline = require('./lib/client-pipeline')
+Pool.prototype.upgrade = require('./lib/client-upgrade')
+Pool.prototype.connect = require('./lib/client-connect')
 
 function undici (url, opts) {
   return new Pool(url, opts)
