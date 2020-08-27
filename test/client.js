9aec54e85c5f99e6a9fafb6b80e8a4dd448a81eb
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-08-27 15:13:13 -0400

    add support for generators

--- test/client.js
+++ test/client.js
@@ -383,6 +383,46 @@ test('basic POST with custom stream', (t) => {
   })
 })
 
+test('basic POST with iterator', (t) => {
+  t.plan(3)
+
+  const expected = 'hello'
+
+  const server = createServer((req, res) => {
+    req.resume().on('end', () => {
+      res.end(expected)
+    })
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.tearDown(client.close.bind(client))
+
+    client.request({
+      path: '/',
+      method: 'POST',
+      requestTimeout: 0,
+      body: function * () {
+        for (let i = 0; i < expected.length - 1; i++) {
+          yield expected[i]
+        }
+        return expected[expected.length - 1]
+      }
+    }, (err, { statusCode, body }) => {
+      t.error(err)
+      t.strictEqual(statusCode, 200)
+      const bufs = []
+      body.on('data', (buf) => {
+        bufs.push(buf)
+      })
+      body.on('end', () => {
+        t.strictEqual('hello', Buffer.concat(bufs).toString('utf8'))
+      })
+    })
+  })
+})
+
 test('basic POST with transfer encoding: chunked', (t) => {
   t.plan(8)
 
