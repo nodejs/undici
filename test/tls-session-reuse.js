41bcc83f4f3d943dcf8b7bb1b48dfc131d2d19b0
delvedor <tommydelved@gmail.com>
2020-08-28 12:31:28 +0200

    Updated test

--- test/tls-session-reuse.js
+++ test/tls-session-reuse.js
@@ -5,8 +5,9 @@ const { join } = require('path')
 const https = require('https')
 const crypto = require('crypto')
 const { test } = require('tap')
-const { Client } = require('..')
-const { kSocket, kTLSOpts } = require('../lib/core/symbols')
+const { Client, Pool } = require('..')
+const { kClients } = Pool[Symbol.for('pool-symbols')]
+const { kSocket, kTLSOpts, kTLSSession } = require('../lib/core/symbols')
 
 const nodeMajor = Number(process.versions.node.split('.')[0])
 
@@ -16,7 +17,37 @@ const options = {
 }
 const ca = readFileSync(join(__dirname, 'fixtures', 'ca.pem'), 'utf8')
 
-test('TLS should reuse sessions', { skip: nodeMajor < 11 }, t => {
+const requests = [{
+  name: 'first',
+  method: 'GET',
+  path: '/'
+}, {
+  name: 'first-reuse',
+  method: 'GET',
+  path: '/'
+}, {
+  name: 'cipher-change',
+  method: 'GET',
+  path: '/',
+  // Choose different cipher to use different cache entry
+  ciphers: 'AES256-SHA'
+}, {
+  // Change the ticket key to ensure session is updated in cache
+  name: 'before-drop',
+  method: 'GET',
+  path: '/drop-key'
+}, {
+  // Ticket will be updated starting from this
+  name: 'after-drop',
+  method: 'GET',
+  path: '/'
+}, {
+  name: 'after-drop-reuse',
+  method: 'GET',
+  path: '/'
+}]
+
+test('TLS should reuse sessions - Client', { skip: nodeMajor < 11 }, t => {
   const clientSessions = {}
   let serverRequests = 0
 
@@ -36,7 +67,6 @@ test('TLS should reuse sessions', { skip: nodeMajor < 11 }, t => {
         tls: {
           ca,
           rejectUnauthorized: false,
-          maxCachedSessions: 1,
           servername: 'agent1'
         }
       })
@@ -46,35 +76,7 @@ test('TLS should reuse sessions', { skip: nodeMajor < 11 }, t => {
         server.close()
       })
 
-      const queue = [{
-        name: 'first',
-        method: 'GET',
-        path: '/'
-      }, {
-        name: 'first-reuse',
-        method: 'GET',
-        path: '/'
-      }, {
-        name: 'cipher-change',
-        method: 'GET',
-        path: '/',
-        // Choose different cipher to use different cache entry
-        ciphers: 'AES256-SHA'
-      }, {
-        // Change the ticket key to ensure session is updated in cache
-        name: 'before-drop',
-        method: 'GET',
-        path: '/drop-key'
-      }, {
-        // Ticket will be updated starting from this
-        name: 'after-drop',
-        method: 'GET',
-        path: '/'
-      }, {
-        name: 'after-drop-reuse',
-        method: 'GET',
-        path: '/'
-      }]
+      const queue = requests.slice()
 
       function request () {
         const options = queue.shift()
@@ -96,6 +98,99 @@ test('TLS should reuse sessions', { skip: nodeMajor < 11 }, t => {
         })
       }
       request()
+    })
+  })
+
+  t.test('Verify cached sessions', t => {
+    t.plan(7)
+    t.strictEqual(serverRequests, 6)
+    t.strictEqual(
+      clientSessions.first.toString('hex'),
+      clientSessions['first-reuse'].toString('hex')
+    )
+    t.notStrictEqual(
+      clientSessions.first.toString('hex'),
+      clientSessions['cipher-change'].toString('hex')
+    )
+    t.notStrictEqual(
+      clientSessions.first.toString('hex'),
+      clientSessions['before-drop'].toString('hex')
+    )
+    t.notStrictEqual(
+      clientSessions['cipher-change'].toString('hex'),
+      clientSessions['before-drop'].toString('hex')
+    )
+    t.notStrictEqual(
+      clientSessions['before-drop'].toString('hex'),
+      clientSessions['after-drop'].toString('hex')
+    )
+    t.strictEqual(
+      clientSessions['after-drop'].toString('hex'),
+      clientSessions['after-drop-reuse'].toString('hex')
+    )
+  })
+
+  t.end()
+})
+
+test('TLS should reuse sessions - Pool', { skip: nodeMajor < 11 }, t => {
+  const clientSessions = {}
+  let serverRequests = 0
+
+  t.test('Prepare request', t => {
+    t.plan(7)
+    const server = https.createServer(options, (req, res) => {
+      if (req.url === '/drop-key') {
+        server.setTicketKeys(crypto.randomBytes(48))
+      }
+      serverRequests++
+      res.end()
+    })
+
+    server.listen(0, function () {
+      const client = new Pool(`https://localhost:${server.address().port}`, {
+        connections: 100,
+        keepAlive: false,
+        tls: {
+          ca,
+          rejectUnauthorized: false,
+          servername: 'agent1'
+        }
+      })
+
+      t.teardown(() => {
+        client.close()
+        server.close()
+      })
+
+      const queue = requests.slice()
+
+      function request (last) {
+        const clientIdx = 6 - queue.length
+        const options = queue.shift()
+        if (options.ciphers) {
+          // Choose different cipher to use different cache entry
+          client[kClients][clientIdx][kTLSOpts].ciphers = options.ciphers
+        } else {
+          delete client[kClients][clientIdx][kTLSOpts].ciphers
+        }
+        client.request(options, (err, data) => {
+          t.error(err)
+          // we are using a random int as index to verify that the session is updated in all clients
+          clientSessions[options.name] = client[kClients][Math.floor(Math.random() * 100)][kTLSSession]
+          if (queue.length !== 0) {
+            // start a new request before consuming the body,
+            // so the next request will be handled by a different client
+            request(clientIdx === 4)
+          }
+          data.body.resume().on('end', () => {
+            if (last) {
+              t.pass()
+            }
+          })
+        })
+      }
+      request(false)
     })
   })
 
