9e455fc19146c801c1c6703344d163561f2095e1
Robert Nagy <ronagy@icloud.com>
2020-08-30 18:54:33 +0200

    fix: destroy body on invalid callback
    Fixes: https://github.com/nodejs/undici/issues/382

--- test/client-request.js
+++ test/client-request.js
@@ -5,6 +5,7 @@ const { Client, errors } = require('..')
 const { createServer } = require('http')
 const EE = require('events')
 const { kConnect } = require('../lib/core/symbols')
+const { Readable } = require('stream')
 
 test('request abort before headers', (t) => {
   t.plan(2)
@@ -36,5 +37,31 @@ test('request abort before headers', (t) => {
         t.ok(err instanceof errors.RequestAbortedError)
       })
     })
+  })
+})
+
+test('request body destroyed on invalid callback', (t) => {
+  t.plan(1)
+
+  const server = createServer((req, res) => {
+  })
+  t.tearDown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.tearDown(client.destroy.bind(client))
+
+    const body = new Readable({
+      read () {}
+    })
+    try {
+      client.request({
+        path: '/',
+        method: 'GET',
+        body
+      }, null)
+    } catch (err) {
+      t.strictEqual(body.destroyed, true)
+    }
   })
 })
