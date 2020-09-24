c8fca32a7ced98a847a20bc4ed11c627749a96ee
Robert Nagy <ronagy@icloud.com>
2020-09-24 23:21:32 +0200

    fixup: test

--- test/socket-back-pressure.js
+++ test/socket-back-pressure.js
@@ -6,39 +6,48 @@ const { Readable } = require('stream')
 const { test } = require('tap')
 
 test('socket back-pressure', (t) => {
-  t.plan(2)
+  t.plan(3)
 
   const server = createServer()
+  let bytesWritten = 0
 
-  let body
+  const buf = Buffer.allocUnsafe(16384)
+  const src = new Readable({
+    read () {
+      bytesWritten += buf.length
+      this.push(buf)
+      if (bytesWritten >= 1e6) {
+        this.push(null)
+      }
+    }
+  })
 
   server.on('request', (req, res) => {
-    let bytesWritten = 0
-    const buf = Buffer.allocUnsafe(16384)
-    new Readable({
-      read () {
-        bytesWritten += buf.length
-        this.push(buf)
-        if (bytesWritten >= 1e6) {
-          this.push(null)
-        }
-      }
-    }).on('end', () => {
-      t.ok(body._readableState.length < body._readableState.highWaterMark)
-    }).pipe(res)
+    src.pipe(res)
   })
+  t.tearDown(server.close.bind(server))
 
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`, {
       pipelining: 1
     })
+    t.tearDown(client.destroy.bind(client))
 
     client.request({ path: '/', method: 'GET', opaque: 'asd' }, (err, data) => {
       t.error(err)
-      body = data.body
+      data.body
         .resume()
-        .on('data', () => {
+        .once('data', () => {
           data.body.pause()
+          // TODO: Try to avoid timeout.
+          setTimeout(() => {
+            t.ok(data.body._readableState.length < bytesWritten - data.body._readableState.highWaterMark)
+            src.push(null)
+            data.body.resume()
+          }, 1e3)
+        })
+        .on('end', () => {
+          t.pass()
         })
     })
   })
