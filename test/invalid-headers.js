186b10787acbb6da417551323be0609b079c28ce
Daniele Belardi <dwon.dnl@gmail.com>
2020-10-09 16:10:44 +0200

    fix(core/request): handle header set to null
    If a header is set to null it is casted to the string 'null'.

--- test/invalid-headers.js
+++ test/invalid-headers.js
@@ -4,7 +4,7 @@ const { test } = require('tap')
 const { Client, errors } = require('..')
 
 test('invalid headers', (t) => {
-  t.plan(11)
+  t.plan(10)
 
   const client = new Client('http://localhost:3000')
   t.teardown(client.destroy.bind(client))
@@ -71,16 +71,6 @@ test('invalid headers', (t) => {
     method: 'GET',
     headers: {
       foo: {}
-    }
-  }, (err, data) => {
-    t.ok(err instanceof errors.InvalidArgumentError)
-  })
-
-  client.request({
-    path: '/',
-    method: 'GET',
-    headers: {
-      asd: null
     }
   }, (err, data) => {
     t.ok(err instanceof errors.InvalidArgumentError)
