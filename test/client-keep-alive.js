d29d21cc2f46c03f967b470712ca50b977ac6f5c
Robert Nagy <ronagy@icloud.com>
2020-11-12 12:49:13 +0100

    fix: keep-alive postfix optional (#477)
    Fixes: https://github.com/nodejs/undici/issues/476

--- test/client-keep-alive.js
+++ test/client-keep-alive.js
@@ -74,6 +74,40 @@ test('keep-alive header 1', (t) => {
   })
 })
 
+test('keep-alive header no postfix', (t) => {
+  t.plan(2)
+
+  const server = createServer((socket) => {
+    socket.write('HTTP/1.1 200 OK\r\n')
+    socket.write('Content-Length: 0\r\n')
+    socket.write('Keep-Alive: timeout=2\r\n')
+    socket.write('Connection: keep-alive\r\n')
+    socket.write('\r\n\r\n')
+  })
+  t.teardown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
+
+    client.request({
+      path: '/',
+      method: 'GET'
+    }, (err, { body }) => {
+      t.error(err)
+      body.on('end', () => {
+        const timeout = setTimeout(() => {
+          t.fail()
+        }, 2e3)
+        client.on('disconnect', () => {
+          t.pass()
+          clearTimeout(timeout)
+        })
+      }).resume()
+    })
+  })
+})
+
 test('keep-alive not timeout', (t) => {
   t.plan(2)
 
