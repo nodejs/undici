bb5fa6f86e806a01bd8020489821ffc59b2bc530
Ethan Arrowood <ethan@arrowood.dev>
2021-01-05 16:26:47 +0000

    lint and some fixes

--- test/agent.js
+++ test/agent.js
@@ -1,36 +1,36 @@
 const tap = require('tap')
 const http = require('http')
-const { Agent, request } = require('../lib/agent')
+const { request } = require('../lib/agent')
 
 tap.test('request a resource using globalAgent', t => {
-    t.plan(6)
-    const wanted = 'payload'
+  t.plan(6)
+  const wanted = 'payload'
 
-    const server = http.createServer((req, res) => {
-        t.strictEqual('/', req.url)
-        t.strictEqual('GET', req.method)
-        t.strictEqual(`localhost:${server.address().port}`, req.headers.host)
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(wanted)
-    })
+  const server = http.createServer((req, res) => {
+    t.strictEqual('/', req.url)
+    t.strictEqual('GET', req.method)
+    t.strictEqual(`localhost:${server.address().port}`, req.headers.host)
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(wanted)
+  })
 
-    t.tearDown(server.close.bind(server))
+  t.tearDown(server.close.bind(server))
 
-    server.listen(0, () => {
-        request(`http://localhost:${server.address().port}`)
-            .then(({ statusCode, headers, body }) => {
-                t.strictEqual(statusCode, 200)
-                t.strictEqual(headers['content-type'], 'text/plain')
-                const bufs = []
-                body.on('data', (buf) => {
-                  bufs.push(buf)
-                })
-                body.on('end', () => {
-                  t.strictEqual(wanted, Buffer.concat(bufs).toString('utf8'))
-                })
-            })
-            .catch(err => {
-                t.fail(err)
-            })
-    })
+  server.listen(0, () => {
+    request(`http://localhost:${server.address().port}`)
+      .then(({ statusCode, headers, body }) => {
+        t.strictEqual(statusCode, 200)
+        t.strictEqual(headers['content-type'], 'text/plain')
+        const bufs = []
+        body.on('data', (buf) => {
+          bufs.push(buf)
+        })
+        body.on('end', () => {
+          t.strictEqual(wanted, Buffer.concat(bufs).toString('utf8'))
+        })
+      })
+      .catch(err => {
+        t.fail(err)
+      })
+  })
 })
