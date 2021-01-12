f8b05ec432bc2a1093812e3bb6d862bb407955a7
Robert Nagy <ronagy@icloud.com>
2021-01-12 18:53:57 +0100

    refactor: simplify agent checks

--- test/test-util.js
+++ test/test-util.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const tap = require('tap')
-const { getPath, checkForWeakRefAndFinalizationRegistrySupport } = require('../lib/core/util')
+const { getPath } = require('../lib/core/util')
 
 tap.test('getPath', t => {
   t.plan(4)
@@ -10,16 +10,4 @@ tap.test('getPath', t => {
   t.strictEqual(getPath({ pathname: 'undici' }), 'undici', 'returns pathname and defaults search to empty string')
   t.strictEqual(getPath({ pathname: 'undici', search: '?foo=bar' }), 'undici?foo=bar', 'concats pathname and search props')
   t.strictEqual(getPath({ search: '?foo=bar' }), '/?foo=bar', 'defaults pathname to / and appends search')
-})
-
-const SKIP = typeof WeakRef === 'undefined' || typeof FinalizationRegistry === 'undefined'
-
-tap.test('checkForWeakRefAndFinalizationRegistrySupport', t => {
-  t.plan(1)
-
-  if (SKIP) {
-    t.throw(() => checkForWeakRefAndFinalizationRegistrySupport())
-  } else {
-    t.notThrow(() => checkForWeakRefAndFinalizationRegistrySupport())
-  }
 })
