022acb42373073cb8a28e00ba5c6ec1cb85acb3f
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-02-01 14:06:13 -0500

    stash agent test changes

--- test/agent.js
+++ test/agent.js
@@ -43,64 +43,7 @@ tap.test('Agent', { skip: SKIP }, t => {
   })
 
   t.test('Agent close and destroy', t => {
-    t.plan(1)
-
-    // const wanted = 'payload'
-
-    // const createServer = () => http.createServer((req, res) => {
-    //   res.setHeader('Content-Type', 'text/plain')
-    //   res.end(wanted)
-    // })
-
-    // const promisifyServerListen = server => new Promise((resolve, reject) => {
-    //   server.listen(0, err => {
-    //     if (err) {
-    //       reject(err)
-    //     } else {
-    //       resolve()
-    //     }
-    //   })
-    // })
-
-    // const _request = async (t, agent, server) => {
-    //   try {
-    //     const { statusCode, headers, body } = await request(`http://localhost:${server.address().port}`, { agent })
-    //     t.strictEqual(statusCode, 200, 'status code should be 200')
-    //     t.strictEqual(headers['content-type'], 'text/plain', 'header should be text/plain')
-    //     const bufs = []
-    //     for await (const buf of body) {
-    //       bufs.push(buf)
-    //     }
-    //     t.strictEqual(wanted, Buffer.concat(bufs).toString('utf8'), 'wanted data should be stream by body')
-    //   } catch (err) {
-    //     t.fail(err)
-    //   }
-    // }
-
-    // t.test('closes all origins', {skip:true}, async t => {
-    //   t.plan(15)
-    //   const N = 3
-
-    //   const servers = Array.from(Array(N)).map(() => createServer())
-
-    //   const agent = new Agent()
-
-    //   await Promise.all(servers.map(server => promisifyServerListen(server)))
-    //   await Promise.all(servers.map(server => _request(t, agent, server)))
-    //   for (const server of servers) {
-    //     t.ok(server.listening, 'server should be listening')
-    //   }
-    //   for (const ref of agent[kAgentCache].values()) {
-    //     const pool = ref.deref()
-    //     pool.on('disconnect', err => {
-    //       console.log(err)
-    //       t.ok(err, 'pools should be closed')
-    //     })
-    //     // t.ok(pool.closed, 'pools should be closed')
-    //   }
-    //   const closedRes = await agent.close()
-    //   console.log(closedRes)
-    // })
+    t.plan(2)
 
     t.test('agent should close internal pools', t => {
       t.plan(2)
@@ -116,51 +59,92 @@ tap.test('Agent', { skip: SKIP }, t => {
 
       server.listen(0, () => {
         const agent = new Agent()
-        
-        const origin = `http://localhost:${server.address().port}`
 
+        const origin = `http://localhost:${server.address().port}`
+        
         request(origin, { agent })
           .then(() => {
-            t.fail()
+            t.pass()
           })
           .catch(err => {
             t.error(err instanceof errors.ClientClosedError)
           })
 
-        const pool = agent.get(origin)
-        pool.once('connect', () => {
-          let closed = false
-          pool.on('disconnect', () => {
-            console.log('DISCONNECT')
-            closed = true
-          })
-          agent.close().then(() => {
-            console.log('CLOSE CB')
-            t.ok(closed, 'pool should be disconnected')
-          })
+        agent.close().then(() => {
+          request(origin, { agent })
+            .then(() => {
+              t.fail()
+            })
+            .catch(err => {
+              t.error(err instanceof errors.ClientClosedError)
+            })
         })
+
+        // i'd expect this to work but it isn't - possibly my own misunderstanding; I want to keep it in to further investigate in the future - @ethan-arrowood
+        // const pool = agent.get(origin)
+        // pool.once('connect', () => {
+        //   let closed = false
+        //   pool.on('disconnect', () => {
+        //     console.log('DISCONNECT')
+        //     closed = true
+        //   })
+        //   agent.close().then(() => {
+        //     console.log('CLOSE CB')
+        //     t.ok(closed, 'pool should be disconnected')
+        //   })
+        // })
       })
     })
+    t.test('agent should destroy internal pools', t => {
+      t.plan(2)
 
-    // t.test('destroys all origins', {skip: true}, async t => {
-    //   t.plan(15)
-    //   const N = 3
+      const wanted = 'payload'
 
-    //   const servers = Array.from(Array(N)).map(() => createServer())
+      const server = http.createServer((req, res) => {
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(wanted)
+      })
 
-    //   const agent = new Agent()
+      t.tearDown(server.close.bind(server))
 
-    //   await Promise.all(servers.map(server => promisifyServerListen(server)))
-    //   await Promise.all(servers.map(server => _request(t, agent, server)))
-    //   for (const server of servers) {
-    //     t.ok(server.listening, 'server should be listening')
-    //   }
-    //   await agent.destroy()
-    //   for (const ref of agent[kAgentCache].values()) {
-    //     const pool = ref.deref()
-    //     t.ok(pool.destroyed, 'pools should be destroyed')
-    //   }
-    // })
+      server.listen(0, () => {
+        const agent = new Agent()
+
+        const origin = `http://localhost:${server.address().port}`
+        
+        request(origin, { agent })
+          .then(() => {
+            t.pass()
+          })
+          .catch(err => {
+            t.error(err instanceof errors.ClientDestroyError)
+          })
+
+        agent.destroy().then(() => {
+          request(origin, { agent })
+            .then(() => {
+              t.fail()
+            })
+            .catch(err => {
+              t.error(err instanceof errors.ClientDestroyError)
+            })
+        })
+
+        // i'd expect this to work but it isn't - possibly my own misunderstanding; I want to keep it in to further investigate in the future - @ethan-arrowood
+        // const pool = agent.get(origin)
+        // pool.once('connect', () => {
+        //   let closed = false
+        //   pool.on('disconnect', () => {
+        //     console.log('DISCONNECT')
+        //     closed = true
+        //   })
+        //   agent.close().then(() => {
+        //     console.log('CLOSE CB')
+        //     t.ok(closed, 'pool should be disconnected')
+        //   })
+        // })
+      })
+    })
   })
 
   t.test('request a resource', t => {
