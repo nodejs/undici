52d485245e4f459f1ad16cc609c006750c63245f
Robert Nagy <ronagy@icloud.com>
2021-04-03 15:11:49 +0200

    fixup

--- test/http-100.js
+++ test/http-100.js
@@ -34,52 +34,52 @@ test('ignore informational response', (t) => {
   })
 })
 
-// test('error 103 body', (t) => {
-//   t.plan(2)
+test('error 103 body', (t) => {
+  t.plan(2)
 
-//   const server = net.createServer((socket) => {
-//     socket.write('HTTP/1.1 103 Early Hints\r\n')
-//     socket.write('Content-Length: 1\r\n')
-//     socket.write('\r\n')
-//     socket.write('a\r\n')
-//   })
-//   t.teardown(server.close.bind(server))
-//   server.listen(0, () => {
-//     const client = new Client(`http://localhost:${server.address().port}`)
-//     t.teardown(client.destroy.bind(client))
+  const server = net.createServer((socket) => {
+    socket.write('HTTP/1.1 103 Early Hints\r\n')
+    socket.write('Content-Length: 1\r\n')
+    socket.write('\r\n')
+    socket.write('a\r\n')
+  })
+  t.teardown(server.close.bind(server))
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
 
-//     client.request({
-//       path: '/',
-//       method: 'GET'
-//     }, (err) => {
-//       t.equal(err.code, 'HPE_INVALID_CONSTANT')
-//     })
-//     client.on('disconnect', () => {
-//       t.pass()
-//     })
-//   })
-// })
+    client.request({
+      path: '/',
+      method: 'GET'
+    }, (err) => {
+      t.equal(err.code, 'HPE_INVALID_CONSTANT')
+    })
+    client.on('disconnect', () => {
+      t.pass()
+    })
+  })
+})
 
-// test('error 100 body', (t) => {
-//   t.plan(2)
+test('error 100 body', (t) => {
+  t.plan(2)
 
-//   const server = net.createServer((socket) => {
-//     socket.write('HTTP/1.1 100 Early Hints\r\n')
-//     socket.write('\r\n')
-//   })
-//   t.teardown(server.close.bind(server))
-//   server.listen(0, () => {
-//     const client = new Client(`http://localhost:${server.address().port}`)
-//     t.teardown(client.destroy.bind(client))
+  const server = net.createServer((socket) => {
+    socket.write('HTTP/1.1 100 Early Hints\r\n')
+    socket.write('\r\n')
+  })
+  t.teardown(server.close.bind(server))
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
 
-//     client.request({
-//       path: '/',
-//       method: 'GET'
-//     }, (err) => {
-//       t.equal(err.message, 'bad response')
-//     })
-//     client.on('disconnect', () => {
-//       t.pass()
-//     })
-//   })
-// })
+    client.request({
+      path: '/',
+      method: 'GET'
+    }, (err) => {
+      t.equal(err.message, 'bad response')
+    })
+    client.on('disconnect', () => {
+      t.pass()
+    })
+  })
+})
