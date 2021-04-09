b57594211515169bbd9d44d84075819b3852b9a5
Robert Nagy <ronagy@icloud.com>
2021-04-09 11:27:42 +0200

    fix: res end before req body (#709)
    * fix: res end before req body
    
    * test: make the check on parser buffer size more  permissive
    
    Co-authored-by: Daniele Belardi <dwon.dnl@gmail.com>

--- test/client.js
+++ test/client.js
@@ -1064,7 +1064,7 @@ test('emit disconnect after destory', t => {
   })
 })
 
-test('parser dinamic allocation', t => {
+test('parser dynamic allocation', t => {
   t.plan(5)
   const chunksSent = []
   const server = createServer((req, res) => {
@@ -1094,13 +1094,47 @@ test('parser dinamic allocation', t => {
       body.on('data', chunk => {
         counter++
         if (counter === 3) {
-          t.equal(client[kSocket][kParser].bufferSize, 16384)
+          t.ok(client[kSocket][kParser].bufferSize > 8192)
         }
         chunksReceived.push(chunk)
       })
       body.on('end', () => {
         t.same(Buffer.concat(chunksReceived), Buffer.concat(chunksSent))
       })
+    })
+  })
+})
+
+test('end response before request', t => {
+  t.plan(2)
+
+  const server = createServer((req, res) => {
+    res.end()
+  })
+  t.teardown(server.close.bind(server))
+
+  server.listen(0, async () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    const readable = new Readable({
+      read () {
+        this.push('asd')
+      }
+    })
+    const { body } = await client.request({
+      method: 'GET',
+      path: '/',
+      body: readable
+    })
+    body
+      .on('error', () => {
+        t.fail()
+      })
+      .on('end', () => {
+        t.pass()
+      })
+      .resume()
+    client.on('disconnect', (url, targets, err) => {
+      t.equal(err.code, 'UND_ERR_INFO')
     })
   })
 })
