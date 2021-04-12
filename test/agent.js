ac124b494a7907b589c5673e04aa01979a15aaad
Robert Nagy <ronagy@icloud.com>
2021-04-13 00:04:01 +0200

    fix: Don't check origin for Pool & Client
    Parsed origin will remove :80 and :443 which will make
    the check fail. Resolving this will incur performance loss
    so let's just remove it.
    
    Fixes: https://github.com/nodejs/undici/issues/727

--- test/agent.js
+++ test/agent.js
@@ -595,3 +595,30 @@ test('drain', t => {
     }, new Handler()), false)
   })
 })
+
+test('agent works with port 80', t => {
+  t.plan(1)
+
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end()
+  })
+
+  t.teardown(server.close.bind(server))
+
+  server.listen(80, async () => {
+    const dispatcher = new Agent()
+
+    const origin = `http://localhost:${server.address().port}`
+
+    try {
+      const { body } = await dispatcher.request({ origin, method: 'GET', path: '/' })
+
+      body.on('end', () => {
+        t.pass()
+      }).resume()
+    } catch (err) {
+      t.error(err)
+    }
+  })
+})
