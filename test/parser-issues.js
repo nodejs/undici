097bb53a3f0c7e6ce9f48f7c73fee1b2a433a021
Robert Nagy <ronagy@icloud.com>
2021-04-13 12:12:49 +0200

    fix: parsing of headers split between multiple data buffers (#738)
    * fix: parsing of headers split between multiple data buffers
    
    Fixes: https://github.com/nodejs/undici/issues/737
    
    * fixup

--- test/parser-issues.js
+++ test/parser-issues.js
@@ -61,3 +61,55 @@ test('parser fail', (t) => {
     })
   })
 })
+
+test('split header field', (t) => {
+  t.plan(2)
+
+  const server = net.createServer(socket => {
+    socket.write('HTTP/1.1 200 OK\r\nA')
+    setTimeout(() => {
+      socket.write('SD: asd,asd\r\n\r\n\r\n')
+    }, 100)
+  })
+  t.teardown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
+
+    client.request({
+      method: 'GET',
+      path: '/'
+    }, (err, data) => {
+      t.error(err)
+      t.equal(data.headers.asd, 'asd,asd')
+      data.body.destroy().on('error', () => {})
+    })
+  })
+})
+
+test('split header value', (t) => {
+  t.plan(2)
+
+  const server = net.createServer(socket => {
+    socket.write('HTTP/1.1 200 OK\r\nASD: asd')
+    setTimeout(() => {
+      socket.write(',asd\r\n\r\n\r\n')
+    }, 100)
+  })
+  t.teardown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    t.teardown(client.destroy.bind(client))
+
+    client.request({
+      method: 'GET',
+      path: '/'
+    }, (err, data) => {
+      t.error(err)
+      t.equal(data.headers.asd, 'asd,asd')
+      data.body.destroy().on('error', () => {})
+    })
+  })
+})
