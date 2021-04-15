9e6caa99c097f34f3fdab588639f8e0314b3fc43
Robert Nagy <ronagy@icloud.com>
2021-04-15 18:05:23 +0200

    fix: remove unused wasm callbacks

--- deps/llhttp/src/api.c
+++ deps/llhttp/src/api.c
@@ -26,9 +26,6 @@ void llhttp_init(llhttp_t* parser, llhttp_type_t type,
 
 #if defined(__wasm__)
 
-extern int wasm_on_message_begin(llhttp_t * p);
-extern int wasm_on_url(llhttp_t* p, const char* at, size_t length);
-extern int wasm_on_status(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_header_field(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_header_value(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_headers_complete(llhttp_t * p);
@@ -36,9 +33,9 @@ extern int wasm_on_body(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_message_complete(llhttp_t * p);
 
 const llhttp_settings_t wasm_settings = {
-  wasm_on_message_begin,
-  wasm_on_url,
-  wasm_on_status,
+  NULL,
+  NULL,
+  NULL,
   wasm_on_header_field,
   wasm_on_header_value,
   wasm_on_headers_complete,
