c29dcf7d9434eba53d46f0fa2d137a9c41b7defa
Robert Nagy <ronagy@icloud.com>
2021-04-20 21:23:05 +0200

    fixup

--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -9,7 +9,7 @@ const path = require('path')
 const { Pool, Client } = require('..')
 
 const iterations = parseInt(process.env.SAMPLES, 10) || 1000
-const errorThreshold = parseInt(process.env.ERROR_TRESHOLD, 10) || 2
+const errorThreshold = parseInt(process.env.ERROR_TRESHOLD, 10) || 3
 const connections = parseInt(process.env.CONNECTIONS, 10) || 50
 const pipelining = parseInt(process.env.PIPELINING, 10) || 10
 const parallelRequests = parseInt(process.env.PARALLEL, 10) || (connections * pipelining)
