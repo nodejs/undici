dc4d38ae77ee2cdd9e3d4fe8990817e34671790a
Robert Nagy <ronagy@icloud.com>
2021-04-25 16:24:13 +0200

    fixup

--- deps/llhttp/src/api.c
+++ deps/llhttp/src/api.c
@@ -44,11 +44,10 @@ extern int wasm_on_header_field(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_header_value(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_headers_complete(llhttp_t * p, int status_code, uint8_t upgrade, int should_keep_alive);
 extern int wasm_on_body(llhttp_t* p, const char* at, size_t length);
-extern int wasm_on_headers_complete(llhttp_t * p);
 
 int on_headers_complete(llhttp_t * p)
 {
-  return wasm_on_headers_complete(p, p>status_code, p->upgrade, llhttp_should_keep_alive(p));
+  return wasm_on_headers_complete(p, p->status_code, p->upgrade, llhttp_should_keep_alive(p));
 }
 
 const llhttp_settings_t wasm_settings = {
@@ -59,7 +58,7 @@ const llhttp_settings_t wasm_settings = {
   wasm_on_header_value,
   on_headers_complete,
   wasm_on_body,
-  wasm_on_message_complete,
+  on_headers_complete,
   NULL,
   NULL,
 };
