da87e49c1a44b6b626a3343f181a1a7cfb165116
Nitzan Uziely <linkgoron@gmail.com>
2021-06-25 21:07:33 +0300

    add timeout, tls, get-head-body tests

--- test/tls.js
+++ test/tls.js
@@ -143,3 +143,46 @@ test('tls get 4', (t) => {
       })
   })
 })
+
+test('tls get 5', (t) => {
+  t.plan(8)
+
+  const client = new Client('https://140.82.112.4')
+  t.teardown(client.destroy.bind(client))
+
+  let didDisconnect = false
+  client.request({
+    method: 'GET',
+    path: '/',
+    headers: {
+      host: 'www.github.com'
+    }
+  }, (err, data) => {
+    t.error(err)
+    t.equal(data.statusCode, 301)
+    t.equal(client[kSocket].authorized, true)
+
+    data.body
+      .resume()
+      .on('end', () => {
+        t.pass()
+      })
+    client.once('disconnect', () => {
+      t.pass()
+      didDisconnect = true
+    })
+  })
+
+  client.request({
+    method: 'POST',
+    path: '/',
+    body: [],
+    headers: {
+      host: 'www.asd.com'
+    }
+  }, (err, data) => {
+    t.equal(didDisconnect, true)
+    t.equal(client[kSocket].authorized, false)
+    t.ok(err)
+  })
+})
