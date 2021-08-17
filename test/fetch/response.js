5f39773583d53a9ed8b995cc56ddda177a1b69ba
Ethan Arrowood <ethan.arrowood@gmail.com>
2021-08-17 16:01:17 -0600

    Initialize some fetch tests (#978)
    * copy header tests from undici-fetch and stub other exports
    
    * only run on v16

---
+++ test/fetch/response.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const tap = require('tap')
+const {
+  makeNetworkError,
+  makeResponse,
+  filterResponse,
+  Response
+} = require('../../lib/fetch/response')
+
+tap.todo('makeNetworkError')
+tap.todo('makeResponse')
+tap.todo('filterResponse')
+tap.todo('Response')
