b7240408e27aaacd3787c5c6fa0f03e7c1a46c54
Robert Nagy <ronagy@icloud.com>
2021-10-13 10:42:40 +0200

    fix: update llhttp (#1060)
    * fix: update llhttp
    
    * Fixed failing test
    
    * Apply suggestions from code review
    
    Co-authored-by: Matteo Collina <hello@matteocollina.com>

--- deps/llhttp/src/api.c
+++ deps/llhttp/src/api.c
@@ -46,7 +46,8 @@ extern int wasm_on_url(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_status(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_header_field(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_header_value(llhttp_t* p, const char* at, size_t length);
-extern int wasm_on_headers_complete(llhttp_t * p, int status_code, uint8_t upgrade, int should_keep_alive);
+extern int wasm_on_headers_complete(llhttp_t * p, int status_code,
+                                    uint8_t upgrade, int should_keep_alive);
 extern int wasm_on_body(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_message_complete(llhttp_t * p);
 
