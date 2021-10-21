22813d4f1ce8d0c0fec3a22bdf83b0557391ecd9
RafaelGSS <rafael.nunu@hotmail.com>
2021-10-21 15:47:52 -0300

    types(proxy-agent): add test to proxy-agent types

--- types/proxy-agent.d.ts
+++ types/proxy-agent.d.ts
@@ -1,22 +1,17 @@
 import Agent = require('./agent')
 import Dispatcher = require('./dispatcher')
-import { Interceptable } from './mock-interceptor'
 
 export = ProxyAgent
 
 declare class ProxyAgent extends Dispatcher {
-  constructor(options?: ProxyAgent.Options)
+  constructor(options: ProxyAgent.Options | string)
 
-  get<TInterceptable extends Interceptable>(origin: string): TInterceptable;
-  get<TInterceptable extends Interceptable>(origin: RegExp): TInterceptable;
-  get<TInterceptable extends Interceptable>(origin: ((origin: string) => boolean)): TInterceptable;
-  /** Dispatches a mocked request. */
   dispatch(options: Agent.DispatchOptions, handler: Dispatcher.DispatchHandlers): void;
-  /** Closes the mock agent and waits for registered mock pools and clients to also close before resolving. */
   close(): Promise<void>;
 }
 
 declare namespace ProxyAgent {
   export interface Options extends Agent.Options {
+    uri: string;
   }
 }
