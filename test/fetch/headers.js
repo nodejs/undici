39aca4a7c29ee0d59ee04da3b50f2ff9b03dd525
Luke Edwards <luke.edwards05@gmail.com>
2021-10-31 21:56:41 -0700

    fix(headers): iterate over cloned list (#1081)
    * chore(headers): add failing test
    
    * fix(headers): iterate over list clone
    
    * chore(headers): add test for infinite iteration

--- test/fetch/headers.js
+++ test/fetch/headers.js
@@ -280,7 +280,47 @@ tap.test('Headers set', t => {
 })
 
 tap.test('Headers as Iterable', t => {
-  t.plan(6)
+  t.plan(8)
+
+  t.test('should freeze values while iterating', t => {
+    t.plan(1)
+    const init = [
+      ['foo', '123'],
+      ['bar', '456']
+    ]
+    const expected = [
+      ['x-bar', '456'],
+      ['x-foo', '123']
+    ]
+    const headers = new Headers(init)
+    for (const [key, val] of headers) {
+      headers.delete(key)
+      headers.set(`x-${key}`, val)
+    }
+    t.strictSame([...headers], expected)
+  })
+
+  t.test('prevent infinite, continuous iteration', t => {
+    t.plan(2)
+
+    const headers = new Headers({
+      z: 1,
+      y: 2,
+      x: 3
+    })
+
+    const order = []
+    for (const [key] of headers) {
+      order.push(key)
+      headers.set(key + key, 1)
+    }
+
+    t.strictSame(order, ['x', 'y', 'z'])
+    t.strictSame(
+      [...headers.keys()],
+      ['x', 'xx', 'y', 'yy', 'z', 'zz']
+    )
+  })
 
   t.test('returns combined and sorted entries using .forEach()', t => {
     t.plan(12)
