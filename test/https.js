062d159c469c440f94f04518e8f27bbaf2bf4276
Robert Nagy <ronagy@icloud.com>
2021-11-03 19:53:29 +0200

    fix: support no servername (#1082)
    * fix: support no servername
    
    * fixup: test

--- test/https.js
+++ test/https.js
@@ -38,3 +38,37 @@ test('https get with tls opts', (t) => {
     })
   })
 })
+
+test('https get with tls opts ip', (t) => {
+  t.plan(6)
+
+  const server = createServer(pem, (req, res) => {
+    t.equal('/', req.url)
+    t.equal('GET', req.method)
+    res.setHeader('content-type', 'text/plain')
+    res.end('hello')
+  })
+  t.teardown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`https://127.0.0.1:${server.address().port}`, {
+      tls: {
+        rejectUnauthorized: false
+      }
+    })
+    t.teardown(client.close.bind(client))
+
+    client.request({ path: '/', method: 'GET' }, (err, { statusCode, headers, body }) => {
+      t.error(err)
+      t.equal(statusCode, 200)
+      t.equal(headers['content-type'], 'text/plain')
+      const bufs = []
+      body.on('data', (buf) => {
+        bufs.push(buf)
+      })
+      body.on('end', () => {
+        t.equal('hello', Buffer.concat(bufs).toString('utf8'))
+      })
+    })
+  })
+})
