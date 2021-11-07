98416bfd0ad8973f175a30f4317e86d0d208c827
Rune <rune@runefh.dev>
2021-11-07 17:51:02 +0100

    feat: Allow buffers as mock response data (#1086)
    * feat: Allow buffers as mock response data
    
    * Update docs

--- types/mock-interceptor.d.ts
+++ types/mock-interceptor.d.ts
@@ -21,7 +21,7 @@ declare class MockScope<TData extends object = object> {
 declare class MockInterceptor {
   constructor(options: MockInterceptor.Options, mockDispatches: MockInterceptor.MockDispatch[]);
   /** Mock an undici request with the defined reply. */
-  reply<TData extends object = object>(statusCode: number, data: TData | string, responseOptions?: MockInterceptor.MockResponseOptions): MockScope<TData>;
+  reply<TData extends object = object>(statusCode: number, data: TData | Buffer| string , responseOptions?: MockInterceptor.MockResponseOptions): MockScope<TData>;
   /** Mock an undici request by throwing the defined reply error. */
   replyWithError<TError extends Error = Error>(error: TError): MockScope;
   /** Set default reply headers on the interceptor for subsequent mocked replies. */
