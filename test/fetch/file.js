caf9ec3385c7e407748ac442c9972065f63e1985
Robert Nagy <ronagy@icloud.com>
2021-11-27 18:40:17 +0100

    fix: some cleanup

--- test/fetch/file.js
+++ test/fetch/file.js
@@ -5,6 +5,6 @@ const { File, FileLike } = require('../../lib/fetch/file')
 
 test('Symbol.toStringTag', (t) => {
   t.plan(2)
-  t.equal(File.prototype[Symbol.toStringTag], 'File')
-  t.equal(FileLike.prototype[Symbol.toStringTag], 'File')
+  t.equal(new File()[Symbol.toStringTag], 'File')
+  t.equal(new FileLike()[Symbol.toStringTag], 'File')
 })
