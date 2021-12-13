52cf2661bdcc8c2d13b75262470620ae11a74ef2
Robert Nagy <ronagy@icloud.com>
2021-12-13 15:43:46 +0100

    fix: don't cancel with invalid state (#1138)
    * fix: don't cancel with invalid state
    
    * fixup
    
    * fixup

---
+++ test/fetch/issue-1137.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const { fetch } = require('../..')
+const { test } = require('tap')
+
+const controller = new AbortController()
+
+test('abort fetch', (t) => {
+  fetch(
+    'https://speed.hetzner.de/100MB.bin', {
+      signal: controller.signal
+  }).then(response => {
+    (async () => {
+        try {
+          await response.text()
+        } catch (err) {
+          t.equal(err.name, 'AbortError')
+        }
+        t.end()
+    })();
+    controller.abort()
+  })
+})
