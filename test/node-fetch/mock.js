cc70740a0c9a7f9d842930be3d2889c45a73a934
Guido D'Orsi <guido.dorsi@nearform.com>
2021-12-20 10:00:35 +0100

    fix(fetch): integration with MockAgent (#1149)
    * fix(fetch): integration with MockAgent
    
    * fix(fetch): integration with MockAgent
    
    * chore: cleanup
    
    * chore: cleaned up the tests
    
    * fix: more tests and headers support
    
    * fix: corrects a syntax error in node 12
    
    * Update lib/mock/mock-utils.js
    
    Co-authored-by: Robert Nagy <ronagy@icloud.com>
    
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>
    Co-authored-by: Robert Nagy <ronagy@icloud.com>

---
+++ test/node-fetch/mock.js
@@ -0,0 +1,86 @@
+/* eslint no-unused-expressions: "off" */
+
+// Test tools
+const chai = require('chai')
+
+const {
+  fetch,
+  MockAgent,
+  setGlobalDispatcher,
+  Headers
+} = require('../../index.js')
+
+const { expect } = chai
+
+describe('node-fetch with MockAgent', () => {
+  it('should match the url', async () => {
+    const mockAgent = new MockAgent()
+    setGlobalDispatcher(mockAgent)
+    const mockPool = mockAgent.get('http://localhost:3000')
+
+    mockPool
+      .intercept({
+        path: '/test',
+        method: 'GET',
+      })
+      .reply(200, { success: true })
+      .persist()
+
+    const res = await fetch('http://localhost:3000/test', {
+      method: 'GET'
+    })
+
+    expect(res.status).to.equal(200)
+    expect(await res.json()).to.deep.equal({ success: true })
+  })
+
+  it('should match the body', async () => {
+    const mockAgent = new MockAgent()
+    setGlobalDispatcher(mockAgent)
+    const mockPool = mockAgent.get('http://localhost:3000')
+
+    mockPool
+      .intercept({
+        path: '/test',
+        method: 'POST',
+        body: (value) => {
+          return value === 'request body'
+        }
+      })
+      .reply(200, { success: true })
+      .persist()
+
+    const res = await fetch('http://localhost:3000/test', {
+      method: 'POST',
+      body: 'request body'
+    })
+
+    expect(res.status).to.equal(200)
+    expect(await res.json()).to.deep.equal({ success: true })
+  })
+
+  it('should match the headers', async () => {
+    const mockAgent = new MockAgent()
+    setGlobalDispatcher(mockAgent)
+    const mockPool = mockAgent.get('http://localhost:3000')
+
+    mockPool
+      .intercept({
+        path: '/test',
+        method: 'GET',
+        headers: {
+          'User-Agent': /^undici$/,
+        }
+      })
+      .reply(200, { success: true })
+      .persist()
+
+    const res = await fetch('http://localhost:3000/test', {
+      method: 'GET',
+      headers: new Headers({ 'User-Agent': 'undici' })
+    })
+
+    expect(res.status).to.equal(200)
+    expect(await res.json()).to.deep.equal({ success: true })
+  })
+})
