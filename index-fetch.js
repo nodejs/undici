d063cc54746a3b84de531612017c67eab4c59b30
Mestery <mestery@protonmail.com>
2022-02-14 16:12:56 +0100

    Update index-fetch.js

--- index-fetch.js
+++ index-fetch.js
@@ -4,13 +4,12 @@ const Agent = require('./lib/agent')
 
 const globalDispatcher = new Agent()
 
-let fetchImpl = null
+const fetchImpl = require('./lib/fetch')
 module.exports.fetch = async function fetch (resource, init) {
-  if (!fetchImpl) {
-    fetchImpl = require('./lib/fetch')
-  }
   return fetchImpl.call(globalDispatcher, resource, init)
 }
+module.exports.FormData = require('./lib/fetch/formdata').FormData
 module.exports.Headers = require('./lib/fetch/headers').Headers
 module.exports.Response = require('./lib/fetch/response').Response
 module.exports.Request = require('./lib/fetch/request').Request
+
