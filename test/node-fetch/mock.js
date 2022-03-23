65feb05f8cbf19784ea7575d62c4975fd727801d
Filip Skokan <panva.ip@gmail.com>
2022-03-23 15:46:15 +0100

    fix(mock): matching the complete header list when using fetch (#1301)

--- test/node-fetch/mock.js
+++ test/node-fetch/mock.js
@@ -83,4 +83,31 @@ describe('node-fetch with MockAgent', () => {
     expect(res.status).to.equal(200)
     expect(await res.json()).to.deep.equal({ success: true })
   })
+
+  it('should match the headers with a matching function', async () => {
+    const mockAgent = new MockAgent()
+    setGlobalDispatcher(mockAgent)
+    const mockPool = mockAgent.get('http://localhost:3000')
+
+    mockPool
+      .intercept({
+        path: '/test',
+        method: 'GET',
+        headers (headers) {
+          expect(headers).to.be.an('object')
+          expect(headers).to.have.property('user-agent', 'undici')
+          return true
+        }
+      })
+      .reply(200, { success: true })
+      .persist()
+
+    const res = await fetch('http://localhost:3000/test', {
+      method: 'GET',
+      headers: new Headers({ 'User-Agent': 'undici' })
+    })
+
+    expect(res.status).to.equal(200)
+    expect(await res.json()).to.deep.equal({ success: true })
+  })
 })
