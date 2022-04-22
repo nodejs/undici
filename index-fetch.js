55d39a355c83dfeb52720d1085495f9b5d3a3a95
Simen Bekkhus <sbekkhus91@gmail.com>
2022-04-22 23:41:40 +0200

    chore: fix lint (#1361)

--- index-fetch.js
+++ index-fetch.js
@@ -12,4 +12,3 @@ module.exports.FormData = require('./lib/fetch/formdata').FormData
 module.exports.Headers = require('./lib/fetch/headers').Headers
 module.exports.Response = require('./lib/fetch/response').Response
 module.exports.Request = require('./lib/fetch/request').Request
-
