f4518b6eb7cab8f7fa985abe683af2478ec52070
Michaël Zasso <targos@protonmail.com>
2022-04-24 09:19:36 +0200

    fix: make index-fetch spec-compliant
    Refs: https://github.com/nodejs/undici/commit/3e267ece5f3a6b9b4cd6562f885a6884e3926937

--- index-fetch.js
+++ index-fetch.js
@@ -5,8 +5,8 @@ const Agent = require('./lib/agent')
 const globalDispatcher = new Agent()
 
 const fetchImpl = require('./lib/fetch')
-module.exports.fetch = async function fetch (resource, init) {
-  return fetchImpl.call(globalDispatcher, resource, init)
+module.exports.fetch = async function fetch (resource) {
+  return fetchImpl.apply(globalDispatcher, arguments)
 }
 module.exports.FormData = require('./lib/fetch/formdata').FormData
 module.exports.Headers = require('./lib/fetch/headers').Headers
