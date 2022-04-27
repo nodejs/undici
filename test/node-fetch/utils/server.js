9abcd90d508063b51c20cc80d51eb18d44f476d4
Sergiy Kyrylkov <kyrylkov@entriwise.com>
2022-04-27 13:05:03 +0200

    Add fetch timeout tests

--- test/node-fetch/utils/server.js
+++ test/node-fetch/utils/server.js
@@ -13,7 +13,7 @@ module.exports = class TestServer {
       console.log(err.stack)
     })
     this.server.on('connection', socket => {
-      socket.setTimeout(1500)
+      socket.setTimeout(310_000)
     })
   }
 
@@ -225,6 +225,22 @@ module.exports = class TestServer {
         res.setHeader('Content-Type', 'text/plain')
         res.end('text')
       }, 1000)
+    }
+
+    if (p === '/timeout60s') {
+      setTimeout(() => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('text')
+      }, 60_000)
+    }
+
+    if (p === '/timeout300s') {
+      setTimeout(() => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('text')
+      }, 300_000)
     }
 
     if (p === '/slow') {
