32c54ce4873c3fc46471c364c2eb40191dc76600
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-05-10 17:15:33 -0400

    fix(Headers Iterator): make spec compliant (#1431)

--- test/fetch/headers-iterator.js
+++ test/fetch/headers-iterator.js
@@ -9,15 +9,22 @@ test('Implements "Headers Iterator" properly', (t) => {
 
     for (const iterable of ['keys', 'values', 'entries', Symbol.iterator]) {
       const gen = headers[iterable]()
+      // https://tc39.es/ecma262/#sec-%25iteratorprototype%25-object
+      const IteratorPrototype = Object.getPrototypeOf(Object.getPrototypeOf([][Symbol.iterator]()))
+      const iteratorProto = Object.getPrototypeOf(gen)
 
       t.ok(gen.constructor === Object)
       t.ok(gen.prototype === undefined)
       // eslint-disable-next-line no-proto
       t.equal(gen.__proto__[Symbol.toStringTag], 'Headers Iterator')
       // https://github.com/node-fetch/node-fetch/issues/1119#issuecomment-100222049
-      t.notOk(gen instanceof function * () {}.constructor)
+      t.notOk(Headers.prototype[iterable] instanceof function * () {}.constructor)
       // eslint-disable-next-line no-proto
       t.ok(gen.__proto__.next.__proto__ === Function.prototype)
+      // https://webidl.spec.whatwg.org/#dfn-iterator-prototype-object
+      // "The [[Prototype]] internal slot of an iterator prototype object must be %IteratorPrototype%."
+      t.equal(gen[Symbol.iterator], IteratorPrototype[Symbol.iterator])
+      t.equal(Object.getPrototypeOf(iteratorProto), IteratorPrototype)
     }
 
     t.end()
@@ -39,6 +46,24 @@ test('Implements "Headers Iterator" properly', (t) => {
     t.equal(gen.return, undefined)
     t.equal(gen.throw, undefined)
     t.equal(typeof gen.next, 'function')
+    t.end()
+  })
+
+  t.test('Symbol.iterator', (t) => {
+    const values = new Headers([['a', 'b']]).entries()[Symbol.iterator]()
+
+    t.same(Array.from(values), [['a', 'b']])
+    t.end()
+  })
+
+  t.test('brand check', (t) => {
+    const gen = new Headers().entries()
+
+    t.throws(() => {
+      // eslint-disable-next-line no-proto
+      gen.__proto__.next()
+    }, TypeError)
+
     t.end()
   })
 
