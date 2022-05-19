0d8877a2da35532b79e8cbb9a9a5a72b382d86ce
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-05-19 08:24:15 -0400

    feat: add `Response.json` (#1452)
    * feat: add `Response.json`
    
    * fix: serialize value properly
    
    * fix: types & more coverage

--- types/fetch.d.ts
+++ types/fetch.d.ts
@@ -199,5 +199,6 @@ export declare class Response implements BodyMixin {
   readonly clone: () => Response
 
   static error (): Response
+  static json(data: any, init?: ResponseInit): Response
   static redirect (url: string | URL, status: ResponseRedirectStatus): Response
 }
