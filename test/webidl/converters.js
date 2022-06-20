e0484a02ab67abacbfa77b9c4642ae1b2f41b2c2
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-06-19 23:15:57 -0400

    feat(Headers): spec-compliant `fill` & constructor

--- test/webidl/converters.js
+++ test/webidl/converters.js
@@ -4,35 +4,43 @@ const { test } = require('tap')
 const { webidl } = require('../../lib/fetch/webidl')
 
 test('sequence', (t) => {
-  t.throws(() => {
-    webidl.converters.sequence(true)
-  }, TypeError)
-
-  t.throws(() => {
-    webidl.converters.sequence({})
-  }, TypeError)
-
-  t.throws(() => {
-    webidl.converters.sequence({
-      [Symbol.iterator]: 42
-    })
-  }, TypeError)
-
-  t.throws(() => {
-    webidl.converters.sequence({
-      [Symbol.iterator] () {
-        return {
-          next: 'never!'
-        }
-      }
-    })
-  }, TypeError)
-
   const converter = webidl.sequenceConverter(
     webidl.converters.DOMString
   )
 
   t.same(converter([1, 2, 3]), ['1', '2', '3'])
+
+  t.throws(() => {
+    converter(3)
+  }, TypeError, 'disallows non-objects')
+
+  t.throws(() => {
+    converter(null)
+  }, TypeError)
+
+  t.throws(() => {
+    converter(undefined)
+  }, TypeError)
+
+  t.throws(() => {
+    converter({})
+  }, TypeError, 'no Symbol.iterator')
+
+  t.throws(() => {
+    converter({
+      [Symbol.iterator]: 42
+    })
+  }, TypeError, 'invalid Symbol.iterator')
+
+  t.throws(() => {
+    converter(webidl.converters.sequence({
+      [Symbol.iterator] () {
+        return {
+          next: 'never!'
+        }
+      }
+    }))
+  }, TypeError, 'invalid generator')
 
   t.end()
 })
