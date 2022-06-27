c7f527b2a727a726d9e53c19297314d2850717cb
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-06-27 03:23:14 -0400

    fix(fetch): use AbortError DOMException (#1511)

--- test/fetch/abort.js
+++ test/fetch/abort.js
@@ -4,6 +4,8 @@ const { test } = require('tap')
 const { fetch } = require('../..')
 const { createServer } = require('http')
 const { once } = require('events')
+const { ReadableStream } = require('stream/web')
+const { DOMException } = require('../../lib/fetch/constants')
 
 /* global AbortController */
 
@@ -52,8 +54,55 @@ test('parallel fetch with the same AbortController works as expected', async (t)
   t.equal(rejected.length, 9) // out of 10 requests, only 1 should succeed
   t.equal(resolved.length, 1)
 
-  t.ok(rejected.every(rej => rej.reason?.code === 'ABORT_ERR'))
+  t.ok(rejected.every(rej => rej.reason?.code === DOMException.ABORT_ERR))
   t.same(resolved[0].value, body)
+
+  t.end()
+})
+
+// https://github.com/web-platform-tests/wpt/blob/fd8aeb1bb2eb33bc43f8a5bbc682b0cff6075dfe/fetch/api/abort/general.any.js#L474-L507
+test('Readable stream synchronously cancels with AbortError if aborted before reading', async (t) => {
+  const server = createServer((req, res) => {
+    res.write('')
+    res.end()
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  const controller = new AbortController()
+  const signal = controller.signal
+  controller.abort()
+
+  let cancelReason
+
+  const body = new ReadableStream({
+    pull (controller) {
+      controller.enqueue(new Uint8Array([42]))
+    },
+    cancel (reason) {
+      cancelReason = reason
+    }
+  })
+
+  const fetchPromise = fetch(`http://localhost:${server.address().port}`, {
+    body,
+    signal,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+
+  t.ok(cancelReason, 'Cancel called sync')
+  t.equal(cancelReason.constructor, DOMException)
+  t.equal(cancelReason.name, 'AbortError')
+
+  await t.rejects(fetchPromise, { name: 'AbortError' })
+
+  const fetchErr = await fetchPromise.catch(e => e)
+
+  t.equal(cancelReason, fetchErr, 'Fetch rejects with same error instance')
 
   t.end()
 })
