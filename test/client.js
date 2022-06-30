fcbb427b73edc53e2d2f52054fbce46c16d93f88
Igor Savin <iselwin@gmail.com>
2022-06-30 13:21:21 +0300

    feat(Errors): include error response body in status code errors (v2) (#1519)
    * Second attempt to implement error response parsing
    
    * Add missing error type
    
    * Add error handling
    
    * Update types/errors.d.ts
    
    Co-authored-by: Robert Nagy <ronagy@icloud.com>
    
    * Update lib/api/api-request.js
    
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>
    
    * Address code review comments
    
    Co-authored-by: Robert Nagy <ronagy@icloud.com>
    Co-authored-by: Matteo Collina <matteo.collina@gmail.com>

--- test/client.js
+++ test/client.js
@@ -324,7 +324,7 @@ test('basic get with query params partially in path', (t) => {
 })
 
 test('basic get returns 400 when configured to throw on errors (callback)', (t) => {
-  t.plan(6)
+  t.plan(7)
 
   const server = createServer((req, res) => {
     res.statusCode = 400
@@ -350,13 +350,46 @@ test('basic get returns 400 when configured to throw on errors (callback)', (t) 
       t.equal(err.statusCode, 400)
       t.equal(err.headers.connection, 'keep-alive')
       t.equal(err.headers['content-length'], '5')
+      t.same(err.body, null)
+    })
+    t.equal(signal.listenerCount('abort'), 1)
+  })
+})
+
+test('basic get returns 400 when configured to throw on errors and correctly handles malformed json (callback)', (t) => {
+  t.plan(6)
+
+  const server = createServer((req, res) => {
+    res.writeHead(400, 'Invalid params', { 'content-type': 'application/json' })
+    res.end('Invalid params')
+  })
+  t.teardown(server.close.bind(server))
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, {
+      keepAliveTimeout: 300e3
+    })
+    t.teardown(client.close.bind(client))
+
+    const signal = new EE()
+    client.request({
+      signal,
+      path: '/',
+      method: 'GET',
+      throwOnError: true
+    }, (err) => {
+      t.equal(err.message, 'Response status code 400: Invalid params')
+      t.equal(err.status, 400)
+      t.equal(err.statusCode, 400)
+      t.equal(err.headers.connection, 'keep-alive')
+      t.same(err.body, null)
     })
     t.equal(signal.listenerCount('abort'), 1)
   })
 })
 
 test('basic get returns 400 when configured to throw on errors (promise)', (t) => {
-  t.plan(5)
+  t.plan(6)
 
   const server = createServer((req, res) => {
     res.writeHead(400, 'Invalid params', { 'content-type': 'text/plain' })
@@ -383,6 +416,7 @@ test('basic get returns 400 when configured to throw on errors (promise)', (t) =
       t.equal(err.message, 'Response status code 400: Invalid params')
       t.equal(err.status, 400)
       t.equal(err.statusCode, 400)
+      t.equal(err.body, 'Invalid params')
       t.equal(err.headers.connection, 'keep-alive')
       t.equal(err.headers['content-type'], 'text/plain')
     }
@@ -390,7 +424,7 @@ test('basic get returns 400 when configured to throw on errors (promise)', (t) =
 })
 
 test('basic get returns error body when configured to throw on errors', (t) => {
-  t.plan(5)
+  t.plan(6)
 
   const server = createServer((req, res) => {
     const body = { msg: 'Error', details: { code: 94 } }
@@ -423,6 +457,7 @@ test('basic get returns error body when configured to throw on errors', (t) => {
       t.equal(err.statusCode, 400)
       t.equal(err.headers.connection, 'keep-alive')
       t.equal(err.headers['content-type'], 'application/json')
+      t.same(err.body, { msg: 'Error', details: { code: 94 } })
     }
   })
 })
