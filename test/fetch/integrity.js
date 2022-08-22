21fdda54ca290456083874ab0727e3c343ee8e45
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-08-22 02:31:05 -0400

    feat(fetch): implement spec compliant integrity checks (#1613)

--- test/fetch/integrity.js
+++ test/fetch/integrity.js
@@ -2,9 +2,12 @@
 
 const { test } = require('tap')
 const { createServer } = require('http')
-const { createHash } = require('crypto')
+const { createHash, getHashes } = require('crypto')
 const { gzipSync } = require('zlib')
 const { fetch, setGlobalDispatcher, Agent } = require('../..')
+const { once } = require('events')
+
+const supportedHashes = getHashes()
 
 setGlobalDispatcher(new Agent({
   keepAliveTimeout: 1,
@@ -44,7 +47,7 @@ test('request with wrong integrity checksum', (t) => {
     fetch(`http://localhost:${server.address().port}`, {
       integrity: `sha256-${hash}`
     }).then(response => {
-      t.fail('fetch did not fail')
+      t.pass('request did not fail')
     }).catch((err) => {
       t.equal(err.cause.message, 'integrity mismatch')
     }).finally(() => {
@@ -71,4 +74,77 @@ test('request with integrity checksum on encoded body', (t) => {
     t.strictSame(body, await response.text())
     t.end()
   })
+})
+
+test('request with a totally incorrect integrity', async (t) => {
+  const server = createServer((req, res) => {
+    res.end()
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  await t.resolves(fetch(`http://localhost:${server.address().port}`, {
+    integrity: 'what-integrityisthis'
+  }))
+})
+
+test('request with mixed in/valid integrities', async (t) => {
+  const body = 'Hello world!'
+  const hash = createHash('sha256').update(body).digest('base64')
+
+  const server = createServer((req, res) => {
+    res.end(body)
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  await t.resolves(fetch(`http://localhost:${server.address().port}`, {
+    integrity: `invalid-integrity sha256-${hash}`
+  }))
+})
+
+test('request with sha384 hash', { skip: !supportedHashes.includes('sha384') }, async (t) => {
+  const body = 'Hello world!'
+  const hash = createHash('sha384').update(body).digest('base64')
+
+  const server = createServer((req, res) => {
+    res.end(body)
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  // request should succeed
+  await t.resolves(fetch(`http://localhost:${server.address().port}`, {
+    integrity: `sha384-${hash}`
+  }))
+
+  // request should fail
+  await t.rejects(fetch(`http://localhost:${server.address().port}`, {
+    integrity: 'sha384-ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='
+  }))
+})
+
+test('request with sha512 hash', { skip: !supportedHashes.includes('sha512') }, async (t) => {
+  const body = 'Hello world!'
+  const hash = createHash('sha512').update(body).digest('base64')
+
+  const server = createServer((req, res) => {
+    res.end(body)
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  // request should succeed
+  await t.resolves(fetch(`http://localhost:${server.address().port}`, {
+    integrity: `sha512-${hash}`
+  }))
+
+  // request should fail
+  await t.rejects(fetch(`http://localhost:${server.address().port}`, {
+    integrity: 'sha512-ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='
+  }))
 })
