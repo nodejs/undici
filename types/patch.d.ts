cccf3f5a91580bcda74319cda77a972415d3acbb
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-11-07 14:46:32 -0500

    fix: try new approach

--- types/patch.d.ts
+++ types/patch.d.ts
@@ -6,13 +6,43 @@ export type DOMException = typeof globalThis extends { DOMException: infer T }
  ? T
  : any
 
-export type EventTarget = typeof globalThis extends { EventTarget: infer T }
+export type EventTarget = typeof globalThis extends { EventTarget: { prototype: infer T } }
   ? T
-  : any
+  : {
+    addEventListener(
+      type: string,
+      listener: EventListener | EventListenerObject,
+      options?: AddEventListenerOptions | boolean,
+    ): void
+    dispatchEvent(event: Event): boolean
+    removeEventListener(
+      type: string,
+      listener: EventListener | EventListenerObject,
+      options?: EventListenerOptions | boolean,
+    ): void
+  }
 
-export type Event = typeof globalThis extends { Event: infer T }
+export type Event = typeof globalThis extends { Event: { prototype: infer T } }
   ? T
-  : any
+  : {
+    readonly bubbles: boolean
+    cancelBubble: () => void
+    readonly cancelable: boolean
+    readonly composed: boolean
+    composedPath(): [EventTarget?]
+    readonly currentTarget: EventTarget | null
+    readonly defaultPrevented: boolean
+    readonly eventPhase: 0 | 2
+    readonly isTrusted: boolean
+    preventDefault(): void
+    returnValue: boolean
+    readonly srcElement: EventTarget | null
+    stopImmediatePropagation(): void
+    stopPropagation(): void
+    readonly target: EventTarget | null
+    readonly timeStamp: number
+    readonly type: string
+  }
 
 export interface EventInit {
   bubbles?: boolean
