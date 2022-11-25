7483127795817f4c1374a2062aa7e55d99f2463a
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-11-25 11:00:02 -0500

    fix(fetch): set content-length header for FormData body

---
+++ test/fetch/content-length.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { test } = require('tap')
+const { Blob } = require('buffer')
+const { FormData, fetch } = require('../..')
+const { once } = require('events')
+const { createServer } = require('http')
+
+// https://github.com/nodejs/undici/issues/1783
+test('Sending a FormData body sets Content-Length header', async (t) => {
+  const server = createServer((req, res) => {
+    t.equal(req.headers['content-length'], '285')
+    res.end()
+  }).listen(0)
+
+  await once(server, 'listening')
+  t.teardown(server.close.bind(server))
+
+  const blob = new Blob(['body'], { type: 'text/plain' })
+
+  const fd = new FormData()
+  fd.append('file', blob)
+  fd.append('string', 'string value')
+
+  await fetch(`http://localhost:${server.address().port}`, {
+    method: 'POST',
+    body: fd
+  })
+})
