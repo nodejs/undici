7ee907b8a063f3d97a5528fc0186c201b89531c4
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-12-06 17:33:20 -0500

    fix: add types for MessageEvent and CloseEvent

--- types/websocket.d.ts
+++ types/websocket.d.ts
@@ -3,6 +3,7 @@
 import {
   EventTarget,
   Event,
+  EventInit,
   EventListenerOptions,
   AddEventListenerOptions,
   EventListenerOrEventListenerObject
@@ -10,11 +11,10 @@ import {
 
 export type BinaryType = 'blob' | 'arraybuffer'
 
-// TODO: add CloseEvent and MessageEvent
 interface WebSocketEventMap {
-  close: Event
+  close: CloseEvent
   error: Event
-  message: Event
+  message: MessageEvent
   open: Event
 }
 
@@ -70,4 +70,52 @@ export declare const WebSocket: {
   readonly CLOSING: number
   readonly CONNECTING: number
   readonly OPEN: number
+}
+
+interface CloseEventInit extends EventInit {
+  code?: number
+  reason?: string
+  wasClean?: boolean
+}
+
+interface CloseEvent extends Event {
+  readonly code: number
+  readonly reason: string
+  readonly wasClean: boolean
+}
+
+export declare const CloseEvent: {
+  prototype: CloseEvent
+  new (type: string, eventInitDict?: CloseEventInit): CloseEvent
+}
+
+interface MessageEventInit<T = any> extends EventInit {
+  data?: T
+  lastEventId?: string
+  origin?: string
+  ports?: (typeof MessagePort)[]
+  source?: typeof MessagePort | null
+}
+
+interface MessageEvent<T = any> extends Event {
+  readonly data: T
+  readonly lastEventId: string
+  readonly origin: string
+  readonly ports: ReadonlyArray<typeof MessagePort>
+  readonly source: typeof MessagePort | null
+  initMessageEvent(
+    type: string,
+    bubbles?: boolean,
+    cancelable?: boolean,
+    data?: any,
+    origin?: string,
+    lastEventId?: string,
+    source?: typeof MessagePort | null,
+    ports?: (typeof MessagePort)[]
+  ): void;
+}
+
+export declare const MessageEvent: {
+  prototype: MessageEvent
+  new<T>(type: string, eventInitDict?: MessageEventInit<T>): MessageEvent<T>
 }
