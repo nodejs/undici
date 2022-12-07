6caaa4786b33d2e85c8e79609c5e7cbe43eabe2d
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-12-07 15:32:26 -0500

    fix: allow sending payloads > 65k bytes

--- test/wpt/server/websocket.mjs
+++ test/wpt/server/websocket.mjs
@@ -1,6 +1,17 @@
 import { WebSocketServer } from 'ws'
 import { server } from './server.mjs'
 
+// When sending a buffer to ws' send method, it auto
+// sets the type to binary. This breaks some tests.
+const textData = [
+  '¥¥¥¥¥¥',
+  'Message to send',
+  '𐐇',
+  '\ufffd',
+  '',
+  'null'
+]
+
 // The file router server handles sending the url, closing,
 // and sending messages back to the main process for us.
 // The types for WebSocketServer don't include a `request`
@@ -13,7 +24,8 @@ const wss = new WebSocketServer({
 
 wss.on('connection', (ws) => {
   ws.on('message', (data) => {
-    ws.send(data)
+    const binary = !textData.includes(data.toString('utf-8'))
+    ws.send(data, { binary })
   })
 
   // Some tests, such as `Create-blocked-port.any.js` do NOT
