f06b0a551e6ae4c73a8ad877400bae91b6ebcaa1
Carlos Fuentes <me@metcoder.dev>
2022-12-21 20:21:55 +0100

    feat(Client): add support for reset option to disable keep alive

--- test/invalid-headers.js
+++ test/invalid-headers.js
@@ -4,7 +4,7 @@ const { test } = require('tap')
 const { Client, errors } = require('..')
 
 test('invalid headers', (t) => {
-  t.plan(10)
+  t.plan(9)
 
   const client = new Client('http://localhost:3000')
   t.teardown(client.destroy.bind(client))
@@ -41,16 +41,6 @@ test('invalid headers', (t) => {
     method: 'GET',
     headers: {
       upgrade: 'asd'
-    }
-  }, (err, data) => {
-    t.type(err, errors.InvalidArgumentError)
-  })
-
-  client.request({
-    path: '/',
-    method: 'GET',
-    headers: {
-      connection: 'close'
     }
   }, (err, data) => {
     t.type(err, errors.InvalidArgumentError)
