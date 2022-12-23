e8241025de5cff55200dd6280012cc25dd55564f
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-12-23 14:33:37 -0500

    test: add tests for WebSocket constructor

---
+++ test/websocket/constructor.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const { test } = require('tap')
+const { WebSocket } = require('../..')
+
+test('Constructor', (t) => {
+  t.throws(
+    () => new WebSocket('abc'),
+    {
+      name: 'SyntaxError',
+      constructor: DOMException
+    }
+  )
+
+  t.throws(
+    () => new WebSocket('https://www.google.com'),
+    {
+      name: 'SyntaxError',
+      constructor: DOMException
+    }
+  )
+
+  t.throws(
+    () => new WebSocket('wss://echo.websocket.events/#a'),
+    {
+      name: 'SyntaxError',
+      constructor: DOMException
+    }
+  )
+
+  t.throws(
+    () => new WebSocket('wss://echo.websocket.events', ''),
+    {
+      name: 'SyntaxError',
+      constructor: DOMException
+    }
+  )
+
+  t.throws(
+    () => new WebSocket('wss://echo.websocket.events', ['chat', 'chat']),
+    {
+      name: 'SyntaxError',
+      constructor: DOMException
+    }
+  )
+
+  t.end()
+})
