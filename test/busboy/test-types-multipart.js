119e172d36e599a1747ee861b8179af5d2160ff5
Khafra <42794878+KhafraDev@users.noreply.github.com>
2023-01-07 13:03:27 -0500

    fix: error on _final if parsing isn't complete

--- test/busboy/test-types-multipart.js
+++ test/busboy/test-types-multipart.js
@@ -931,7 +931,7 @@ const tests = [
   }
 ]
 
-for (const test of tests.slice(0, 1)) {
+for (const test of tests.slice(0, 3)) {
   active.set(test, 1)
 
   const { what, boundary, events, limits, preservePath, fileHwm } = test
@@ -1013,7 +1013,7 @@ for (const test of tests.slice(0, 1)) {
 }
 
 // Byte-by-byte versions
-for (let test of tests.slice(0, 1)) {
+for (let test of tests.slice(0, 3)) {
   test = { ...test }
   test.what += ' (byte-by-byte)'
   active.set(test, 1)
