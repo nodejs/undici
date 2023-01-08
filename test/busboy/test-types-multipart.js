6c1b276d6e38c48ce5134faeaced92385bd1f34e
Khafra <42794878+KhafraDev@users.noreply.github.com>
2023-01-08 16:49:11 -0500

    fix(finally): validate header name and fix parser issues

--- test/busboy/test-types-multipart.js
+++ test/busboy/test-types-multipart.js
@@ -931,7 +931,7 @@ const tests = [
   }
 ]
 
-for (const test of tests.slice(0, 10)) {
+for (const test of tests.slice(0, 11)) {
   active.set(test, 1)
 
   const { what, boundary, events, limits, preservePath, fileHwm } = test
@@ -1013,7 +1013,7 @@ for (const test of tests.slice(0, 10)) {
 }
 
 // Byte-by-byte versions
-for (let test of tests.slice(0, 10)) {
+for (let test of tests.slice(0, 11)) {
   test = { ...test }
   test.what += ' (byte-by-byte)'
   active.set(test, 1)
