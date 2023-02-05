a4dfaa01caaf7c421182ae650e537e07e7484506
Robert Nagy <ronagy@icloud.com>
2023-02-05 13:50:24 +0100

    fixup

--- test/client-timeout.js
+++ test/client-timeout.js
@@ -7,44 +7,44 @@ const { Readable } = require('stream')
 const FakeTimers = require('@sinonjs/fake-timers')
 const timers = require('../lib/timers')
 
-// test('refresh timeout on pause', (t) => {
-//   t.plan(1)
+test('refresh timeout on pause', (t) => {
+  t.plan(1)
 
-//   const server = createServer((req, res) => {
-//     res.flushHeaders()
-//   })
-//   t.teardown(server.close.bind(server))
+  const server = createServer((req, res) => {
+    res.flushHeaders()
+  })
+  t.teardown(server.close.bind(server))
 
-//   server.listen(0, () => {
-//     const client = new Client(`http://localhost:${server.address().port}`, {
-//       bodyTimeout: 500
-//     })
-//     t.teardown(client.destroy.bind(client))
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, {
+      bodyTimeout: 500
+    })
+    t.teardown(client.destroy.bind(client))
 
-//     client.dispatch({
-//       path: '/',
-//       method: 'GET'
-//     }, {
-//       onConnect () {
-//       },
-//       onHeaders (statusCode, headers, resume) {
-//         setTimeout(() => {
-//           resume()
-//         }, 1000)
-//         return false
-//       },
-//       onData () {
+    client.dispatch({
+      path: '/',
+      method: 'GET'
+    }, {
+      onConnect () {
+      },
+      onHeaders (statusCode, headers, resume) {
+        setTimeout(() => {
+          resume()
+        }, 1000)
+        return false
+      },
+      onData () {
 
-//       },
-//       onComplete () {
+      },
+      onComplete () {
 
-//       },
-//       onError (err) {
-//         t.type(err, errors.BodyTimeoutError)
-//       }
-//     })
-//   })
-// })
+      },
+      onError (err) {
+        t.type(err, errors.BodyTimeoutError)
+      }
+    })
+  })
+})
 
 test('start headers timeout after request body', (t) => {
   t.plan(2)
