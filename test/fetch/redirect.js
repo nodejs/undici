066948bcb6d237583c81e3c46cb18b1dcb20160f
Michele Azzolari <michele@azzolari.it>
2023-03-27 15:59:21 +0200

    fix(fetch): remove assertion on request.body.source on redirect (#2027)
        Fixes https://github.com/nodejs/undici/issues/2027

--- test/fetch/redirect.js
+++ test/fetch/redirect.js
@@ -27,3 +27,24 @@ test('Redirecting with a body does not cancel the current request - #1776', asyn
   t.equal(await resp.text(), '/redirect/')
   t.ok(resp.redirected)
 })
+
+test('Redirecting with an empty body does not throw an error - #2027', async (t) => {
+  const server = createServer((req, res) => {
+    if (req.url === '/redirect') {
+      res.statusCode = 307
+      res.setHeader('location', '/redirect/')
+      res.write('<a href="/redirect/">Moved Permanently</a>')
+      res.end()
+      return
+    }
+    res.write(req.url)
+    res.end()
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  const resp = await fetch(`http://localhost:${server.address().port}/redirect`, { method: 'PUT', body: '' })
+  t.equal(await resp.text(), '/redirect/')
+  t.ok(resp.redirected)
+})
