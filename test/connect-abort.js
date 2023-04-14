3e9191513d0f6db4421640e4b50b773dc3a02eaa
Robert Nagy <ronagy@icloud.com>
2023-04-14 17:25:25 +0200

    fix: don't leak socket if client is destroyed while connecting (#2058)
    * fix: don't leak socket if client is destroyed will connecting
    
    * Update types/connector.d.ts

---
+++ test/connect-abort.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const { test } = require('tap')
+const { Client } = require('..')
+const { PassThrough } = require('stream')
+
+test(t => {
+  t.plan(2)
+
+  const client = new Client('http://localhost:1234', {
+    connect: (_, cb) => {
+      client.destroy()
+      cb(null, new PassThrough({
+        destroy (err, cb) {
+          t.same(err?.name, 'ClientDestroyedError')
+          cb(null)
+        }
+      }))
+    }
+  })
+
+  client.request({
+    path: '/',
+    method: 'GET'
+  }, (err, data) => {
+    t.same(err?.name, 'ClientDestroyedError')
+  })
+})
