3a179f25180ca3743622376251bd63bde6a19bea
Khafra <maitken033380023@gmail.com>
2023-04-16 21:38:15 -0400

    test: add cache-storage wpts

---
+++ test/wpt/start-cacheStorage.mjs
@@ -0,0 +1,28 @@
+import { WPTRunner } from './runner/runner.mjs'
+import { join } from 'path'
+import { fileURLToPath } from 'url'
+import { fork } from 'child_process'
+import { on } from 'events'
+import { CacheStorage } from '../../lib/cache/cachestorage.js'
+import { kConstruct } from '../../lib/cache/symbols.js'
+
+const serverPath = fileURLToPath(join(import.meta.url, '../server/server.mjs'))
+
+const child = fork(serverPath, [], {
+  stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+})
+
+child.on('exit', (code) => process.exit(code))
+
+for await (const [message] of on(child, 'message')) {
+  if (message.server) {
+    const runner = new WPTRunner('service-workers/cache-storage', message.server)
+    runner.run()
+
+    runner.once('completion', () => {
+      if (child.connected) {
+        child.send('shutdown')
+      }
+    })
+  }
+}
