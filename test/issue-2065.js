8ffad9d782146d5dd405b81eeb2cbfe97c6a03b1
fengmk2 <fengmk2@gmail.com>
2023-04-27 12:55:25 +0800

    fix: skip set content-length when FormData value is stream (#2091)
    https://github.com/nodejs/undici/pull/2066/files#r1174932605

--- test/issue-2065.js
+++ test/issue-2065.js
@@ -4,6 +4,7 @@ const { test, skip } = require('tap')
 const { nodeMajor, nodeMinor } = require('../lib/core/util')
 const { createServer } = require('http')
 const { once } = require('events')
+const { createReadStream } = require('fs')
 const { File, FormData, request } = require('..')
 
 if (nodeMajor < 16 || (nodeMajor === 16 && nodeMinor < 8)) {
@@ -22,6 +23,46 @@ test('undici.request with a FormData body should set content-length header', asy
 
   const body = new FormData()
   body.set('file', new File(['abc'], 'abc.txt'))
+
+  await request(`http://localhost:${server.address().port}`, {
+    method: 'POST',
+    body
+  })
+})
+
+test('undici.request with a FormData stream value should set transfer-encoding header', async (t) => {
+  const server = createServer((req, res) => {
+    t.equal(req.headers['transfer-encoding'], 'chunked')
+    res.end()
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  class BlobFromStream {
+    #stream
+    #type
+    constructor (stream, type) {
+      this.#stream = stream
+      this.#type = type
+    }
+
+    stream () {
+      return this.#stream
+    }
+
+    get type () {
+      return this.#type
+    }
+
+    get [Symbol.toStringTag] () {
+      return 'Blob'
+    }
+  }
+
+  const body = new FormData()
+  const fileReadable = createReadStream(__filename)
+  body.set('file', new BlobFromStream(fileReadable, '.js'), 'streamfile')
 
   await request(`http://localhost:${server.address().port}`, {
     method: 'POST',
