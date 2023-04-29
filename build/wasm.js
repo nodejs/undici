3c514d8d98b3887db165b0ace3014a9c6ad0dfb9
Jason Zhang <xzha4350@gmail.com>
2023-04-30 01:11:42 +0930

    feat: allow build:wasm to auto detect platform (#2102)

--- build/wasm.js
+++ build/wasm.js
@@ -3,29 +3,41 @@
 const { execSync } = require('child_process')
 const { writeFileSync, readFileSync } = require('fs')
 const { join, resolve } = require('path')
-const { WASI_ROOT } = process.env
 
 const ROOT = resolve(__dirname, '../')
 const WASM_SRC = resolve(__dirname, '../deps/llhttp')
 const WASM_OUT = resolve(__dirname, '../lib/llhttp')
+const DOCKERFILE = resolve(__dirname, './Dockerfile')
+
+let platform = process.env.WASM_PLATFORM
+if (!platform && process.argv[2]) {
+  platform = execSync('docker info -f "{{.OSType}}/{{.Architecture}}"').toString().trim()
+}
+
+if (process.argv[2] === '--prebuild') {
+  const cmd = `docker build --platform=${platform.toString().trim()} -t llhttp_wasm_builder -f ${DOCKERFILE} ${ROOT}`
+
+  console.log(`> ${cmd}\n\n`)
+  execSync(cmd, { stdio: 'inherit' })
+
+  process.exit(0)
+}
 
 if (process.argv[2] === '--docker') {
-  let cmd = 'docker run --rm -it'
+  let cmd = `docker run --rm -it --platform=${platform.toString().trim()}`
   if (process.platform === 'linux') {
     cmd += ` --user ${process.getuid()}:${process.getegid()}`
   }
+
   cmd += ` --mount type=bind,source=${ROOT}/lib/llhttp,target=/home/node/undici/lib/llhttp llhttp_wasm_builder node build/wasm.js`
+  console.log(`> ${cmd}\n\n`)
   execSync(cmd, { stdio: 'inherit' })
   process.exit(0)
 }
 
-if (!WASI_ROOT) {
-  throw new Error('Please setup the WASI_ROOT env variable.')
-}
-
 // Build wasm binary
-execSync(`${WASI_ROOT}/bin/clang \
- --sysroot=${WASI_ROOT}/share/wasi-sysroot \
+execSync(`clang \
+ --sysroot=/usr/share/wasi-sysroot \
  -target wasm32-unknown-wasi \
  -Ofast \
  -fno-exceptions \
@@ -40,6 +52,7 @@ execSync(`${WASI_ROOT}/bin/clang \
  -Wl,--export-table \
  -Wl,--export=malloc \
  -Wl,--export=free \
+ -Wl,--no-entry \
  ${join(WASM_SRC, 'src')}/*.c \
  -I${join(WASM_SRC, 'include')} \
  -o ${join(WASM_OUT, 'llhttp.wasm')}`, { stdio: 'inherit' })
@@ -51,8 +64,8 @@ writeFileSync(
 )
 
 // Build wasm simd binary
-execSync(`${WASI_ROOT}/bin/clang \
- --sysroot=${WASI_ROOT}/share/wasi-sysroot \
+execSync(`clang \
+ --sysroot=/usr/share/wasi-sysroot \
  -target wasm32-unknown-wasi \
  -msimd128 \
  -Ofast \
@@ -68,6 +81,7 @@ execSync(`${WASI_ROOT}/bin/clang \
  -Wl,--export-table \
  -Wl,--export=malloc \
  -Wl,--export=free \
+ -Wl,--no-entry \
  ${join(WASM_SRC, 'src')}/*.c \
  -I${join(WASM_SRC, 'include')} \
  -o ${join(WASM_OUT, 'llhttp_simd.wasm')}`, { stdio: 'inherit' })
