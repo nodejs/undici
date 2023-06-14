c415fbbb59e2b898c5db6a681265cf3da865d02c
Georgii Rychko <rychko.georgiy@gmail.com>
2023-06-14 05:33:47 +0300

    fix: reverse decompression order of "Content-Encoding" encodings (fixes #2158) (#2159)
    Co-authored-by: rychkog-ma <georgii.ry@moonactive.com>

--- test/fetch/encoding.js
+++ test/fetch/encoding.js
@@ -4,7 +4,7 @@ const { test } = require('tap')
 const { createServer } = require('http')
 const { once } = require('events')
 const { fetch } = require('../..')
-const { createBrotliCompress, createGzip } = require('zlib')
+const { createBrotliCompress, createGzip, createDeflate } = require('zlib')
 
 test('content-encoding header is case-iNsENsITIve', async (t) => {
   const contentCodings = 'GZiP, bR'
@@ -17,10 +17,10 @@ test('content-encoding header is case-iNsENsITIve', async (t) => {
     res.setHeader('Content-Encoding', contentCodings)
     res.setHeader('Content-Type', 'text/plain')
 
-    brotli.pipe(gzip).pipe(res)
+    gzip.pipe(brotli).pipe(res)
 
-    brotli.write(text)
-    brotli.end()
+    gzip.write(text)
+    gzip.end()
   }).listen(0)
 
   t.teardown(server.close.bind(server))
@@ -30,4 +30,29 @@ test('content-encoding header is case-iNsENsITIve', async (t) => {
 
   t.equal(await response.text(), text)
   t.equal(response.headers.get('content-encoding'), contentCodings)
+})
+
+test('response decompression according to content-encoding should be handled in a correct order', async (t) => {
+  const contentCodings = 'deflate, gzip'
+  const text = 'Hello, World!'
+
+  const server = createServer((req, res) => {
+    const gzip = createGzip()
+    const deflate = createDeflate()
+
+    res.setHeader('Content-Encoding', contentCodings)
+    res.setHeader('Content-Type', 'text/plain')
+
+    deflate.pipe(gzip).pipe(res)
+
+    deflate.write(text)
+    deflate.end()
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  const response = await fetch(`http://localhost:${server.address().port}`)
+
+  t.equal(await response.text(), text)
 })
