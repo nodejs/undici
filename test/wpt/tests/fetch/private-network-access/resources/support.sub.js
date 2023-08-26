111fd2388094270259814ca7ea5da7d07b2126f6
Khafra <maitken033380023@gmail.com>
2023-08-26 07:32:35 -0400

    Update wpts (#2226)

--- test/wpt/tests/fetch/private-network-access/resources/support.sub.js
+++ test/wpt/tests/fetch/private-network-access/resources/support.sub.js
@@ -75,21 +75,21 @@ async function postMessageAndAwaitReply(target, message) {
 // Maps protocol (without the trailing colon) and address space to port.
 const SERVER_PORTS = {
   "http": {
-    "loopback": {{ports[http][0]}},
-    "local": {{ports[http-private][0]}},
+    "local": {{ports[http][0]}},
+    "private": {{ports[http-private][0]}},
     "public": {{ports[http-public][0]}},
   },
   "https": {
-    "loopback": {{ports[https][0]}},
-    "other-loopback": {{ports[https][1]}},
-    "local": {{ports[https-private][0]}},
+    "local": {{ports[https][0]}},
+    "other-local": {{ports[https][1]}},
+    "private": {{ports[https-private][0]}},
     "public": {{ports[https-public][0]}},
   },
   "ws": {
-    "loopback": {{ports[ws][0]}},
+    "local": {{ports[ws][0]}},
   },
   "wss": {
-    "loopback": {{ports[wss][0]}},
+    "local": {{ports[wss][0]}},
   },
 };
 
@@ -127,15 +127,15 @@ class Server {
     };
   }
 
-  static HTTP_LOCAL = Server.get("http", "loopback");
-  static HTTP_PRIVATE = Server.get("http", "local");
+  static HTTP_LOCAL = Server.get("http", "local");
+  static HTTP_PRIVATE = Server.get("http", "private");
   static HTTP_PUBLIC = Server.get("http", "public");
-  static HTTPS_LOCAL = Server.get("https", "loopback");
-  static OTHER_HTTPS_LOCAL = Server.get("https", "other-loopback");
-  static HTTPS_PRIVATE = Server.get("https", "local");
+  static HTTPS_LOCAL = Server.get("https", "local");
+  static OTHER_HTTPS_LOCAL = Server.get("https", "other-local");
+  static HTTPS_PRIVATE = Server.get("https", "private");
   static HTTPS_PUBLIC = Server.get("https", "public");
-  static WS_LOCAL = Server.get("ws", "loopback");
-  static WSS_LOCAL = Server.get("wss", "loopback");
+  static WS_LOCAL = Server.get("ws", "local");
+  static WSS_LOCAL = Server.get("wss", "local");
 };
 
 // Resolves a URL relative to the current location, returning an absolute URL.
@@ -341,6 +341,40 @@ async function fetchTest(t, { source, target, fetchOptions, expected }) {
   }
 }
 
+// Similar to `fetchTest`, but replaced iframes with fenced frames.
+async function fencedFrameFetchTest(t, { source, target, fetchOptions, expected }) {
+  const fetcher_url =
+      resolveUrl("resources/fenced-frame-fetcher.https.html", sourceResolveOptions(source));
+
+  const target_url = preflightUrl(target);
+  target_url.searchParams.set("is-loaded-in-fenced-frame", true);
+
+  fetcher_url.searchParams.set("mode", fetchOptions.mode);
+  fetcher_url.searchParams.set("method", fetchOptions.method);
+  fetcher_url.searchParams.set("url", target_url);
+
+  const error_token = token();
+  const ok_token = token();
+  const body_token = token();
+  const type_token = token();
+  const source_url = generateURL(fetcher_url, [error_token, ok_token, body_token, type_token]);
+
+  const urn = await generateURNFromFledge(source_url, []);
+  attachFencedFrame(urn);
+
+  const error = await nextValueFromServer(error_token);
+  const ok = await nextValueFromServer(ok_token);
+  const body = await nextValueFromServer(body_token);
+  const type = await nextValueFromServer(type_token);
+
+  assert_equals(error, expected.error || "" , "error");
+  assert_equals(body, expected.body || "", "response body");
+  assert_equals(ok, expected.ok !== undefined ? expected.ok.toString() : "", "response ok");
+  if (expected.type !== undefined) {
+    assert_equals(type, expected.type, "response type");
+  }
+}
+
 const XhrTestResult = {
   SUCCESS: {
     loaded: true,
@@ -393,7 +427,7 @@ async function xhrTest(t, { source, target, method, expected }) {
   assert_equals(body, expected.body, "response body");
 }
 
-const IframeTestResult = {
+const FrameTestResult = {
   SUCCESS: "loaded",
   FAILURE: "timeout",
 };
@@ -425,6 +459,37 @@ async function iframeTest(t, { source, target, expected }) {
         t.step_timeout(() => resolve("timeout"), 500 /* ms */);
       }),
   ]);
+
+  assert_equals(result, expected);
+}
+
+// Similar to `iframeTest`, but replaced iframes with fenced frames.
+async function fencedFrameTest(t, { source, target, expected }) {
+  // Allows running tests in parallel.
+  const target_url = preflightUrl(target);
+  target_url.searchParams.set("file", "fenced-frame-local-network-access-target.https.html");
+  target_url.searchParams.set("is-loaded-in-fenced-frame", true);
+
+  const frame_loaded_key = token();
+  const child_frame_target = generateURL(target_url, [frame_loaded_key]);
+
+  const source_url =
+      resolveUrl("resources/fenced-frame-local-network-access.https.html", sourceResolveOptions(source));
+  source_url.searchParams.set("fenced_frame_url", child_frame_target);
+
+  const urn = await generateURNFromFledge(source_url, []);
+  attachFencedFrame(urn);
+
+  // The grandchild fenced frame writes a value to the server iff it loads
+  // successfully.
+  const result = (expected == FrameTestResult.SUCCESS) ?
+    await nextValueFromServer(frame_loaded_key) :
+    await Promise.race([
+      nextValueFromServer(frame_loaded_key),
+      new Promise((resolve) => {
+        t.step_timeout(() => resolve("timeout"), 10000 /* ms */);
+      }),
+    ]);
 
   assert_equals(result, expected);
 }
