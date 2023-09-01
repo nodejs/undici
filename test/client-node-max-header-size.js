f245be8764efe41d5e94b2c9fa8ff4245b6ba3f7
Balázs Orbán <info@balazsorban.com>
2023-09-01 13:18:21 +0200

    fix test

--- test/client-node-max-header-size.js
+++ test/client-node-max-header-size.js
@@ -1,7 +1,7 @@
 const { execSync } = require('node:child_process')
 const { test } = require('tap')
 
-const command = 'node -e "require(`./index-fetch`);fetch(`https://httpbin.org/get`)"'
+const command = 'node -e "require(\'./index-fetch\');fetch(\'https://httpbin.org/get\')"'
 
 test("respect Node.js' --max-http-header-size", async (t) => {
   t.throws(
