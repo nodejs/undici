a0eb07a0b54e5cfa88787d948a86ce7e58864675
Balázs Orbán <info@balazsorban.com>
2023-09-01 13:16:42 +0200

    cleaner test

--- test/client-node-max-header-size.js
+++ test/client-node-max-header-size.js
@@ -1,16 +1,20 @@
-const { execSync } = require("node:child_process");
-const { test } = require("tap");
+const { execSync } = require('node:child_process')
+const { test } = require('tap')
 
-test("Respect --max-http-header-size Node.js flag", (t) => {
-  const command =
-    'node -e "require(`../undici-fetch`);fetch(`https://httpbin.org/get`)"';
-  try {
-    execSync(`${command} --max-http-header-size=1}`, { cwd: __dirname });
-  } catch (error) {
-    t.same(error.message.includes("UND_ERR_HEADERS_OVERFLOW"), true);
-  }
+const command = 'node -e "require(`./index-fetch`);fetch(`https://httpbin.org/get`)"'
 
-  execSync(command, { cwd: __dirname });
+test("respect Node.js' --max-http-header-size", async (t) => {
+  t.throws(
+    () => execSync(`${command} --max-http-header-size=1`),
+    /UND_ERR_HEADERS_OVERFLOW/,
+    'max-http-header-size=1 should throw'
+  )
 
-  t.end();
-});
+  t.doesNotThrow(
+    () => execSync(command),
+    /UND_ERR_HEADERS_OVERFLOW/,
+    'default max-http-header-size should not throw'
+  )
+
+  t.end()
+})
