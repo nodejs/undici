417ef095ebe0f364fc2905098d430f9213821b2b
Balázs Orbán <info@balazsorban.com>
2023-09-05 14:10:16 +0200

    add test under `test/fetch`

---
+++ test/fetch/client-node-max-header-size.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { execSync } = require('node:child_process')
+const { test } = require('tap')
+
+const command = 'node -e "require(\'.\').fetch(\'https://httpbin.org/get\')"'
+
+test("respect Node.js' --max-http-header-size", async (t) => {
+
+
+  t.throws(
+    () => execSync(`${command} --max-http-header-size=1`),
+    /UND_ERR_HEADERS_OVERFLOW/,
+    'max-http-header-size=1 should throw'
+  )
+
+  t.doesNotThrow(
+    () => execSync(command),
+    /UND_ERR_HEADERS_OVERFLOW/,
+    'default max-http-header-size should not throw'
+  )
+
+  t.end()
+})
