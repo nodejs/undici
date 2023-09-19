db889883ce29c81502654bab2ac74ff129b9d685
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-09-19 11:09:01 -0600

    use postpublish script

--- scripts/publish-types.js
+++ scripts/publish-types.js
@@ -2,6 +2,12 @@ const fs = require('node:fs')
 const path = require('node:path')
 const childProcess = require('node:child_process')
 
+const otp = process.env.npm_config_otp;
+
+if (!otp) {
+  throw new Error('Missing otp');
+}
+
 const packageJSONPath = path.join(__dirname, '..', 'package.json')
 const packageJSONRaw = fs.readFileSync(packageJSONPath, 'utf-8')
 const packageJSON = JSON.parse(packageJSONRaw)
@@ -17,7 +23,8 @@ packageTypesJSON.version = packageJSON.version
 fs.writeFileSync(packageJSONPath, JSON.stringify(packageTypesJSON, null, 2))
 
 try {
-  childProcess.execSync('npm publish', { cwd: path.join(__dirname, '..') })
+  console.log('publishing!')
+  // childProcess.execSync(`npm publish --otp=${otp}`, { cwd: path.join(__dirname, '..') })
 } finally {
   fs.rmSync(packageJSONPath)
   fs.renameSync(temporaryRename, packageJSONPath)
