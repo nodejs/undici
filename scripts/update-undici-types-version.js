5c2e597bd938ce99a0246f9dfbfac6d3f3a31be9
Ethan Arrowood <ethan@arrowood.dev>
2023-09-20 15:02:42 -0600

    Add publish types script (#2273)
    * add publish types script
    
    * use postpublish script
    
    * 5.24.0-test.0
    
    * 5.24.0-test.1
    
    * uncomment
    
    * 5.24.0-test.2
    
    * simplify automation
    
    * 5.24.0-test.3
    
    * fix script
    
    * 5.24.0-test.4
    
    * fix script
    
    * 5.24.0-test.5
    
    * undici-type@5.24.0-test.5
    
    * restore versions
    
    * delete generated file
    
    * Update update-undici-types-version.js
    
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
    
    * fix contributor list and fix types export
    
    * this should fix types
    
    * one more adjustment
    
    * 5.24.0-test.6
    
    * undici-types@5.24.0-test.6
    
    * revert versions and add README for undici-types
    
    ---------
    
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>

---
+++ scripts/update-undici-types-version.js
@@ -0,0 +1,18 @@
+const fs = require('node:fs')
+const path = require('node:path')
+const childProcess = require('node:child_process')
+
+const packageJSONPath = path.join(__dirname, '..', 'package.json')
+const packageJSONRaw = fs.readFileSync(packageJSONPath, 'utf-8')
+const packageJSON = JSON.parse(packageJSONRaw)
+const version = packageJSON.version
+
+const packageTypesJSONPath = path.join(__dirname, '..', 'types', 'package.json')
+const packageTypesJSONRaw = fs.readFileSync(packageTypesJSONPath, 'utf-8')
+const packageTypesJSON = JSON.parse(packageTypesJSONRaw)
+packageTypesJSON.version = version
+
+fs.writeFileSync(packageTypesJSONPath, JSON.stringify(packageTypesJSON, null, 2))
+
+childProcess.execSync('git add types/package.json', { cwd: path.join(__dirname, '..') })
+childProcess.execSync(`git commit -n -m 'undici-types@${version}'`, { cwd: path.join(__dirname, '..') })
