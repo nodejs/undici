ce4a364dd82d46afcf1b0a20ad23e236dd38ad40
Carlos Fuentes <me@metcoder.dev>
2023-10-06 11:06:09 +0200

    fix(#2311): End stream after body sent

--- test/fetch/http2.js
+++ test/fetch/http2.js
@@ -13,7 +13,61 @@ const { Client, fetch } = require('../..')
 
 const nodeVersion = Number(process.version.split('v')[1].split('.')[0])
 
-plan(5)
+plan(6)
+
+test('[Fetch] Issue#2311', async t => {
+  const expectedBody = 'hello from client!'
+
+  const server = createSecureServer(pem, async (req, res) => {
+    let body = ''
+
+    req.setEncoding('utf8')
+
+    res.writeHead(200, {
+      'content-type': 'text/plain; charset=utf-8',
+      'x-custom-h2': req.headers['x-my-header']
+    })
+
+    for await (const chunk of req) {
+      body += chunk
+    }
+
+    res.end(body)
+  })
+
+  t.plan(1)
+
+  server.listen()
+  await once(server, 'listening')
+
+  const client = new Client(`https://localhost:${server.address().port}`, {
+    connect: {
+      rejectUnauthorized: false
+    },
+    allowH2: true
+  })
+
+  const response = await fetch(
+    `https://localhost:${server.address().port}/`,
+    // Needs to be passed to disable the reject unauthorized
+    {
+      method: 'POST',
+      dispatcher: client,
+      headers: {
+        'x-my-header': 'foo',
+        'content-type': 'text-plain'
+      },
+      body: expectedBody
+    }
+  )
+
+  const responseBody = await response.text()
+
+  t.teardown(server.close.bind(server))
+  t.teardown(client.close.bind(client))
+
+  t.equal(responseBody, expectedBody)
+})
 
 test('[Fetch] Simple GET with h2', async t => {
   const server = createSecureServer(pem)
@@ -119,68 +173,72 @@ test('[Fetch] Should handle h2 request with body (string or buffer)', async t =>
 })
 
 // Skipping for now, there is something odd in the way the body is handled
-test('[Fetch] Should handle h2 request with body (stream)', { skip: nodeVersion === 16 }, async t => {
-  const server = createSecureServer(pem)
-  const expectedBody = readFileSync(__filename, 'utf-8')
-  const stream = createReadStream(__filename)
-  const requestChunks = []
+test(
+  '[Fetch] Should handle h2 request with body (stream)',
+  { skip: nodeVersion === 16 },
+  async t => {
+    const server = createSecureServer(pem)
+    const expectedBody = readFileSync(__filename, 'utf-8')
+    const stream = createReadStream(__filename)
+    const requestChunks = []
 
-  server.on('stream', async (stream, headers) => {
-    t.equal(headers[':method'], 'PUT')
-    t.equal(headers[':path'], '/')
-    t.equal(headers[':scheme'], 'https')
+    server.on('stream', async (stream, headers) => {
+      t.equal(headers[':method'], 'PUT')
+      t.equal(headers[':path'], '/')
+      t.equal(headers[':scheme'], 'https')
 
-    stream.respond({
-      'content-type': 'text/plain; charset=utf-8',
-      'x-custom-h2': headers['x-my-header'],
-      ':status': 200
+      stream.respond({
+        'content-type': 'text/plain; charset=utf-8',
+        'x-custom-h2': headers['x-my-header'],
+        ':status': 200
+      })
+
+      stream.end('hello h2!')
+
+      for await (const chunk of stream) {
+        requestChunks.push(chunk)
+      }
     })
 
-    stream.end('hello h2!')
+    t.plan(8)
 
-    for await (const chunk of stream) {
-      requestChunks.push(chunk)
-    }
-  })
+    server.listen(0)
+    await once(server, 'listening')
 
-  t.plan(8)
-
-  server.listen(0)
-  await once(server, 'listening')
-
-  const client = new Client(`https://localhost:${server.address().port}`, {
-    connect: {
-      rejectUnauthorized: false
-    },
-    allowH2: true
-  })
-
-  t.teardown(server.close.bind(server))
-  t.teardown(client.close.bind(client))
-
-  const response = await fetch(
-    `https://localhost:${server.address().port}/`,
-    // Needs to be passed to disable the reject unauthorized
-    {
-      method: 'PUT',
-      dispatcher: client,
-      headers: {
-        'x-my-header': 'foo',
-        'content-type': 'text-plain'
+    const client = new Client(`https://localhost:${server.address().port}`, {
+      connect: {
+        rejectUnauthorized: false
       },
-      body: Readable.toWeb(stream),
-      duplex: 'half'
-    }
-  )
+      allowH2: true
+    })
 
-  const responseBody = await response.text()
+    t.teardown(server.close.bind(server))
+    t.teardown(client.close.bind(client))
 
-  t.equal(response.status, 200)
-  t.equal(response.headers.get('content-type'), 'text/plain; charset=utf-8')
-  t.equal(response.headers.get('x-custom-h2'), 'foo')
-  t.equal(responseBody, 'hello h2!')
-  t.equal(Buffer.concat(requestChunks).toString('utf-8'), expectedBody)
-})
+    const response = await fetch(
+      `https://localhost:${server.address().port}/`,
+      // Needs to be passed to disable the reject unauthorized
+      {
+        method: 'PUT',
+        dispatcher: client,
+        headers: {
+          'x-my-header': 'foo',
+          'content-type': 'text-plain'
+        },
+        body: Readable.toWeb(stream),
+        duplex: 'half'
+      }
+    )
+
+    const responseBody = await response.text()
+
+    t.equal(response.status, 200)
+    t.equal(response.headers.get('content-type'), 'text/plain; charset=utf-8')
+    t.equal(response.headers.get('x-custom-h2'), 'foo')
+    t.equal(responseBody, 'hello h2!')
+    t.equal(Buffer.concat(requestChunks).toString('utf-8'), expectedBody)
+  }
+)
 test('Should handle h2 request with body (Blob)', { skip: !Blob }, async t => {
   const server = createSecureServer(pem)
   const expectedBody = 'asd'
