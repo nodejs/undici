a237f467fad2ba38a2063f14aaab5f2731ba0fcd
Filip Skokan <panva.ip@gmail.com>
2023-10-17 10:49:56 +0200

    test: only include WebSocket in WPT Report where it's landed

--- test/wpt/start-websockets.mjs
+++ test/wpt/start-websockets.mjs
@@ -3,8 +3,17 @@ import { join } from 'path'
 import { fileURLToPath } from 'url'
 import { fork } from 'child_process'
 import { on } from 'events'
+import options from 'internal/options'
 
 const { WPT_REPORT } = process.env
+
+function isGlobalAvailable () {
+  if (typeof WebSocket !== 'undefined') {
+    return true
+  }
+
+  return typeof options.getOptionValue('--experimental-websocket') === 'boolean'
+}
 
 if (process.env.CI) {
   // TODO(@KhafraDev): figure out *why* these tests are flaky in the CI.
@@ -22,7 +31,7 @@ child.on('exit', (code) => process.exit(code))
 for await (const [message] of on(child, 'message')) {
   if (message.server) {
     const runner = new WPTRunner('websockets', message.server, {
-      appendReport: !!WPT_REPORT,
+      appendReport: !!WPT_REPORT && isGlobalAvailable(),
       reportPath: WPT_REPORT
     })
     runner.run()
