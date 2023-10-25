476ce9b2143ea614b94275e75417dfa8d8fae7ad
Khafra <maitken033380023@gmail.com>
2023-10-25 14:43:07 -0400

    fix

--- test/fetch/user-agent.js
+++ test/fetch/user-agent.js
@@ -4,13 +4,14 @@ const { test, skip } = require('tap')
 const events = require('events')
 const http = require('http')
 const undici = require('../../')
-const nodeBuild = require('../../undici-fetch.js')
 const { nodeMajor } = require('../../lib/core/util')
 
 if (nodeMajor === 16) {
   skip('esbuild uses static blocks with --keep-names which node 16.8 does not have')
   process.exit()
 }
+
+const nodeBuild = require('../../undici-fetch.js')
 
 test('user-agent defaults correctly', async (t) => {
   const server = http.createServer((req, res) => {
