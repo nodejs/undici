5a159cc0207a3705da507afca7b85d948354eba1
Carlos Fuentes <me@metcoder.dev>
2023-10-27 10:37:31 +0200

    feat: add support for weak etag check

--- test/retry-handler.js
+++ test/retry-handler.js
@@ -6,9 +6,6 @@ const tap = require('tap')
 
 const { RetryHandler, Client } = require('..')
 
-// TODO: rewrite tests to not use explicit Promise handling
-// TODO: add tests for retry-after on 429
-
 tap.test('Should retry status code', t => {
   let counter = 0
   const chunks = []
@@ -635,6 +632,101 @@ tap.test('Should handle 206 partial content', t => {
         retry: function (err) {
           counter++
 
+          if (err.code && err.code === 'UND_ERR_DESTROYED') {
+            return null
+          }
+
+          return err.statusCode === 206 ? null : 800
+        }
+      }
+    )
+
+    client.dispatch(
+      {
+        method: 'GET',
+        path: '/',
+        headers: {
+          'content-type': 'application/json'
+        }
+      },
+      handler
+    )
+
+    t.teardown(async () => {
+      await client.close()
+
+      server.close()
+      await once(server, 'close')
+    })
+  })
+})
+tap.test('Should handle 206 partial content - bad-etag', { only: true }, t => {
+  const chunks = []
+
+  // Took from: https://github.com/nxtedition/nxt-lib/blob/4b001ebc2f22cf735a398f35ff800dd553fe5933/test/undici/retry.js#L47
+  let x = 0
+  const server = createServer((req, res) => {
+    if (x === 0) {
+      t.pass()
+      res.setHeader('etag', 'asd')
+      res.write('abc')
+      setTimeout(() => {
+        res.destroy()
+      }, 1e2)
+    } else if (x === 1) {
+      t.same(req.headers.range, 'bytes=3-')
+      res.setHeader('content-range', 'bytes 3-6/6')
+      res.setHeader('etag', 'erwsd')
+      res.statusCode = 206
+      res.end('def')
+    }
+    x++
+  })
+
+  const dispatchOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  t.plan(6)
+
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`)
+    const handler = new RetryHandler(
+      dispatchOptions,
+      {
+        dispatch: (...args) => {
+          return client.dispatch(...args)
+        },
+        handler: {
+          onConnect () {
+            t.pass()
+          },
+          onBodySent () {
+            t.pass()
+          },
+          onHeaders (status, _rawHeaders, resume, _statusMessage) {
+            t.pass()
+            return true
+          },
+          onData (chunk) {
+            chunks.push(chunk)
+            return true
+          },
+          onComplete () {
+            t.error('should not complete')
+          },
+          onError (err) {
+            t.equal(Buffer.concat(chunks).toString('utf-8'), 'abc')
+            t.equal(err.code, 'UND_ERR_REQ_RETRY')
+          }
+        }
+      },
+      {
+        retry: function (err) {
           if (err.code && err.code === 'UND_ERR_DESTROYED') {
             return null
           }
