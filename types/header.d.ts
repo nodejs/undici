46b98d3cf6395ebdf4408200599b3178aa652aab
tsctx <91457664+tsctx@users.noreply.github.com>
2023-11-13 17:39:13 +0900

    fix: type

--- types/header.d.ts
+++ types/header.d.ts
@@ -6,4 +6,4 @@ export type IncomingHttpHeaders = Record<string, string | string[] | undefined>;
 /**
  * The h2 pseudo-header type declaration of `undici`.
  */
-export type IncomingH2PseudoHeaders = Record<':status', number | undefined> & Record<`:${string}`, string | string[] | undefined>;
+export type IncomingH2PseudoHeaders = Record<':status', number | undefined> | Record<`:${string}`, string | number | string[] | undefined>;
