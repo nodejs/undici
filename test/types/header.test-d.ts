8bd96556d90b722de590acb401cd4b524de33f70
tsctx <91457664+tsctx@users.noreply.github.com>
2023-11-13 17:41:25 +0900

    test: typescript type

--- test/types/header.test-d.ts
+++ test/types/header.test-d.ts
@@ -1,13 +1,22 @@
 import { IncomingHttpHeaders as CoreIncomingHttpHeaders } from "http";
 import { expectAssignable, expectNotAssignable } from "tsd";
-import { IncomingHttpHeaders } from "../../types/header";
+import {
+  IncomingHttpHeaders,
+  IncomingH2PseudoHeaders,
+} from "../../types/header";
 
 const headers = {
   authorization: undefined,
   ["content-type"]: "application/json",
 } satisfies CoreIncomingHttpHeaders;
 
+const pseudoHeaders = {
+  ":status": 200,
+} satisfies IncomingH2PseudoHeaders;
+
 expectAssignable<IncomingHttpHeaders>(headers);
+
+expectAssignable<IncomingH2PseudoHeaders>(pseudoHeaders);
 
 // It is why we do not need to add ` | null` to `IncomingHttpHeaders`:
 expectNotAssignable<CoreIncomingHttpHeaders>({
