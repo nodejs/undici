6f4d97beea2f099178ea049e10b592d25ea94f8b
Robert Nagy <ronagy@icloud.com>
2023-12-06 09:18:21 +0100

    fix: stream error timings (#2497)

--- test/readable.test.js
+++ test/readable.test.js
@@ -21,3 +21,40 @@ test('avoid body reordering', async function (t) {
 
   t.equal(text, 'helloworld')
 })
+
+test('destroy timing text', async function (t) {
+  t.plan(1)
+
+  function resume () {
+  }
+  function abort () {
+  }
+  const _err = new Error('kaboom')
+  const r = new Readable({ resume, abort })
+  r.destroy(_err)
+  try {
+    await r.text()
+  } catch (err) {
+    t.same(err, _err)
+  }
+})
+
+test('destroy timing promise', async function (t) {
+  t.plan(1)
+
+  function resume () {
+  }
+  function abort () {
+  }
+  const r = await new Promise(resolve => {
+    const r = new Readable({ resume, abort })
+    r.destroy(new Error('kaboom'))
+    resolve(r)
+  })
+  await new Promise(resolve => {
+    r.on('error', err => {
+      t.ok(err)
+      resolve(null)
+    })
+  })
+})
