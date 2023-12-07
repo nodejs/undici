bd17e9561b85d869ab8feef7933d73cfe32b0a84
tsctx <91457664+tsctx@users.noreply.github.com>
2023-12-08 06:54:43 +0900

    suggested change

--- test/tree.js
+++ test/tree.js
@@ -1,35 +1,38 @@
+'use strict'
+
 const { TernarySearchTree } = require('../lib/core/tree')
 const { test } = require('tap')
 
-function generateAsciiString (length) {
-  let result = ''
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  const charactersLength = characters.length
-  for (let i = 0; i < length; ++i) {
-    result += characters[Math.floor(Math.random() * charactersLength)]
+test('Ternary Search Tree', (t) => {
+  t.plan(1)
+  function generateAsciiString (length) {
+    let result = ''
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < length; ++i) {
+      result += characters[Math.floor(Math.random() * charactersLength)]
+    }
+    return result
   }
-  return result
-}
+  const tst = new TernarySearchTree()
 
-const tst = new TernarySearchTree()
+  /** @type {string[]} */
+  const random = new Array(5000)
+    .fill(0)
+    .map(() => generateAsciiString((Math.random() * 100 + 5) | 0))
+  const randomBuffer = random.map((c) => Buffer.from(c))
 
-/** @type {string[]} */
-const random = new Array(5000)
-  .fill(0)
-  .map(() => generateAsciiString((Math.random() * 100 + 5) | 0))
-const randomBuffer = random.map((c) => Buffer.from(c))
-
-for (let i = 0; i < random.length; ++i) {
-  const key = random[i]
-  const lowerCasedKey = key.toLowerCase()
-  const buffer = Buffer.from(lowerCasedKey)
-  tst.insert(buffer, lowerCasedKey)
-}
-
-test('all', (t) => {
-  for (let i = 0; i < randomBuffer.length; ++i) {
-    t.equal(tst.lookup(randomBuffer[i]), random[i].toLowerCase())
+  for (let i = 0; i < random.length; ++i) {
+    const key = random[i]
+    const lowerCasedKey = key.toLowerCase()
+    const buffer = Buffer.from(lowerCasedKey)
+    tst.insert(buffer, lowerCasedKey)
   }
-  t.end()
+  t.test('all', (t) => {
+    for (let i = 0; i < randomBuffer.length; ++i) {
+      t.equal(tst.lookup(randomBuffer[i]), random[i].toLowerCase())
+    }
+    t.end()
+  })
 })
