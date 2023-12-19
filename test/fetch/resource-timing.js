871baa74ec326518a3de3ee4591459e25617402e
Torstein Bjørnstad <tosh@tosh.no>
2023-12-19 20:31:09 +0100

    feat: Add resource timing entries for connection, request and response (#2481)

--- test/fetch/resource-timing.js
+++ test/fetch/resource-timing.js
@@ -70,3 +70,69 @@ test('should include encodedBodySize in performance entry', { skip }, (t) => {
 
   t.teardown(server.close.bind(server))
 })
+
+test('timing entries should be in order', { skip }, (t) => {
+  t.plan(13)
+  const obs = new PerformanceObserver(list => {
+    const [entry] = list.getEntries()
+
+    t.ok(entry.startTime > 0)
+    t.ok(entry.fetchStart >= entry.startTime)
+    t.ok(entry.domainLookupStart >= entry.fetchStart)
+    t.ok(entry.domainLookupEnd >= entry.domainLookupStart)
+    t.ok(entry.connectStart >= entry.domainLookupEnd)
+    t.ok(entry.connectEnd >= entry.connectStart)
+    t.ok(entry.requestStart >= entry.connectEnd)
+    t.ok(entry.responseStart >= entry.requestStart)
+    t.ok(entry.responseEnd >= entry.responseStart)
+    t.ok(entry.duration > 0)
+
+    t.ok(entry.redirectStart === 0)
+    t.ok(entry.redirectEnd === 0)
+
+    obs.disconnect()
+    performance.clearResourceTimings()
+  })
+
+  obs.observe({ entryTypes: ['resource'] })
+
+  const server = createServer((req, res) => {
+    res.end('ok')
+  }).listen(0, async () => {
+    const body = await fetch(`http://localhost:${server.address().port}/redirect`)
+    t.strictSame('ok', await body.text())
+  })
+
+  t.teardown(server.close.bind(server))
+})
+
+test('redirect timing entries should be included when redirecting', { skip }, (t) => {
+  t.plan(4)
+  const obs = new PerformanceObserver(list => {
+    const [entry] = list.getEntries()
+
+    t.ok(entry.redirectStart >= entry.startTime)
+    t.ok(entry.redirectEnd >= entry.redirectStart)
+    t.ok(entry.connectStart >= entry.redirectEnd)
+
+    obs.disconnect()
+    performance.clearResourceTimings()
+  })
+
+  obs.observe({ entryTypes: ['resource'] })
+
+  const server = createServer((req, res) => {
+    if (req.url === '/redirect') {
+      res.statusCode = 307
+      res.setHeader('location', '/redirect/')
+      res.end()
+      return
+    }
+    res.end('ok')
+  }).listen(0, async () => {
+    const body = await fetch(`http://localhost:${server.address().port}/redirect`)
+    t.strictSame('ok', await body.text())
+  })
+
+  t.teardown(server.close.bind(server))
+})
