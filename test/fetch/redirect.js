5b0771072e22638c36e94da01d1020e428e452f3
Angel Yan <angelyan96@gmail.com>
2023-12-26 13:22:33 -0300

    fix(fetch): do not abort fetch on redirect

--- test/fetch/redirect.js
+++ test/fetch/redirect.js
@@ -48,3 +48,29 @@ test('Redirecting with an empty body does not throw an error - #2027', async (t)
   t.equal(await resp.text(), '/redirect/')
   t.ok(resp.redirected)
 })
+
+test('Redirecting with a body does not fail to write body - #2543', async (t) => {
+  const server = createServer((req, res) => {
+    if (req.url === '/redirect') {
+      res.writeHead(307, { location: '/target' })
+      res.write('<a href="/redirect/">Moved Permanently</a>')
+      setTimeout(() => res.end(), 500)
+    } else {
+      let body = ''
+      req.on('data', (chunk) => { body += chunk })
+      req.on('end', () => t.equals(body, 'body'))
+      res.write('ok')
+      res.end()
+    }
+  }).listen(0)
+
+  t.teardown(server.close.bind(server))
+  await once(server, 'listening')
+
+  const resp = await fetch(`http://localhost:${server.address().port}/redirect`, {
+    method: 'POST',
+    body: 'body'
+  })
+  t.equal(await resp.text(), 'ok')
+  t.ok(resp.redirected)
+})
