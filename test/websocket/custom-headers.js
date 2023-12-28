f52ae984312d10b77aba7a9684712c82abed3393
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-28 14:45:59 +0100

    fix: custom headers

--- test/websocket/custom-headers.js
+++ test/websocket/custom-headers.js
@@ -13,7 +13,8 @@ test('Setting custom headers', (t) => {
   return new Promise((resolve, reject) => {
     class TestAgent extends Agent {
       dispatch (options) {
-        assert.match(options.headers, headers)
+        assert.deepStrictEqual(options.headers['x-khafra-hello'], headers['x-khafra-hello'])
+        assert.deepStrictEqual(options.headers.Authorization, headers.Authorization)
         resolve()
         return false
       }
