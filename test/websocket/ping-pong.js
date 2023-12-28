b2f51e76ec2557cf17583a19c1add0b59b3185c2
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-28 12:22:12 +0100

    fix: ping pong

--- test/websocket/ping-pong.js
+++ test/websocket/ping-pong.js
@@ -1,43 +1,47 @@
 'use strict'
 
-const { test, after } = require('node:test')
+const { test } = require('node:test')
 const assert = require('node:assert')
 const { WebSocketServer } = require('ws')
 const diagnosticsChannel = require('diagnostics_channel')
 const { WebSocket } = require('../..')
 
 test('Receives ping and parses body', () => {
-  const server = new WebSocketServer({ port: 0 })
+  return new Promise((resolve, reject) => {
+    const server = new WebSocketServer({ port: 0 })
 
-  server.on('connection', (ws) => {
-    ws.ping('Hello, world')
-  })
+    server.on('connection', (ws) => {
+      ws.ping('Hello, world')
+    })
 
-  after(server.close.bind(server))
+    const ws = new WebSocket(`ws://localhost:${server.address().port}`)
+    ws.onerror = ws.onmessage = reject
 
-  const ws = new WebSocket(`ws://localhost:${server.address().port}`)
-  ws.onerror = ws.onmessage = t.fail
-
-  diagnosticsChannel.channel('undici:websocket:ping').subscribe(({ payload }) => {
-    assert.deepStrictEqual(payload, Buffer.from('Hello, world'))
-    ws.close()
+    diagnosticsChannel.channel('undici:websocket:ping').subscribe(({ payload }) => {
+      assert.deepStrictEqual(payload, Buffer.from('Hello, world'))
+      ws.close()
+      server.close()
+      resolve()
+    })
   })
 })
 
 test('Receives pong and parses body', () => {
-  const server = new WebSocketServer({ port: 0 })
+  return new Promise((resolve, reject) => {
+    const server = new WebSocketServer({ port: 0 })
 
-  server.on('connection', (ws) => {
-    ws.pong('Pong')
-  })
+    server.on('connection', (ws) => {
+      ws.pong('Pong')
+    })
 
-  after(server.close.bind(server))
+    const ws = new WebSocket(`ws://localhost:${server.address().port}`)
+    ws.onerror = ws.onmessage = reject
 
-  const ws = new WebSocket(`ws://localhost:${server.address().port}`)
-  ws.onerror = ws.onmessage = t.fail
-
-  diagnosticsChannel.channel('undici:websocket:pong').subscribe(({ payload }) => {
-    assert.deepStrictEqual(payload, Buffer.from('Pong'))
-    ws.close()
+    diagnosticsChannel.channel('undici:websocket:pong').subscribe(({ payload }) => {
+      assert.deepStrictEqual(payload, Buffer.from('Pong'))
+      server.close()
+      ws.close()
+      resolve()
+    })
   })
 })
