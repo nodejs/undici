007872f83ae5f926618b97ae00f28631da04d986
Antoine Gomez <antoine@gomez.lu>
2023-12-29 21:11:20 +0000

    apply comments

--- test/dns-resolver.js
+++ test/dns-resolver.js
@@ -1,3 +1,33 @@
+'use strict'
+
+// source: https://raw.githubusercontent.com/szmarczak/cacheable-lookup/9e60c9f6e74a003692aec68f3ddad93afe613b8f/tests/test.mjs
+
+/**
+
+MIT License
+
+Copyright (c) 2019 Szymon Marczak
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 const { promises: dnsPromises, V4MAPPED, ADDRCONFIG, ALL } = require('node:dns')
 const { promisify } = require('node:util')
 const http = require('node:http')
@@ -856,9 +886,11 @@ test('returns IPv6 if no other entries available', async (t) => {
 
 test('throws when no internet connection', async (t) => {
   const cacheable = new DNSResolver({ resolver })
-
   await t.rejects(cacheable.lookupAsync('econnrefused'), {
-    code: 'ECONNREFUSED'
+    errors: [
+      { code: 'ECONNREFUSED' },
+      { code: 'ECONNREFUSED' }
+    ]
   })
 })
 
