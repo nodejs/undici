5b0d4019be3d9bfb82518077c43fff4f7173ffa7
Chemi Atlow <chemi@atlow.co.il>
2024-01-08 01:08:47 +0200

    fix: non-object error in abort throws bad error (#2597)

--- test/fetch/issue-2242.js
+++ test/fetch/issue-2242.js
@@ -3,10 +3,20 @@
 const { test } = require('node:test')
 const assert = require('node:assert')
 const { fetch } = require('../..')
+const nodeFetch = require('../../index-fetch')
 
 test('fetch with signal already aborted', async () => {
   await assert.rejects(
     fetch('http://localhost', {
+      signal: AbortSignal.abort('Already aborted')
+    }),
+    /Already aborted/
+  )
+})
+
+test('fetch with signal already aborted (from index-fetch)', async () => {
+  await assert.rejects(
+    nodeFetch.fetch('http://localhost', {
       signal: AbortSignal.abort('Already aborted')
     }),
     /Already aborted/
