df64d93187f70f47595409e552e2f6774d521c27
Matteo Collina <hello@matteocollina.com>
2024-01-19 14:25:14 +0100

    Use correct http Agent for node-fetch, axios, got and request
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -66,6 +66,27 @@ const httpKeepAliveOptions = {
   })
 }
 
+const axiosAgent = new http.Agent({
+  keepAlive: true,
+  maxSockets: connections
+})
+
+
+const fetchAgent = new http.Agent({
+  keepAlive: true,
+  maxSockets: connections
+})
+
+const gotAgent = new http.Agent({
+  keepAlive: true,
+  maxSockets: connections
+})
+
+const requestAgent = new http.Agent({
+  keepAlive: true,
+  maxSockets: connections
+})
+
 const undiciOptions = {
   path: '/',
   method: 'GET',
@@ -280,7 +301,7 @@ if (process.env.PORT) {
 
   experiments['node-fetch'] = () => {
     return makeParallelRequests(resolve => {
-      nodeFetch(dest.url).then(res => {
+      nodeFetch(dest.url, { agent: fetchAgent }).then(res => {
         res.body.pipe(new Writable({
           write (chunk, encoding, callback) {
             callback()
@@ -292,7 +313,7 @@ if (process.env.PORT) {
 
   experiments.axios = () => {
     return makeParallelRequests(resolve => {
-      axios.get(dest.url, { responseType: 'stream' }).then(res => {
+      axios.get(dest.url, { responseType: 'stream', httpAgent: axiosAgent }).then(res => {
         res.data.pipe(new Writable({
           write (chunk, encoding, callback) {
             callback()
@@ -304,7 +325,7 @@ if (process.env.PORT) {
 
   experiments.got = () => {
     return makeParallelRequests(resolve => {
-      got.get(dest.url).then(res => {
+      got.get(dest.url, null, { http: gotAgent }).then(res => {
         res.pipe(new Writable({
           write (chunk, encoding, callback) {
             callback()
@@ -316,7 +337,7 @@ if (process.env.PORT) {
 
   experiments.request = () => {
     return makeParallelRequests(resolve => {
-      request(dest.url).then(res => {
+      request(dest.url, { agent: requestAgent }).then(res => {
         res.pipe(new Writable({
           write (chunk, encoding, callback) {
             callback()
