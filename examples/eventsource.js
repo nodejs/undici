4b18785b39f666d4bdd4b29745437f1cbef067f3
uzlopak <aras.abbasi@googlemail.com>
2024-01-20 17:25:26 +0100

    improve example for eventsource

--- examples/eventsource.js
+++ examples/eventsource.js
@@ -1,17 +1,20 @@
 'use strict'
 
+const { randomBytes } = require('crypto')
 const { EventSource } = require('../')
 
 async function main () {
-  const ev = new EventSource('https://smee.io/wcGp009TievZCLT')
-  ev.onmessage = (event) => {
-    console.log(event)
-  }
-  ev.onerror = event => {
-    console.log(event)
-  }
-  ev.onopen = event => {
-    console.log(event)
-  }
+  const url = `https://smee.io/${randomBytes(8).toString('base64url')}`
+  console.log(`Connecting to event source server ${url}}`)
+  const ev = new EventSource(url)
+  ev.onmessage = console.log
+  ev.onerror = console.log
+  ev.onopen = console.log
+
+  // Special event of smee.io
+  ev.addEventListener('ready', console.log)
+
+  // Ping event is sent every 30 seconds by smee.io
+  ev.addEventListener('ping', console.log)
 }
 main()
