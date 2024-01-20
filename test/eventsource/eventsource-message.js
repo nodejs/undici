7fd4a6489a1aaae5a8f2098d0f36ff1ec42eed79
uzlopak <aras.abbasi@googlemail.com>
2024-01-20 06:44:08 +0100

    more tests

--- test/eventsource/eventsource-message.js
+++ test/eventsource/eventsource-message.js
@@ -168,6 +168,154 @@ describe('EventSource - message', () => {
     await finishedPromise.promise
   })
 
+  test('Should emit a message event if data as a field is provided', async () => {
+    const finishedPromise = {
+      promise: undefined,
+      resolve: undefined,
+      reject: undefined
+    }
+
+    finishedPromise.promise = new Promise((resolve, reject) => {
+      finishedPromise.resolve = resolve
+      finishedPromise.reject = reject
+    })
+
+    const server = http.createServer(async (req, res) => {
+      res.writeHead(200, 'OK', { 'Content-Type': 'text/event-stream' })
+      res.write('data\n\n')
+      await setTimeout(100)
+
+      res.end()
+    })
+
+    server.listen(0)
+
+    await events.once(server, 'listening')
+    const port = server.address().port
+
+    const eventSourceInstance = new EventSource(`http://localhost:${port}`)
+    eventSourceInstance.addEventListener('message', () => {
+      finishedPromise.resolve()
+      eventSourceInstance.close()
+      server.close()
+    })
+
+    await setTimeout(500)
+
+    await finishedPromise.promise
+  })
+
+  test('Should emit a custom message event if data is empty', async () => {
+    const finishedPromise = {
+      promise: undefined,
+      resolve: undefined,
+      reject: undefined
+    }
+
+    finishedPromise.promise = new Promise((resolve, reject) => {
+      finishedPromise.resolve = resolve
+      finishedPromise.reject = reject
+    })
+
+    const server = http.createServer(async (req, res) => {
+      res.writeHead(200, 'OK', { 'Content-Type': 'text/event-stream' })
+      res.write('event:custom\ndata:\n\n')
+      await setTimeout(100)
+
+      res.end()
+    })
+
+    server.listen(0)
+
+    await events.once(server, 'listening')
+    const port = server.address().port
+
+    const eventSourceInstance = new EventSource(`http://localhost:${port}`)
+    eventSourceInstance.addEventListener('custom', () => {
+      finishedPromise.resolve()
+      eventSourceInstance.close()
+      server.close()
+    })
+
+    await setTimeout(500)
+
+    await finishedPromise.promise
+  })
+
+  test('Should emit a message event if data is empty', async () => {
+    const finishedPromise = {
+      promise: undefined,
+      resolve: undefined,
+      reject: undefined
+    }
+
+    finishedPromise.promise = new Promise((resolve, reject) => {
+      finishedPromise.resolve = resolve
+      finishedPromise.reject = reject
+    })
+
+    const server = http.createServer(async (req, res) => {
+      res.writeHead(200, 'OK', { 'Content-Type': 'text/event-stream' })
+      res.write('data:\n\n')
+      await setTimeout(100)
+
+      res.end()
+    })
+
+    server.listen(0)
+
+    await events.once(server, 'listening')
+    const port = server.address().port
+
+    const eventSourceInstance = new EventSource(`http://localhost:${port}`)
+    eventSourceInstance.addEventListener('message', () => {
+      finishedPromise.resolve()
+      eventSourceInstance.close()
+      server.close()
+    })
+
+    await setTimeout(500)
+
+    await finishedPromise.promise
+  })
+
+  test('Should emit a custom message event if data only as a field is provided', async () => {
+    const finishedPromise = {
+      promise: undefined,
+      resolve: undefined,
+      reject: undefined
+    }
+
+    finishedPromise.promise = new Promise((resolve, reject) => {
+      finishedPromise.resolve = resolve
+      finishedPromise.reject = reject
+    })
+
+    const server = http.createServer(async (req, res) => {
+      res.writeHead(200, 'OK', { 'Content-Type': 'text/event-stream' })
+      res.write('event:custom\ndata\n\n')
+      await setTimeout(100)
+
+      res.end()
+    })
+
+    server.listen(0)
+
+    await events.once(server, 'listening')
+    const port = server.address().port
+
+    const eventSourceInstance = new EventSource(`http://localhost:${port}`)
+    eventSourceInstance.addEventListener('custom', () => {
+      finishedPromise.resolve()
+      eventSourceInstance.close()
+      server.close()
+    })
+
+    await setTimeout(500)
+
+    await finishedPromise.promise
+  })
+
   test('Should not emit a custom type message if no data is provided', async () => {
     const finishedPromise = {
       promise: undefined,
