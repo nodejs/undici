6514881663b79caa92d9c4df2c765fc4ee33549c
uzlopak <aras.abbasi@googlemail.com>
2024-01-27 12:08:13 +0100

    fix: reduce noice in test logs

--- test/client-node-max-header-size.js
+++ test/client-node-max-header-size.js
@@ -1,22 +1,21 @@
 'use strict'
 
 const { execSync } = require('node:child_process')
-const { test } = require('tap')
+const { throws, doesNotThrow } = require('node:assert')
+const { test } = require('node:test')
 
 const command = 'node -e "require(\'.\').request(\'https://httpbin.org/get\')"'
 
-test("respect Node.js' --max-http-header-size", async (t) => {
-  t.throws(
-    () => execSync(`${command} --max-http-header-size=1`),
+test("respect Node.js' --max-http-header-size", () => {
+  throws(
+    () => execSync(`${command} --max-http-header-size=1`, { stdio: 'pipe' }),
     /UND_ERR_HEADERS_OVERFLOW/,
     'max-http-header-size=1 should throw'
   )
 
-  t.doesNotThrow(
+  doesNotThrow(
     () => execSync(command),
     /UND_ERR_HEADERS_OVERFLOW/,
     'default max-http-header-size should not throw'
   )
-
-  t.end()
 })
