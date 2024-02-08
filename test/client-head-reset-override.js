a058f5bac33f38ae1b3b512d60adcd3cba1b969b
uzlopak <aras.abbasi@googlemail.com>
2024-02-09 00:05:15 +0100

    chore: transform tests from tap to native test runner

--- test/client-head-reset-override.js
+++ test/client-head-reset-override.js
@@ -1,62 +1,68 @@
 'use strict'
 
+const { tspl } = require('@matteo.collina/tspl')
+const { once } = require('node:events')
 const { createServer } = require('node:http')
-const { test } = require('tap')
+const { test, after } = require('node:test')
 const { Client } = require('..')
 
-test('override HEAD reset', (t) => {
+test('override HEAD reset', async (t) => {
+  t = tspl(t, { plan: 4 })
+
   const expected = 'testing123'
   const server = createServer((req, res) => {
     if (req.method === 'GET') {
       res.write(expected)
     }
     res.end()
+  }).listen(0)
+
+  after(() => server.close())
+
+  await once(server, 'listening')
+  const client = new Client(`http://localhost:${server.address().port}`)
+  after(() => client.close())
+
+  let done
+  client.on('disconnect', () => {
+    if (!done) {
+      t.fail()
+    }
   })
-  t.teardown(server.close.bind(server))
 
-  server.listen(0, () => {
-    const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.close.bind(client))
+  client.request({
+    path: '/',
+    method: 'HEAD',
+    reset: false
+  }, (err, res) => {
+    t.ifError(err)
+    res.body.resume()
+  })
 
-    let done
-    client.on('disconnect', () => {
-      if (!done) {
-        t.fail()
-      }
-    })
+  client.request({
+    path: '/',
+    method: 'HEAD',
+    reset: false
+  }, (err, res) => {
+    t.ifError(err)
+    res.body.resume()
+  })
 
-    client.request({
-      path: '/',
-      method: 'HEAD',
-      reset: false
-    }, (err, res) => {
-      t.error(err)
-      res.body.resume()
-    })
-
-    client.request({
-      path: '/',
-      method: 'HEAD',
-      reset: false
-    }, (err, res) => {
-      t.error(err)
-      res.body.resume()
-    })
-
-    client.request({
-      path: '/',
-      method: 'GET',
-      reset: false
-    }, (err, res) => {
-      t.error(err)
-      let str = ''
-      res.body.on('data', (data) => {
-        str += data
-      }).on('end', () => {
-        t.same(str, expected)
-        done = true
-        t.end()
-      })
+  client.request({
+    path: '/',
+    method: 'GET',
+    reset: false
+  }, (err, res) => {
+    t.ifError(err)
+    let str = ''
+    res.body.on('data', (data) => {
+      str += data
+    }).on('end', () => {
+      t.strictEqual(str, expected)
+      done = true
+      t.end()
     })
   })
+
+  await t.completed
 })
--- test/issue-1670.js
+++ test/issue-1670.js
@@ -1,12 +1,10 @@
 'use strict'
 
-const { test } = require('tap')
+const { test } = require('node:test')
 const { request } = require('..')
 
-test('https://github.com/mcollina/undici/issues/810', async (t) => {
+test('https://github.com/mcollina/undici/issues/810', async () => {
   const { body } = await request('https://api.github.com/user/emails')
 
   await body.text()
-
-  t.end()
 })
