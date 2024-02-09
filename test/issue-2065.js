4f5b3a8db8aabafee83931796ae6789d15e71154
Khafra <maitken033380023@gmail.com>
2024-02-09 12:38:34 -0500

    fixup

--- test/issue-2065.js
+++ test/issue-2065.js
@@ -4,7 +4,6 @@ const { tspl } = require('@matteo.collina/tspl')
 const { test, after } = require('node:test')
 const { createServer } = require('node:http')
 const { once } = require('node:events')
-const { createReadStream } = require('node:fs')
 const { File, FormData, request } = require('..')
 
 test('undici.request with a FormData body should set content-length header', async (t) => {
@@ -20,48 +19,6 @@ test('undici.request with a FormData body should set content-length header', asy
 
   const body = new FormData()
   body.set('file', new File(['abc'], 'abc.txt'))
-
-  await request(`http://localhost:${server.address().port}`, {
-    method: 'POST',
-    body
-  })
-})
-
-test('undici.request with a FormData stream value should set transfer-encoding header', async (t) => {
-  t = tspl(t, { plan: 1 })
-
-  const server = createServer((req, res) => {
-    t.strictEqual(req.headers['transfer-encoding'], 'chunked')
-    res.end()
-  }).listen(0)
-
-  after(() => server.close())
-  await once(server, 'listening')
-
-  class BlobFromStream {
-    #stream
-    #type
-    constructor (stream, type) {
-      this.#stream = stream
-      this.#type = type
-    }
-
-    stream () {
-      return this.#stream
-    }
-
-    get type () {
-      return this.#type
-    }
-
-    get [Symbol.toStringTag] () {
-      return 'Blob'
-    }
-  }
-
-  const body = new FormData()
-  const fileReadable = createReadStream(__filename)
-  body.set('file', new BlobFromStream(fileReadable, '.js'), 'streamfile')
 
   await request(`http://localhost:${server.address().port}`, {
     method: 'POST',
