7f90f6364900b5b73b56eecec7c7bd2094f4eb34
Aras Abbasi <aras.abbasi@googlemail.com>
2024-02-12 12:01:44 +0100

    chore: migrate a batch of tests to node test runner (#2737)

--- test/headers-as-array.js
+++ test/headers-as-array.js
@@ -1,102 +1,122 @@
 'use strict'
 
-const { test } = require('tap')
+const { tspl } = require('@matteo.collina/tspl')
+const { test, after } = require('node:test')
 const { Client, errors } = require('..')
 const { createServer } = require('node:http')
 
-test('handle headers as array', (t) => {
-  t.plan(1)
+test('handle headers as array', async (t) => {
+  t = tspl(t, { plan: 3 })
   const headers = ['a', '1', 'b', '2', 'c', '3']
 
   const server = createServer((req, res) => {
-    t.match(req.headers, { a: '1', b: '2', c: '3' })
+    t.strictEqual(req.headers.a, '1')
+    t.strictEqual(req.headers.b, '2')
+    t.strictEqual(req.headers.c, '3')
     res.end()
   })
-  t.teardown(server.close.bind(server))
+  after(() => server.close())
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.destroy.bind(client))
+    after(() => client.close())
 
     client.request({
       path: '/',
       method: 'GET',
       headers
-    }, () => {})
+    }, () => { })
   })
+
+  await t.completed
 })
 
-test('handle multi-valued headers as array', (t) => {
-  t.plan(1)
+test('handle multi-valued headers as array', async (t) => {
+  t = tspl(t, { plan: 4 })
   const headers = ['a', '1', 'b', '2', 'c', '3', 'd', '4', 'd', '5']
 
   const server = createServer((req, res) => {
-    t.match(req.headers, { a: '1', b: '2', c: '3', d: '4, 5' })
+    t.strictEqual(req.headers.a, '1')
+    t.strictEqual(req.headers.b, '2')
+    t.strictEqual(req.headers.c, '3')
+    t.strictEqual(req.headers.d, '4, 5')
     res.end()
   })
-  t.teardown(server.close.bind(server))
+  after(() => server.close())
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.destroy.bind(client))
+    after(() => client.close())
 
     client.request({
       path: '/',
       method: 'GET',
       headers
-    }, () => {})
+    }, () => { })
   })
+
+  await t.completed
 })
 
-test('handle headers with array', (t) => {
-  t.plan(1)
+test('handle headers with array', async (t) => {
+  t = tspl(t, { plan: 4 })
   const headers = { a: '1', b: '2', c: '3', d: ['4'] }
 
   const server = createServer((req, res) => {
-    t.match(req.headers, { a: '1', b: '2', c: '3', d: '4' })
+    t.strictEqual(req.headers.a, '1')
+    t.strictEqual(req.headers.b, '2')
+    t.strictEqual(req.headers.c, '3')
+    t.strictEqual(req.headers.d, '4')
     res.end()
   })
-  t.teardown(server.close.bind(server))
+  after(() => server.close())
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.destroy.bind(client))
+    after(() => client.close())
 
     client.request({
       path: '/',
       method: 'GET',
       headers
-    }, () => {})
+    }, () => { })
   })
+
+  await t.completed
 })
 
-test('handle multi-valued headers', (t) => {
-  t.plan(1)
+test('handle multi-valued headers', async (t) => {
+  t = tspl(t, { plan: 4 })
   const headers = { a: '1', b: '2', c: '3', d: ['4', '5'] }
 
   const server = createServer((req, res) => {
-    t.match(req.headers, { a: '1', b: '2', c: '3', d: '4, 5' })
+    t.strictEqual(req.headers.a, '1')
+    t.strictEqual(req.headers.b, '2')
+    t.strictEqual(req.headers.c, '3')
+    t.strictEqual(req.headers.d, '4, 5')
     res.end()
   })
-  t.teardown(server.close.bind(server))
+  after(() => server.close())
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.destroy.bind(client))
+    after(() => client.close())
 
     client.request({
       path: '/',
       method: 'GET',
       headers
-    }, () => {})
+    }, () => { })
   })
+
+  await t.completed
 })
 
-test('fail if headers array is odd', (t) => {
-  t.plan(2)
+test('fail if headers array is odd', async (t) => {
+  t = tspl(t, { plan: 2 })
   const headers = ['a', '1', 'b', '2', 'c', '3', 'd']
 
   const server = createServer((req, res) => { res.end() })
-  t.teardown(server.close.bind(server))
+  after(() => server.close())
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.destroy.bind(client))
+    after(() => client.close())
 
     client.request({
       path: '/',
@@ -104,20 +124,22 @@ test('fail if headers array is odd', (t) => {
       headers
     }, (err) => {
       t.ok(err instanceof errors.InvalidArgumentError)
-      t.equal(err.message, 'headers array must be even')
+      t.strictEqual(err.message, 'headers array must be even')
     })
   })
+
+  await t.completed
 })
 
-test('fail if headers is not an object or an array', (t) => {
-  t.plan(2)
+test('fail if headers is not an object or an array', async (t) => {
+  t = tspl(t, { plan: 2 })
   const headers = 'not an object or an array'
 
   const server = createServer((req, res) => { res.end() })
-  t.teardown(server.close.bind(server))
+  after(() => server.close())
   server.listen(0, () => {
     const client = new Client(`http://localhost:${server.address().port}`)
-    t.teardown(client.destroy.bind(client))
+    after(() => client.close())
 
     client.request({
       path: '/',
@@ -125,7 +147,9 @@ test('fail if headers is not an object or an array', (t) => {
       headers
     }, (err) => {
       t.ok(err instanceof errors.InvalidArgumentError)
-      t.equal(err.message, 'headers must be an object or an array')
+      t.strictEqual(err.message, 'headers must be an object or an array')
     })
   })
+
+  await t.completed
 })
