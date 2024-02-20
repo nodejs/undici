f9679ccbeef894575a37a03ca27774aa77d9c131
uzlopak <aras.abbasi@googlemail.com>
2024-02-20 02:52:08 +0100

    fix wrong description of test

--- test/issue-1670.js
+++ test/issue-1670.js
@@ -3,7 +3,7 @@
 const { test } = require('node:test')
 const { request } = require('..')
 
-test('https://github.com/mcollina/undici/issues/810', async () => {
+test('https://github.com/mcollina/undici/issues/1670', async () => {
   const { body } = await request('https://api.github.com/user/emails')
 
   await body.text()
