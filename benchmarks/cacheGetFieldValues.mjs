007d3171aef64a2bee64cf18539e8eb3a8dfda5a
uzlopak <aras.abbasi@googlemail.com>
2024-02-20 01:59:34 +0100

    chore: improve getFieldValue

---
+++ benchmarks/cacheGetFieldValues.mjs
@@ -0,0 +1,19 @@
+import { bench, group, run } from 'mitata'
+import { getFieldValues } from '../lib/cache/util.js'
+
+const values = [
+  '',
+  'foo',
+  'invälid',
+  'foo, '
+]
+
+group('getFieldValues', () => {
+  bench('getFieldValues', () => {
+    for (let i = 0; i < values.length; ++i) {
+      getFieldValues(values[i])
+    }
+  })
+})
+
+await run()
