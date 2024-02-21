a16260c618cb3f164049d0eebf8d99c8e0fe01d9
tsctx <91457664+tsctx@users.noreply.github.com>
2024-02-21 20:29:41 +0900

    perf: improve sort algorithm

---
+++ test/fetch/headerslist-sortedarray.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const { test } = require('node:test')
+const assert = require('node:assert')
+const { HeadersList, compareHeaderName } = require('../../lib/fetch/headers')
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+const charactersLength = characters.length
+
+function generateAsciiString (length) {
+  let result = ''
+  for (let i = 0; i < length; ++i) {
+    result += characters[Math.floor(Math.random() * charactersLength)]
+  }
+  return result
+}
+
+const SORT_RUN = 4000
+
+test('toSortedArray (fast-path)', () => {
+  for (let i = 0; i < SORT_RUN; ++i) {
+    const headersList = new HeadersList()
+    for (let j = 0; j < 32; ++j) {
+      headersList.append(generateAsciiString(4), generateAsciiString(4))
+    }
+    assert.deepStrictEqual(headersList.toSortedArray(), [...headersList].sort(compareHeaderName))
+  }
+})
+
+// Do we really need it?
+test('toSortedArray (slow-path)', () => {
+  for (let i = 0; i < SORT_RUN; ++i) {
+    const headersList = new HeadersList()
+    for (let j = 0; j < 64; ++j) {
+      headersList.append(generateAsciiString(4), generateAsciiString(4))
+    }
+    assert.deepStrictEqual(headersList.toSortedArray(), [...headersList].sort(compareHeaderName))
+  }
+})
