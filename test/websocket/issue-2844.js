69f303279b3929e45355a974128edb50ba356c91
Khafra <maitken033380023@gmail.com>
2024-02-25 17:37:52 -0500

    fixup

--- test/websocket/issue-2844.js
+++ test/websocket/issue-2844.js
@@ -39,3 +39,35 @@ test('The server must reply with at least one subprotocol the client sends', asy
 
   await completed
 })
+
+test('The connection fails when the client sends subprotocols that the server does not responc with', async (t) => {
+  const { completed, fail, ok } = tspl(t, { plan: 1 })
+
+  const wss = new WebSocketServer({
+    handleProtocols: () => false,
+    port: 0
+  })
+
+  wss.on('connection', (ws) => {
+    ws.on('error', fail)
+    ws.send('something')
+  })
+
+  await once(wss, 'listening')
+
+  const ws = new WebSocket(`ws://localhost:${wss.address().port}`, {
+    protocols: ['json']
+  })
+
+  ws.onerror = ok.bind(null, true)
+  // The server will try to send 'something', this ensures that the connection
+  // fails during the handshake and doesn't receive any messages.
+  ws.onmessage = fail
+
+  t.after(() => {
+    wss.close()
+    ws.close()
+  })
+
+  await completed
+})
