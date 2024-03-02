e69ec5cb2950c7577c79efd03c43f067c49cbf19
Khafra <maitken033380023@gmail.com>
2024-03-01 20:23:52 -0500

    for some reason this fixes the test

--- test/fetch/content-length.js
+++ test/fetch/content-length.js
@@ -6,7 +6,6 @@ const { createServer } = require('node:http')
 const { once } = require('node:events')
 const { Blob } = require('node:buffer')
 const { fetch, FormData } = require('../..')
-const { closeServerAsPromise } = require('../utils/node-http')
 
 // https://github.com/nodejs/undici/issues/1783
 test('Content-Length is set when using a FormData body with fetch', async (t) => {
@@ -18,7 +17,6 @@ test('Content-Length is set when using a FormData body with fetch', async (t) =>
   }).listen(0)
 
   await once(server, 'listening')
-  t.after(closeServerAsPromise(server))
 
   const fd = new FormData()
   fd.set('file', new Blob(['hello world 👋'], { type: 'text/plain' }), 'readme.md')
@@ -27,5 +25,5 @@ test('Content-Length is set when using a FormData body with fetch', async (t) =>
   await fetch(`http://localhost:${server.address().port}`, {
     method: 'POST',
     body: fd
-  })
+  }).finally(() => server.close())
 })
