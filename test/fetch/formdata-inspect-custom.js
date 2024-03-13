f29fa79e029e307705eb8560117906dd29d8192d
Mert Can Altin <mert.altin@trendyol.com>
2024-03-13 22:37:54 +0300

    feat: repair code

--- test/fetch/formdata-inspect-custom.js
+++ test/fetch/formdata-inspect-custom.js
@@ -11,5 +11,5 @@ test('FormData class custom inspection', () => {
 
   const expectedOutput = 'FormData:\nusername: john_doe\nemail: john@example.com\n'
 
-  assert.strictEqual(formData.toString(), expectedOutput)
+  assert.strictEqual(formData, expectedOutput)
 })
