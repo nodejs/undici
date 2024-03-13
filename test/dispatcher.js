a1ee3caa8b4ebe15d8fc545197cfd027b9a165e8
Carlos Fuentes <me@metcoder.dev>
2024-03-13 17:49:33 +0100

    feat: add new dispatch compose (#2826)
    * feat: add new dispatch compose
    
    * fix: review
    
    Co-authored-by: Matteo Collina <hello@matteocollina.com>
    
    * revert: linting
    
    * docs: add documentation
    
    * fix: smaller tweaks to proxy interceptor
    
    * test: fix tests for proxy
    
    * refactor: expose interceptor as is
    
    * test: add testing for retry
    
    * refactor: rewrite interceptors
    
    * refactor: proxy interceptor
    
    * feat: redirect interceptor
    
    * refactor: change the compose behaviour
    
    * docs: update docs
    
    * test: add testing for compose
    
    * feat: composed dispatcher
    
    * docs: adjust documentation
    
    * refactor: apply review
    
    * docs: tweaks
    
    * feat: drop proxy
    
    ---------
    
    Co-authored-by: Matteo Collina <hello@matteocollina.com>

--- test/dispatcher.js
+++ test/dispatcher.js
@@ -20,3 +20,23 @@ test('dispatcher implementation', (t) => {
   t.throws(() => poorImplementation.close(), Error, 'throws on unimplemented close')
   t.throws(() => poorImplementation.destroy(), Error, 'throws on unimplemented destroy')
 })
+
+test('dispatcher.compose', (t) => {
+  t = tspl(t, { plan: 10 })
+
+  const dispatcher = new Dispatcher()
+  const interceptor = () => (opts, handler) => {}
+  // Should return a new dispatcher
+  t.ok(Object.getPrototypeOf(dispatcher.compose(interceptor)) instanceof Dispatcher)
+  t.ok(Object.getPrototypeOf(dispatcher.compose(interceptor, interceptor)) instanceof Dispatcher)
+  t.ok(Object.getPrototypeOf(dispatcher.compose([interceptor, interceptor])) instanceof Dispatcher)
+  t.ok(dispatcher.compose(interceptor) !== dispatcher)
+  t.throws(() => dispatcher.dispatch({}), Error, 'invalid interceptor')
+  t.throws(() => dispatcher.dispatch(() => null), Error, 'invalid interceptor')
+  t.throws(() => dispatcher.dispatch(dispatch => dispatch, () => () => {}, Error, 'invalid interceptor'))
+
+  const composed = dispatcher.compose(interceptor)
+  t.equal(typeof composed.dispatch, 'function', 'returns an object with a dispatch method')
+  t.equal(typeof composed.close, 'function', 'returns an object with a close method')
+  t.equal(typeof composed.destroy, 'function', 'returns an object with a destroy method')
+})
---
+++ test/interceptors/retry.js
@@ -0,0 +1,482 @@
+'use strict'
+
+const { tspl } = require('@matteo.collina/tspl')
+const { test, after } = require('node:test')
+const { createServer } = require('node:http')
+const { once } = require('node:events')
+
+const { Client, interceptors } = require('../..')
+const { retry } = interceptors
+
+test('Should retry status code', async t => {
+  t = tspl(t, { plan: 4 })
+
+  let counter = 0
+  const server = createServer()
+  const retryOptions = {
+    retry: (err, { state, opts }, done) => {
+      counter++
+
+      if (err.statusCode === 500 || err.message.includes('other side closed')) {
+        setTimeout(done, 500)
+        return
+      }
+
+      return done(err)
+    }
+  }
+  const requestOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  server.on('request', (req, res) => {
+    switch (counter) {
+      case 0:
+        req.destroy()
+        t.ok(true, 'pass')
+        return
+      case 1:
+        res.writeHead(500)
+        res.end('failed')
+        t.ok(true, 'pass')
+        return
+      case 2:
+        res.writeHead(200)
+        res.end('hello world!')
+        return
+      default:
+        t.fail()
+    }
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry(retryOptions))
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request(requestOptions)
+
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+})
+
+test('Should use retry-after header for retries', async t => {
+  t = tspl(t, { plan: 3 })
+
+  let counter = 0
+  const server = createServer()
+  let checkpoint
+  const dispatchOptions = {
+    method: 'PUT',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  server.on('request', (req, res) => {
+    switch (counter) {
+      case 0:
+        res.writeHead(429, {
+          'retry-after': 1
+        })
+        res.end('rate limit')
+        checkpoint = Date.now()
+        counter++
+        return
+      case 1:
+        res.writeHead(200)
+        res.end('hello world!')
+        t.ok(Date.now() - checkpoint >= 500)
+        counter++
+        return
+      default:
+        t.fail('unexpected request')
+    }
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request(dispatchOptions)
+
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+})
+
+test('Should use retry-after header for retries (date)', async t => {
+  t = tspl(t, { plan: 3 })
+
+  let counter = 0
+  const server = createServer()
+  let checkpoint
+  const reuestOptions = {
+    method: 'PUT',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  server.on('request', (req, res) => {
+    switch (counter) {
+      case 0:
+        res.writeHead(429, {
+          'retry-after': new Date(
+            new Date().setSeconds(new Date().getSeconds() + 1)
+          ).toUTCString()
+        })
+        res.end('rate limit')
+        checkpoint = Date.now()
+        counter++
+        return
+      case 1:
+        res.writeHead(200)
+        res.end('hello world!')
+        t.ok(Date.now() - checkpoint >= 1)
+        counter++
+        return
+      default:
+        t.fail('unexpected request')
+    }
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request(reuestOptions)
+
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+})
+
+test('Should retry with defaults', async t => {
+  t = tspl(t, { plan: 2 })
+
+  let counter = 0
+  const server = createServer()
+  const requestOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  server.on('request', (req, res) => {
+    switch (counter) {
+      case 0:
+        req.destroy()
+        counter++
+        return
+      case 1:
+        res.writeHead(500)
+        res.end('failed')
+        counter++
+        return
+      case 2:
+        res.writeHead(200)
+        res.end('hello world!')
+        counter++
+        return
+      default:
+        t.fail()
+    }
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request(requestOptions)
+
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+})
+
+test('Should handle 206 partial content', async t => {
+  t = tspl(t, { plan: 5 })
+
+  let counter = 0
+
+  // Took from: https://github.com/nxtedition/nxt-lib/blob/4b001ebc2f22cf735a398f35ff800dd553fe5933/test/undici/retry.js#L47
+  let x = 0
+  const server = createServer((req, res) => {
+    if (x === 0) {
+      t.ok(true, 'pass')
+      res.setHeader('etag', 'asd')
+      res.write('abc')
+      setTimeout(() => {
+        res.destroy()
+      }, 1e2)
+    } else if (x === 1) {
+      t.deepStrictEqual(req.headers.range, 'bytes=3-')
+      res.setHeader('content-range', 'bytes 3-6/6')
+      res.setHeader('etag', 'asd')
+      res.statusCode = 206
+      res.end('def')
+    }
+    x++
+  })
+
+  const retryOptions = {
+    retry: function (err, _, done) {
+      counter++
+
+      if (err.code && err.code === 'UND_ERR_DESTROYED') {
+        return done(false)
+      }
+
+      if (err.statusCode === 206) return done(err)
+
+      setTimeout(done, 800)
+    }
+  }
+  const requestOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    },
+    retryOptions
+  }
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request(requestOptions)
+
+  t.equal(response.statusCode, 200)
+  t.strictEqual(await response.body.text(), 'abcdef')
+  t.strictEqual(counter, 1)
+})
+
+test('Should handle 206 partial content - bad-etag', async t => {
+  t = tspl(t, { plan: 3 })
+
+  // Took from: https://github.com/nxtedition/nxt-lib/blob/4b001ebc2f22cf735a398f35ff800dd553fe5933/test/undici/retry.js#L47
+  let x = 0
+  const server = createServer((req, res) => {
+    if (x === 0) {
+      t.ok(true, 'pass')
+      res.setHeader('etag', 'asd')
+      res.write('abc')
+      setTimeout(() => {
+        res.destroy()
+      }, 1e2)
+    } else if (x === 1) {
+      t.deepStrictEqual(req.headers.range, 'bytes=3-')
+      res.setHeader('content-range', 'bytes 3-6/6')
+      res.setHeader('etag', 'erwsd')
+      res.statusCode = 206
+      res.end('def')
+    }
+    x++
+  })
+
+  const requestOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    },
+    retryOptions: {
+      retry: (err, { state, opts }, done) => {
+        if (err.message.includes('other side closed')) {
+          setTimeout(done, 100)
+          return
+        }
+
+        return done(err)
+      }
+    }
+  }
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  try {
+    const response = await client.request(requestOptions)
+    await response.body.text()
+  } catch (error) {
+    t.strict(error, {
+      message: 'ETag mismatch',
+      code: 'UND_ERR_REQ_RETRY',
+      name: 'RequestRetryError'
+    })
+  }
+})
+
+test('retrying a request with a body', async t => {
+  t = tspl(t, { plan: 2 })
+  let counter = 0
+  const server = createServer()
+  const requestOptions = {
+    method: 'POST',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ hello: 'world' }),
+    retryOptions: {
+      retry: (err, { state, opts }, done) => {
+        counter++
+
+        if (
+          err.statusCode === 500 ||
+          err.message.includes('other side closed')
+        ) {
+          setTimeout(done, 500)
+          return
+        }
+
+        return done(err)
+      }
+    }
+  }
+
+  server.on('request', (req, res) => {
+    switch (counter) {
+      case 0:
+        req.destroy()
+        return
+      case 1:
+        res.writeHead(500)
+        res.end('failed')
+        return
+      case 2:
+        res.writeHead(200)
+        res.end('hello world!')
+        return
+      default:
+        t.fail()
+    }
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request(requestOptions)
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+})
+
+test('should not error if request is not meant to be retried', async t => {
+  t = tspl(t, { plan: 2 })
+
+  const server = createServer()
+  server.on('request', (req, res) => {
+    res.writeHead(400)
+    res.end('Bad request')
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(retry())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request({
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+  t.equal(response.statusCode, 400)
+  t.equal(await response.body.text(), 'Bad request')
+})
