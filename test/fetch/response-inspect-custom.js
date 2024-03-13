5a58f675f47faeb4cc4fe3c5ed8ce464836dc7f6
Mert Can Altin <mert.altin@trendyol.com>
2024-03-13 14:10:18 +0300

    fetch: improve output for Response

---
+++ test/fetch/response-inspect-custom.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const { describe, it } = require('node:test')
+const assert = require('assert')
+const util = require('util')
+const { Response } = require('../../')
+
+describe('Response custom inspection', () => {
+  it('should return a custom inspect output', () => {
+    const response = new Response(null)
+    const inspectedOutput = util.inspect(response)
+    const expectedOutput = `Response { status: 200, statusText: "", headers: HeadersList {
+  cookies: null,
+  [Symbol(headers map)]: Map(0) {},
+  [Symbol(headers map sorted)]: null
+} }`
+    assert.strictEqual(inspectedOutput, expectedOutput)
+  })
+})
