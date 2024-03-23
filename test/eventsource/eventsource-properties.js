e6812529d1995fe526daa824c198c0f8350994c8
Aras Abbasi <aras.abbasi@googlemail.com>
2024-03-23 18:41:08 +0100

    Apply suggestions from code review

--- test/eventsource/eventsource-properties.js
+++ test/eventsource/eventsource-properties.js
@@ -5,11 +5,11 @@ const assert = require('node:assert')
 const { EventSource } = require('../..') // assuming the test is in test/eventsource/
 
 test('EventSource.prototype properties are configured correctly', () => {
-    const props = Object.entries(Object.getOwnPropertyDescriptors(EventSource.prototype))
+  const props = Object.entries(Object.getOwnPropertyDescriptors(EventSource.prototype))
 
-    for (const [key, value] of props) {
+  for (const [key, value] of props) {
         if (key !== 'constructor') {
-            assert(value.enumerable, `${key} is not enumerable`)
-        }
+      assert(value.enumerable, `${key} is not enumerable`)
     }
+  }
 })
