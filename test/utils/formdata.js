82719a7be21e4c002ee42218ca3e53f12ddc80ec
uzlopak <aras.abbasi@googlemail.com>
2024-04-07 04:06:17 +0200

    test: activate testing for interceptors and cache

--- test/utils/formdata.js
+++ test/utils/formdata.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const Busboy = require('@fastify/busboy')
 
 function parseFormDataString (
--- test/utils/node-http.js
+++ test/utils/node-http.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const util = require('node:util')
 
 function closeServerAsPromise (server) {
