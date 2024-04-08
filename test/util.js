413fd4de5b161c1e9a539089a4d51965abd5018d
Aras Abbasi <aras.abbasi@googlemail.com>
2024-04-08 08:05:13 +0200

    perf: improve isBlobLike (#3070)

---
+++ test/util.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const { strictEqual } = require('node:assert')
+const { test, describe } = require('node:test')
+const { isBlobLike } = require('../lib/core/util')
+const { Blob, File } = require('node:buffer')
+
+describe('isBlobLike', () => {
+  test('buffer', () => {
+    const buffer = Buffer.alloc(1)
+    strictEqual(isBlobLike(buffer), false)
+  })
+
+  test('blob', { skip: !Blob }, () => {
+    const blob = new Blob(['asd'], {
+      type: 'application/json'
+    })
+    strictEqual(isBlobLike(blob), true)
+  })
+
+  test('file', { skip: !File }, () => {
+    const file = new File(['asd'], 'file.txt', {
+      type: 'text/plain'
+    })
+    strictEqual(isBlobLike(file), true)
+  })
+
+  test('blobLikeStream', () => {
+    const blobLikeStream = {
+      [Symbol.toStringTag]: 'Blob',
+      stream: () => { }
+    }
+    strictEqual(isBlobLike(blobLikeStream), true)
+  })
+
+  test('fileLikeStream', () => {
+    const fileLikeStream = {
+      stream: () => { },
+      [Symbol.toStringTag]: 'File'
+    }
+    strictEqual(isBlobLike(fileLikeStream), true)
+  })
+
+  test('fileLikeArrayBuffer', () => {
+    const blobLikeArrayBuffer = {
+      [Symbol.toStringTag]: 'Blob',
+      arrayBuffer: () => { }
+    }
+    strictEqual(isBlobLike(blobLikeArrayBuffer), true)
+  })
+
+  test('blobLikeArrayBuffer', () => {
+    const fileLikeArrayBuffer = {
+      [Symbol.toStringTag]: 'File',
+      arrayBuffer: () => { }
+    }
+    strictEqual(isBlobLike(fileLikeArrayBuffer), true)
+  })
+
+  test('string', () => {
+    strictEqual(isBlobLike('Blob'), false)
+  })
+
+  test('null', () => {
+    strictEqual(isBlobLike(null), false)
+  })
+})
