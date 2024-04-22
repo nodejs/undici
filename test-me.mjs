7119e817c022c3681d11282c6611c1cf7e3cb660
Matteo Collina <hello@matteocollina.com>
2024-04-22 17:29:56 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test-me.mjs
+++
@@ -1,9 +0,0 @@
-import { fetch } from './index.js'
-
-console.time('fetch')
-for (let i = 0; i < 100000; i++) {
-  const res = await fetch('http://localhost:3000')
-  // console.log(await res.text())
-  await res.text()
-}
-console.timeEnd('fetch')
