961b76ad7cac17d23580d172702e11a080974f5d
Matteo Collina <hello@matteocollina.com>
2024-04-23 13:51:16 +0200

    Expose EnvHttpProxyAgent to Node.js core bundle, so it can be turned … (#3148)

--- index-fetch.js
+++ index-fetch.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { getGlobalDispatcher, setGlobalDispatcher } = require('./lib/global')
+const EnvHttpProxyAgent = require('./lib/dispatcher/env-http-proxy-agent')
 const fetchImpl = require('./lib/web/fetch').fetch
 
 module.exports.fetch = function fetch (resource, init = undefined) {
@@ -19,3 +21,8 @@ module.exports.WebSocket = require('./lib/web/websocket/websocket').WebSocket
 module.exports.MessageEvent = require('./lib/web/websocket/events').MessageEvent
 
 module.exports.EventSource = require('./lib/web/eventsource/eventsource').EventSource
+
+// Expose the fetch implementation to be enabled in Node.js core via a flag
+module.exports.EnvHttpProxyAgent = EnvHttpProxyAgent
+module.exports.getGlobalDispatcher = getGlobalDispatcher
+module.exports.setGlobalDispatcher = setGlobalDispatcher
