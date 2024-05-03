e410afc4c6cdbf7f259d09bd01998dc8bf193a86
Matteo Collina <hello@matteocollina.com>
2024-05-03 19:22:22 +0200

    use FinalizationRegistry to cancel the body if response is collected
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ test/fetch/fire-and-forget.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const { randomFillSync } = require('node:crypto')
+const { setTimeout: sleep } = require('timers/promises')
+const { test } = require('node:test')
+const { fetch, Agent, setGlobalDispatcher } = require('../..')
+const { createServer } = require('node:http')
+const { closeServerAsPromise } = require('../utils/node-http')
+
+const blob = randomFillSync(new Uint8Array(1024 * 512))
+const fmt = new Intl.NumberFormat()
+
+test('does not need the body to be consumed to continue', async (t) => {
+  const agent = new Agent({
+    keepAliveMaxTimeout: 10,
+    keepAliveTimeoutThreshold: 10
+  })
+  setGlobalDispatcher(agent)
+  const server = createServer((req, res) => {
+    res.writeHead(200)
+    res.end(blob)
+  })
+  t.after(closeServerAsPromise(server))
+
+  await new Promise((resolve) => {
+    server.listen(0, resolve)
+  })
+
+  const url = new URL(`http://127.0.0.1:${server.address().port}`)
+
+  const batch = 50
+  const delay = 0
+  let total = 0
+  while (total < 10000) {
+    const array = new Array(batch)
+    for (let i = 0; i < batch; i++) {
+      array[i] = fetch(url).catch(() => {})
+    }
+    await Promise.all(array)
+    await sleep(delay)
+
+    console.log(
+      'RSS',
+      (process.memoryUsage.rss() / 1024 / 1024) | 0,
+      'MB after',
+      fmt.format((total += batch)) + ' fetch() requests'
+    )
+  }
+})
