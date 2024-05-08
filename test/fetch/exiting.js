3fd089485b540d6c951d53000a98e9d0affd123b
Matteo Collina <hello@matteocollina.com>
2024-05-08 09:34:40 +0200

    Add test to verify if the connection is correctly aborted on cancel
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ test/fetch/exiting.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const { test } = require('node:test')
+const { fetch } = require('../..')
+const { createServer } = require('node:http')
+const { closeServerAsPromise } = require('../utils/node-http')
+const tspl = require('@matteo.collina/tspl')
+
+test('abort the request on the other side if the stream is canceled', async (t) => {
+  const p = tspl(t, { plan: 1 })
+  const server = createServer((req, res) => {
+    res.writeHead(200)
+    res.write('hello')
+    req.on('aborted', () => {
+      p.ok('aborted')
+    })
+    // Let's not end the response on purpose
+  })
+  t.after(closeServerAsPromise(server))
+
+  await new Promise((resolve) => {
+    server.listen(0, resolve)
+  })
+
+  const url = new URL(`http://127.0.0.1:${server.address().port}`)
+
+  const response = await fetch(url, {
+    // if added both scripts will also exit on 21 and 22.0
+    // dispatcher: new Agent({ bodyTimeout: 1000 })
+  })
+
+  const reader = response.body.getReader()
+
+  try {
+    await reader.read()
+  } finally {
+    reader.releaseLock()
+    await response.body.cancel()
+  }
+
+  await p.completed
+})
