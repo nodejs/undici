a239a51dfcd3568d1062efbddc4a885a69164e71
Khafra <maitken033380023@gmail.com>
2024-05-12 15:45:34 -0400

    add autobahn suite

---
+++ test/autobahn/client.js
@@ -0,0 +1,42 @@
+// @ts-check
+const { WebSocket } = require('../..')
+
+let currentTest = 1
+let testCount
+
+function nextTest () {
+  let ws
+
+  if (currentTest > testCount) {
+    ws = new WebSocket('ws://localhost:9001/updateReports?agent=ws')
+    return
+  }
+
+  console.log(`Running test case ${currentTest}/${testCount}`)
+
+  ws = new WebSocket(
+    `ws://localhost:9001/runCase?case=${currentTest}&agent=ws`
+  )
+  ws.addEventListener('message', (data) => {
+    ws.send(data.data)
+  })
+  ws.addEventListener('close', () => {
+    currentTest++
+    process.nextTick(nextTest)
+  })
+  ws.addEventListener('error', (e) => {
+    console.error(e.error)
+    currentTest++
+    process.nextTick(nextTest)
+  })
+}
+
+const ws = new WebSocket('ws://localhost:9001/getCaseCount')
+ws.addEventListener('message', (data) => {
+  testCount = parseInt(data.data)
+})
+ws.addEventListener('close', () => {
+  if (testCount > 0) {
+    nextTest()
+  }
+})
