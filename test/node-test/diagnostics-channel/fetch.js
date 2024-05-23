d7ef0fb75f654717fcfc1f32b47da70d0a4f97f2
Crystal Magloire <crys.magloire@gmail.com>
2024-05-23 11:06:18 -0400

    resolving conflicts

--- test/node-test/diagnostics-channel/fetch.js
+++ test/node-test/diagnostics-channel/fetch.js
@@ -34,47 +34,47 @@ describe('diagnosticsChannel for fetch', { skip }, () => {
     t = tspl(t, { plan: 17 })
 
     let startCalled = 0
-    diagnosticsChannel.channel('tracing:undici:fetch:start').subscribe(({ input, init, result, error }) => {
+    diagnosticsChannel.channel('tracing:undici:fetch:start').subscribe(({ req, input, init, result, error }) => {
       startCalled += 1
       if (input.redirect) {
         t.strictEqual(input, 'badrequest')
         t.deepStrictEqual(init, { redirect: 'error' })
       } else {
         t.strictEqual(input, `http://localhost:${server.address().port}`)
-        t.deepStrictEqual(init, {})
+        t.deepStrictEqual(init, undefined)
       }
     })
 
     let endCalled = 0
-    diagnosticsChannel.channel('tracing:undici:fetch:end').subscribe(({ input, init, result, error }) => {
+    diagnosticsChannel.channel('tracing:undici:fetch:end').subscribe(({ req, input, init, result, error }) => {
       endCalled += 1
-      if (init.redirect) {
+      if (init && init.redirect) {
         t.strictEqual(input, 'badrequest')
         t.deepStrictEqual(init, { redirect: 'error' })
       } else {
         t.strictEqual(input, `http://localhost:${server.address().port}`)
-        t.deepStrictEqual(init, {})
+        t.deepStrictEqual(init, undefined)
       }
       t.strictEqual(result, null)
     })
 
     let asyncStartCalled = 0
-    diagnosticsChannel.channel('tracing:undici:fetch:asyncStart').subscribe(({ input, init, result, error }) => {
+    diagnosticsChannel.channel('tracing:undici:fetch:asyncStart').subscribe(({ req, input, init, result, error }) => {
       asyncStartCalled += 1
-      if (init.redirect) {
+      if (init && init.redirect) {
         t.strictEqual(input, 'badrequest')
         t.deepStrictEqual(init, { redirect: 'error' })
       } else {
         t.strictEqual(input, `http://localhost:${server.address().port}`)
-        t.deepStrictEqual(init, {})
+        t.deepStrictEqual(init, undefined)
         t.ok(result)
       }
     })
 
     let asyncEndCalled = 0
-    diagnosticsChannel.channel('tracing:undici:fetch:asyncEnd').subscribe(async ({ input, init, result, error }) => {
+    diagnosticsChannel.channel('tracing:undici:fetch:asyncEnd').subscribe(async ({ req, input, init, result, error }) => {
       asyncEndCalled += 1
-      if (init.redirect) {
+      if (init && init.redirect) {
         t.strictEqual(input, 'badrequest')
         t.deepStrictEqual(init, { redirect: 'error' })
         t.strictEqual(result, null)
@@ -82,7 +82,7 @@ describe('diagnosticsChannel for fetch', { skip }, () => {
         t.strictEqual(error.cause.code, 'ERR_INVALID_URL')
       } else {
         t.strictEqual(input, `http://localhost:${server.address().port}`)
-        t.deepStrictEqual(init, {})
+        t.deepStrictEqual(init, undefined)
         t.ok(result)
         t.strictEqual(result.status, 200)
         t.strictEqual(error, null)
