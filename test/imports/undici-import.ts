fb67581d7ce68ca8a46ebc80d471df1a55660df8
jake li <jake.li@sine.co>
2024-06-10 23:27:22 +0930

    fixup! add a test

--- test/imports/undici-import.ts
+++ test/imports/undici-import.ts
@@ -1,5 +1,7 @@
 import { request } from '../../'
+import { interceptors } from '../../'
 
 async function exampleCode() {
+  const ri = interceptors.createRedirectInterceptor({})
   await request('http://localhost:3000/foo')
 }
