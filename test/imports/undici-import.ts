3e170a5fcbd90dbaa00dd8f70a3efaa8344d84e0
jake li <jake.li@sine.co>
2024-06-11 17:59:07 +0930

    fixup! add retry, redirect, dump and remove createRedirectInterceptor

--- test/imports/undici-import.ts
+++ test/imports/undici-import.ts
@@ -2,6 +2,9 @@ import { request } from '../../'
 import { interceptors } from '../../'
 
 async function exampleCode() {
-  const ri = interceptors.createRedirectInterceptor({})
+  const retry = interceptors.retry()
+  const rd = interceptors.redirect()
+  const dump = interceptors.dump()
+
   await request('http://localhost:3000/foo')
 }
