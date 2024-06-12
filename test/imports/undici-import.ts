1e508d761cee1b1838c483f76421d7daea0bd5a8
jakecastelli <38635403+jakecastelli@users.noreply.github.com>
2024-06-12 19:13:29 +0930

    export interceptor to type def file (#3318)
    * export interceptor to type def file
    
    Fixes: https://github.com/nodejs/undici/issues/3308
    
    * fixup! add a test
    
    * fixup! add retry, redirect, dump and remove createRedirectInterceptor
    
    ---------
    
    Co-authored-by: jake li <jake.li@sine.co>

--- test/imports/undici-import.ts
+++ test/imports/undici-import.ts
@@ -1,5 +1,10 @@
 import { request } from '../../'
+import { interceptors } from '../../'
 
 async function exampleCode() {
+  const retry = interceptors.retry()
+  const rd = interceptors.redirect()
+  const dump = interceptors.dump()
+
   await request('http://localhost:3000/foo')
 }
