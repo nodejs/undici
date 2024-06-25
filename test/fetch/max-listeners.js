1efb435cebbd9ede216942280b7553aa0a7499e5
Fawaz Ahmed <fawazahmed0@hotmail.com>
2024-06-26 02:59:29 +0530

    add test

---
+++ test/fetch/max-listeners.js
@@ -0,0 +1,13 @@
+const { setMaxListeners, getMaxListeners } = require('events')
+const { test } = require('node:test')
+const assert = require('node:assert')
+const { fetch } = require('../..')
+
+test('test max listeners', async (t) => {
+    const controller = new AbortController();
+    setMaxListeners(Infinity, controller.signal)
+    let url = URL.createObjectURL(new Blob())
+    for(let i=0;i<1600;i++)
+        fetch(url, { signal: controller.signal })
+    assert.strictEqual(getMaxListeners(controller.signal), Infinity);
+});
