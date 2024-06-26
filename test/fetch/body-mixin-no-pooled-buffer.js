5ee86202cb24a9ba16929da809d1379c7fe20712
tsctx <91457664+tsctx@users.noreply.github.com>
2024-06-26 21:12:59 +0900

    fetch: don't use pooled buffer in body mixin

---
+++ test/fetch/body-mixin-no-pooled-buffer.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { Response } = require('../../')
+const assert = require('node:assert')
+const { test } = require('node:test')
+
+test('Don not use pooled buffer in body mixin', async () => {
+  const allocUnsafe = Buffer.allocUnsafe
+
+  try {
+    let counter = 0
+    Buffer.allocUnsafe = function (...args) {
+      counter++
+      return allocUnsafe(...args)
+    }
+    await new Response([new Uint8Array(1)]).text()
+    assert.strictEqual(counter, 0)
+  } finally {
+    Buffer.allocUnsafe = allocUnsafe
+  }
+})
