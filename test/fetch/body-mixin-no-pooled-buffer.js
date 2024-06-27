b95af7039f31d89ec782b3673c7e836bdea6b046
tsctx <91457664+tsctx@users.noreply.github.com>
2024-06-28 07:09:23 +0900

    Update body-mixin-no-pooled-buffer.js

--- test/fetch/body-mixin-no-pooled-buffer.js
+++ test/fetch/body-mixin-no-pooled-buffer.js
@@ -14,7 +14,8 @@ test('Do not use pooled buffer in body mixin', async () => {
       return allocUnsafe(...args)
     }
     // Do not use Buffer.allocUnsafe as it exposes the body to the pooled buffer.
-    await new Response(new TextEncoder().encode('...any body')).text()
+    await new Response('...any body')
+).text()
     // Body will be printed included.
     // console.log(new TextDecoder().decode(Buffer.allocUnsafe(1).buffer.slice(304)))
     assert.strictEqual(counter, 0)
