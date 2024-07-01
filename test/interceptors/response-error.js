a1b282ff377f2557e4411e2014f4c470cd93d349
Mert Can Altin <mert.altin@trendyol.com>
2024-07-01 14:29:43 +0300

    lint & added more test

--- test/interceptors/response-error.js
+++ test/interceptors/response-error.js
@@ -4,8 +4,8 @@ const assert = require('assert')
 const { test } = require('node:test')
 const createResponseErrorInterceptor = require('../../lib/interceptor/response-error')
 
-test('should not error if request is not meant to be retried', async (t) => {
-  const response = { statusCode: 400 }
+test('should not error if request is not meant to throw error', async (t) => {
+  const opts = { throwOnError: false }
   const handler = {
     onError: () => {},
     onData: () => {},
@@ -14,5 +14,44 @@ test('should not error if request is not meant to be retried', async (t) => {
 
   const interceptor = createResponseErrorInterceptor((opts, handler) => handler.onComplete())
 
-  assert.doesNotThrow(() => interceptor({ response, throwOnError: false }, handler))
+  assert.doesNotThrow(() => interceptor(opts, handler))
+})
+
+test('should error if request status code is in the specified error codes', async (t) => {
+  const opts = { throwOnError: true, statusCodes: [500] }
+  const response = { statusCode: 500 }
+  let capturedError
+  const handler = {
+    onError: (err) => {
+      capturedError = err
+    },
+    onData: () => {},
+    onComplete: () => {}
+  }
+
+  const interceptor = createResponseErrorInterceptor((opts, handler) => handler.onComplete())
+
+  // opts ve response parametrelerini interceptor'a doğru şekilde geçin
+  interceptor({ ...opts, response }, handler)
+
+  // `setImmediate` kullanarak `capturedError`'ın set edilmesini bekleyin
+  await new Promise(resolve => setImmediate(resolve))
+
+  assert(capturedError, 'Expected error to be captured but it was not.')
+  assert.strictEqual(capturedError.message, 'Response Error')
+  assert.strictEqual(capturedError.statusCode, 500)
+})
+
+test('should not error if request status code is not in the specified error codes', async (t) => {
+  const opts = { throwOnError: true, statusCodes: [500] }
+  const response = { statusCode: 404 }
+  const handler = {
+    onError: () => {},
+    onData: () => {},
+    onComplete: () => {}
+  }
+
+  const interceptor = createResponseErrorInterceptor((opts, handler) => handler.onComplete())
+
+  assert.doesNotThrow(() => interceptor({ ...opts, response }, handler))
 })
