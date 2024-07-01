3e52a161de03e702f0fba183ce22ef328e27d2ba
jfhr <me@jfhr.de>
2024-07-01 22:29:07 +0200

    Add generic type for opaque object
    This adds a `TOpaque` generic type parameter to the type definitions
    for request(), connect(), stream(), and pipeline(). The type parameter
    defaults to null, which is the default value of the opaque property.
    If an opaque value is passed in the options, its type can usually be
    inferred automatically, such that no explicit type declaration is
    necessary. This commit also adds tsd tests to make sure the type
    definitions work as expected.
    
    Previously, the type of `opaque` was `unknown`, which means it needed
    to be either type-checked or casted to another type before anything
    could be done with it. Such code should not be broken by this commit,
    although some type checks or assertions might become redundant. Code
    that disabled type checks (e.g. by casting to `any` or using
    `@ts-ignore` should be unaffected. Code that does not use typescript
    at all is also unaffected.
    
    This closes #3378

--- test/types/api.test-d.ts
+++ test/types/api.test-d.ts
@@ -1,5 +1,5 @@
 import { Duplex, Readable, Writable } from 'stream'
-import { expectAssignable } from 'tsd'
+import { expectAssignable, expectType } from 'tsd'
 import { Dispatcher, request, stream, pipeline, connect, upgrade } from '../..'
 
 // request
@@ -10,12 +10,22 @@ expectAssignable<Promise<Dispatcher.ResponseData>>(request('', { method: 'GET', 
 // stream
 expectAssignable<Promise<Dispatcher.StreamData>>(stream('', { method: 'GET' }, data => {
   expectAssignable<Dispatcher.StreamFactoryData>(data)
+  expectType<null>(data.opaque)
+  return new Writable()
+}))
+expectAssignable<Promise<Dispatcher.StreamData<{ example: string }>>>(stream('', { method: 'GET', opaque: { example: '' } }, data => {
+  expectType<{ example: string }>(data.opaque)
   return new Writable()
 }))
 
 // pipeline
 expectAssignable<Duplex>(pipeline('', { method: 'GET' }, data => {
   expectAssignable<Dispatcher.PipelineHandlerData>(data)
+  expectType<null>(data.opaque)
+  return new Readable()
+}))
+expectAssignable<Duplex>(pipeline('', { method: 'GET', opaque: { example: '' } }, data => {
+  expectType<{ example: string }>(data.opaque)
   return new Readable()
 }))
 
