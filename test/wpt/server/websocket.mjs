4abc633fa90fbcde7b6c560d8e5408600c0bed84
Khafra <maitken033380023@gmail.com>
2024-09-07 11:17:48 -0400

    fix 7 errors

--- test/wpt/server/websocket.mjs
+++ test/wpt/server/websocket.mjs
@@ -7,11 +7,18 @@ import { server } from './server.mjs'
 // event, so I'm unsure if we can stop relying on server.
 
 const wss = new WebSocketServer({
-  server,
+  noServer: true,
   handleProtocols: (protocols) => protocols.values().next().value
 })
 
 wss.on('connection', (ws, request) => {
+  if (request.url === '/protocol_array') {
+    const line = request.headers['sec-websocket-protocol']
+    const protocol = line.split(',')[0]
+    ws.send(protocol)
+    return
+  }
+
   ws.on('message', (data, isBinary) => {
     const str = data.toString('utf-8')
 
@@ -42,5 +49,17 @@ wss.on('connection', (ws, request) => {
 
   ws.on('close', () => {
     clearTimeout(timeout)
+  })
+})
+
+server.on('upgrade', (request, socket, head) => {
+  if (request.url === '/404') {
+    socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+    socket.destroy()
+    return
+  }
+
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request)
   })
 })
