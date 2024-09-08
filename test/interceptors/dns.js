52f6c346d450f8277b309b33302fdef4769a042e
Carlos Fuentes <me@metcoder.dev>
2024-09-08 14:32:58 +0200

    feat: add support for TTL

--- test/interceptors/dns.js
+++ test/interceptors/dns.js
@@ -1,10 +1,13 @@
 'use strict'
 
-const { tspl } = require('@matteo.collina/tspl')
 const { test, after } = require('node:test')
 const { isIP } = require('node:net')
+const { lookup } = require('node:dns')
 const { createServer } = require('node:http')
 const { once } = require('node:events')
+const { setTimeout: sleep } = require('node:timers/promises')
+
+const { tspl } = require('@matteo.collina/tspl')
 
 const { interceptors, Agent } = require('../..')
 const { dns } = interceptors
@@ -479,4 +482,214 @@ test('Should automatically resolve IPs (dual stack disabled - 6)', async t => {
 
   t.equal(response2.statusCode, 200)
   t.equal(await response2.body.text(), 'hello world!')
+})
+
+test('Should we handle TTL (4)', async t => {
+  t = tspl(t, { plan: 7 })
+
+  let counter = 0
+  let lookupCounter = 0
+  const server = createServer()
+  const requestOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  server.on('request', (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' })
+    res.end('hello world!')
+  })
+
+  server.listen(0, '127.0.0.1')
+
+  await once(server, 'listening')
+
+  const client = new Agent().compose([
+    dispatch => {
+      return (opts, handler) => {
+        ++counter
+        const url = new URL(opts.origin)
+
+        switch (counter) {
+          case 1:
+            t.equal(isIP(url.hostname), 4)
+            break
+
+          case 2:
+            t.equal(isIP(url.hostname), 4)
+            break
+          default:
+            t.fail('should not reach this point')
+        }
+
+        return dispatch(opts, handler)
+      }
+    },
+    dns({
+      dualStack: false,
+      affinity: 4,
+      maxTTL: 100,
+      lookup: (origin, opts, cb) => {
+        ++lookupCounter
+        lookup(
+          origin.hostname,
+          { all: true, family: opts.affinity },
+          (err, addresses) => {
+            if (err) {
+              return cb(err)
+            }
+
+            const results = []
+
+            for (const addr of addresses) {
+              const record = {
+                address: addr.address,
+                ttl: opts.maxTTL,
+                family: addr.family
+              }
+
+              results.push(record)
+            }
+
+            cb(null, results)
+          }
+        )
+      }
+    })
+  ])
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request({
+    ...requestOptions,
+    origin: `http://localhost:${server.address().port}`
+  })
+
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+
+  await sleep(200)
+
+  const response2 = await client.request({
+    ...requestOptions,
+    origin: `http://localhost:${server.address().port}`
+  })
+
+  t.equal(response2.statusCode, 200)
+  t.equal(await response2.body.text(), 'hello world!')
+  t.equal(lookupCounter, 2)
+})
+
+test('Should we handle TTL (6)', async t => {
+  t = tspl(t, { plan: 7 })
+
+  let counter = 0
+  let lookupCounter = 0
+  const server = createServer()
+  const requestOptions = {
+    method: 'GET',
+    path: '/',
+    headers: {
+      'content-type': 'application/json'
+    }
+  }
+
+  server.on('request', (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' })
+    res.end('hello world!')
+  })
+
+  server.listen(0, '::1')
+
+  await once(server, 'listening')
+
+  const client = new Agent().compose([
+    dispatch => {
+      return (opts, handler) => {
+        ++counter
+        const url = new URL(opts.origin)
+
+        switch (counter) {
+          case 1:
+            // [::1] -> ::1
+            t.equal(isIP(url.hostname.slice(1, 4)), 6)
+            break
+
+          case 2:
+            // [::1] -> ::1
+            t.equal(isIP(url.hostname.slice(1, 4)), 6)
+            break
+          default:
+            t.fail('should not reach this point')
+        }
+
+        return dispatch(opts, handler)
+      }
+    },
+    dns({
+      dualStack: false,
+      affinity: 6,
+      maxTTL: 100,
+      lookup: (origin, opts, cb) => {
+        ++lookupCounter
+        lookup(
+          origin.hostname,
+          { all: true, family: opts.affinity },
+          (err, addresses) => {
+            if (err) {
+              return cb(err)
+            }
+
+            const results = []
+
+            for (const addr of addresses) {
+              const record = {
+                address: addr.address,
+                ttl: opts.maxTTL,
+                family: addr.family
+              }
+
+              results.push(record)
+            }
+
+            cb(null, results)
+          }
+        )
+      }
+    })
+  ])
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  const response = await client.request({
+    ...requestOptions,
+    origin: `http://localhost:${server.address().port}`
+  })
+
+  t.equal(response.statusCode, 200)
+  t.equal(await response.body.text(), 'hello world!')
+
+  await sleep(200)
+
+  const response2 = await client.request({
+    ...requestOptions,
+    origin: `http://localhost:${server.address().port}`
+  })
+
+  t.equal(response2.statusCode, 200)
+  t.equal(await response2.body.text(), 'hello world!')
+  t.equal(lookupCounter, 2)
 })
