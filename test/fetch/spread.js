0d00e468cb84b66d83635fa2920b19e3697c07dd
Khafra <maitken033380023@gmail.com>
2024-09-23 01:59:36 -0400

    cleanup web symbol removal (#3638)

--- test/fetch/spread.js
+++ test/fetch/spread.js
@@ -3,6 +3,7 @@
 const undici = require('../..')
 const { test } = require('node:test')
 const assert = require('node:assert')
+const { inspect } = require('node:util')
 
 test('spreading web classes yields empty objects', (t) => {
   for (const object of [
@@ -11,5 +12,30 @@ test('spreading web classes yields empty objects', (t) => {
     new undici.Request('http://a')
   ]) {
     assert.deepStrictEqual({ ...object }, {})
+  }
+})
+
+test('Objects only have an expected set of symbols on their prototypes', (t) => {
+  const allowedSymbols = [
+    Symbol.iterator,
+    Symbol.toStringTag,
+    inspect.custom
+  ]
+
+  for (const object of [
+    undici.FormData,
+    undici.Response,
+    undici.Request,
+    undici.Headers,
+    undici.WebSocket,
+    undici.MessageEvent,
+    undici.CloseEvent,
+    undici.ErrorEvent,
+    undici.EventSource
+  ]) {
+    const symbols = Object.keys(Object.getOwnPropertyDescriptors(object.prototype))
+      .filter(v => typeof v === 'symbol')
+
+    assert(symbols.every(symbol => allowedSymbols.includes(symbol)))
   }
 })
