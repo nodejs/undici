998f885bb03fc10dfc302a3a3b26a7979291a0ca
Aras Abbasi <aras.abbasi@googlemail.com>
2024-10-03 15:16:33 +0200

    ensure that http.maxHeadersize is valid

--- test/client-node-max-header-size.js
+++ test/client-node-max-header-size.js
@@ -41,4 +41,23 @@ describe("Node.js' --max-http-header-size cli option", () => {
 
     await t.completed
   })
+
+  test('--max-http-header-size with Client API', async (t) => {
+    t = tspl(t, { plan: 6 })
+    const command = 'node -e "new (require(\'.\').Client)(new URL(\'http://localhost:200\'))"'
+
+    exec(`${command} --max-http-header-size=0`, { stdio: 'pipe' }, (err, stdout, stderr) => {
+      t.strictEqual(err.code, 1)
+      t.strictEqual(stdout, '')
+      t.match(stderr, /http module not available or http.maxHeaderSize invalid/, '--max-http-header-size=0 should result in an Error when using the Client API')
+    })
+
+    exec(command, { stdio: 'pipe' }, (err, stdout, stderr) => {
+      t.ifError(err)
+      t.strictEqual(stdout, '')
+      t.strictEqual(stderr, '', 'default max-http-header-size should not throw')
+    })
+
+    await t.completed
+  })
 })
