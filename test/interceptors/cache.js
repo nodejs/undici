nodejs/undici

commit 68107da3fdd6cd0397f4800a2bc9630c802eda58

[http-cache] follow up (#3733)
* followup http-cache

* fix methods check

* avoid instantiation in case of error

* improve

* rename assertCacheStoreType to assertCacheStore

* remove validation

* remove fallback

* fix

* fix

lib/handler/cache-handler.js
@@ -2,24 +2,35 @@
 
 const util = require('../core/util')
 const DecoratorHandler = require('../handler/decorator-handler')
-const { parseCacheControlHeader, parseVaryHeader, UNSAFE_METHODS, assertCacheStoreType } = require('../util/cache')
+const {
+  parseCacheControlHeader,
+  parseVaryHeader
+} = require('../util/cache')
 
 /**
  * Writes a response to a CacheStore and then passes it on to the next handler
  */
 class CacheHandler extends DecoratorHandler {
   /**
-   * @type {import('../../types/cache-interceptor.d.ts').default.CacheOptions | null}
-  */
-  #opts = null
-  /**
-   * @type {import('../../types/dispatcher.d.ts').default.RequestOptions | null}
+   * @type {import('../../types/cache-interceptor.d.ts').default.CacheStore}
    */
-  #req = null
+  #store
+
   /**
-   * @type {import('../../types/dispatcher.d.ts').default.DispatchHandlers | null}
+   * @type {import('../../types/cache-interceptor.d.ts').default.CacheMethods}
    */
-  #handler = null
+  #methods
+
+  /**
+   * @type {import('../../types/dispatcher.d.ts').default.RequestOptions}
+   */
+  #requestOptions
+
+  /**
+   * @type {import('../../types/dispatcher.d.ts').default.DispatchHandlers}
+   */
+  #handler
+
   /**
    * @type {import('../../types/cache-interceptor.d.ts').default.CacheStoreWriteable | undefined}
    */
@@ -27,29 +38,18 @@ class CacheHandler extends DecoratorHandler {
 
   /**
    * @param {import('../../types/cache-interceptor.d.ts').default.CacheOptions} opts
-   * @param {import('../../types/dispatcher.d.ts').default.RequestOptions} req
+   * @param {import('../../types/dispatcher.d.ts').default.RequestOptions} requestOptions
    * @param {import('../../types/dispatcher.d.ts').default.DispatchHandlers} handler
    */
-  constructor (opts, req, handler) {
+  constructor (opts, requestOptions, handler) {
+    const { store, methods } = opts
+
     super(handler)
 
-    if (typeof opts !== 'object') {
-      throw new TypeError(`expected opts to be an object, got type ${typeof opts}`)
-    }
-
-    assertCacheStoreType(opts.store)
-
-    if (typeof req !== 'object') {
-      throw new TypeError(`expected req to be an object, got type ${typeof opts}`)
-    }
-
-    if (typeof handler !== 'object') {
-      throw new TypeError(`expected handler to be an object, got type ${typeof opts}`)
-    }
-
-    this.#opts = opts
-    this.#req = req
+    this.#store = store
+    this.#requestOptions = requestOptions
     this.#handler = handler
+    this.#methods = methods
   }
 
   /**
@@ -75,14 +75,14 @@ class CacheHandler extends DecoratorHandler {
     )
 
     if (
-      UNSAFE_METHODS.includes(this.#req.method) &&
+      !this.#methods.includes(this.#requestOptions.method) &&
       statusCode >= 200 &&
       statusCode <= 399
     ) {
       // https://www.rfc-editor.org/rfc/rfc9111.html#name-invalidating-stored-respons
       // Try/catch for if it's synchronous
       try {
-        const result = this.#opts.store.deleteByOrigin(this.#req.origin)
+        const result = this.#store.deleteByOrigin(this.#requestOptions.origin)
         if (
           result &&
           typeof result.catch === 'function' &&
@@ -103,7 +103,7 @@ class CacheHandler extends DecoratorHandler {
     const cacheControlHeader = headers['cache-control']
     const contentLengthHeader = headers['content-length']
 
-    if (!cacheControlHeader || !contentLengthHeader || this.#opts.store.isFull) {
+    if (!cacheControlHeader || !contentLengthHeader || this.#store.isFull) {
       // Don't have the headers we need, can't cache
       return downstreamOnHeaders()
     }
@@ -122,7 +122,7 @@ class CacheHandler extends DecoratorHandler {
     const staleAt = determineStaleAt(now, headers, cacheControlDirectives)
     if (staleAt) {
       const varyDirectives = headers.vary
-        ? parseVaryHeader(headers.vary, this.#req.headers)
+        ? parseVaryHeader(headers.vary, this.#requestOptions.headers)
         : undefined
       const deleteAt = determineDeleteAt(now, cacheControlDirectives, staleAt)
 
@@ -132,7 +132,7 @@ class CacheHandler extends DecoratorHandler {
         cacheControlDirectives
       )
 
-      this.#writeStream = this.#opts.store.createWriteStream(this.#req, {
+      this.#writeStream = this.#store.createWriteStream(this.#requestOptions, {
         statusCode,
         statusMessage,
         rawHeaders: strippedHeaders,

lib/interceptor/cache.js
@@ -4,39 +4,31 @@ const util = require('../core/util')
 const CacheHandler = require('../handler/cache-handler')
 const MemoryCacheStore = require('../cache/memory-cache-store')
 const CacheRevalidationHandler = require('../handler/cache-revalidation-handler')
-const { UNSAFE_METHODS, assertCacheStoreType } = require('../util/cache.js')
+const { assertCacheStore, assertCacheMethods } = require('../util/cache.js')
 
 const AGE_HEADER = Buffer.from('age')
 
 /**
- * @param {import('../../types/cache-interceptor.d.ts').default.CacheOptions | undefined} globalOpts
+ * @param {import('../../types/cache-interceptor.d.ts').default.CacheOptions} [opts]
  * @returns {import('../../types/dispatcher.d.ts').default.DispatcherComposeInterceptor}
  */
-module.exports = globalOpts => {
-  if (!globalOpts) {
-    globalOpts = {}
+module.exports = (opts = {}) => {
+  const {
+    store = new MemoryCacheStore(),
+    methods = ['GET']
+  } = opts
+
+  if (typeof opts !== 'object' || opts === null) {
+    throw new TypeError(`expected type of opts to be an Object, got ${opts === null ? 'null' : typeof opts}`)
   }
 
-  if (globalOpts.store) {
-    assertCacheStoreType(globalOpts.store)
-  } else {
-    globalOpts.store = new MemoryCacheStore()
+  assertCacheStore(store, 'opts.store')
+  assertCacheMethods(methods, 'opts.methods')
+
+  const globalOpts = {
+    store,
+    methods
   }
-
-  if (globalOpts.methods) {
-    if (!Array.isArray(globalOpts.methods)) {
-      throw new TypeError(`methods needs to be an array, got ${typeof globalOpts.methods}`)
-    }
-
-    if (globalOpts.methods.length === 0) {
-      throw new Error('methods must have at least one method in it')
-    }
-  } else {
-    globalOpts.methods = ['GET']
-  }
-
-  // Safe methods the user wants and unsafe methods
-  const methods = [...globalOpts.methods, ...UNSAFE_METHODS]
 
   return dispatch => {
     return (opts, handler) => {
@@ -45,7 +37,7 @@ module.exports = globalOpts => {
         return dispatch(opts, handler)
       }
 
-      const stream = globalOpts.store.createReadStream(opts)
+      const stream = store.createReadStream(opts)
       if (!stream) {
         // Request isn't cached
         return dispatch(opts, new CacheHandler(globalOpts, opts, handler))
@@ -169,7 +161,7 @@ module.exports = globalOpts => {
         respondWithCachedValue(stream, value)
       }
 
-      Promise.resolve(stream).then(handleStream).catch(err => handler.onError(err))
+      Promise.resolve(stream).then(handleStream).catch(handler.onError)
 
       return true
     }

lib/util/cache.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const UNSAFE_METHODS = /** @type {const} */ ([
-  'POST', 'PUT', 'PATCH', 'DELETE'
-])
+const {
+  safeHTTPMethods
+} = require('../core/util')
 
 /**
  * @see https://www.rfc-editor.org/rfc/rfc9111.html#name-cache-control
@@ -181,25 +181,44 @@ function parseVaryHeader (varyHeader, headers) {
  * @param {unknown} store
  * @returns {asserts store is import('../../types/cache-interceptor.d.ts').default.CacheStore}
  */
-function assertCacheStoreType (store) {
+function assertCacheStore (store, name = 'CacheStore') {
   if (typeof store !== 'object' || store === null) {
-    throw new TypeError(`expected type to be an store, got ${typeof store}`)
+    throw new TypeError(`expected type of ${name} to be a CacheStore, got ${store === null ? 'null' : typeof store}`)
   }
 
   for (const fn of ['createReadStream', 'createWriteStream', 'deleteByOrigin']) {
     if (typeof store[fn] !== 'function') {
-      throw new TypeError(`CacheStore needs a \`${fn}()\` function`)
+      throw new TypeError(`${name} needs to have a \`${fn}()\` function`)
     }
   }
 
   if (typeof store.isFull !== 'boolean') {
-    throw new TypeError(`CacheStore needs a isFull getter with type boolean, current type: ${typeof store.isFull}`)
+    throw new TypeError(`${name} needs a isFull getter with type boolean, current type: ${typeof store.isFull}`)
+  }
+}
+/**
+ * @param {unknown} methods
+ * @returns {asserts methods is import('../../types/cache-interceptor.d.ts').default.CacheMethods[]}
+ */
+function assertCacheMethods (methods, name = 'CacheMethods') {
+  if (!Array.isArray(methods)) {
+    throw new TypeError(`expected type of ${name} needs to be an array, got ${methods === null ? 'null' : typeof methods}`)
+  }
+
+  if (methods.length === 0) {
+    throw new TypeError(`${name} needs to have at least one method`)
+  }
+
+  for (const method of methods) {
+    if (!safeHTTPMethods.includes(method)) {
+      throw new TypeError(`element of ${name}-array needs to be one of following values: ${safeHTTPMethods.join(', ')}, got ${method}`)
+    }
   }
 }
 
 module.exports = {
-  UNSAFE_METHODS,
   parseCacheControlHeader,
   parseVaryHeader,
-  assertCacheStoreType
+  assertCacheMethods,
+  assertCacheStore
 }

lib/core/util.js
@@ -924,6 +924,6 @@ module.exports = {
   isHttpOrHttpsPrefixed,
   nodeMajor,
   nodeMinor,
-  safeHTTPMethods: ['GET', 'HEAD', 'OPTIONS', 'TRACE'],
+  safeHTTPMethods: Object.freeze(['GET', 'HEAD', 'OPTIONS', 'TRACE']),
   wrapRequestBody
 }

lib/handler/cache-revalidation-handler.js
@@ -32,11 +32,11 @@ class CacheRevalidationHandler extends DecoratorHandler {
    * @param {import('../../types/dispatcher.d.ts').default.DispatchHandlers} handler
    */
   constructor (successCallback, handler) {
-    super(handler)
-
     if (typeof successCallback !== 'function') {
       throw new TypeError('successCallback must be a function')
     }
+
+    super(handler)
 
     this.#successCallback = successCallback
     this.#handler = handler

types/cache-interceptor.d.ts
@@ -4,6 +4,8 @@ import Dispatcher from './dispatcher'
 export default CacheHandler
 
 declare namespace CacheHandler {
+  export type CacheMethods = 'GET' | 'HEAD' | 'OPTIONS' | 'TRACE'
+
   export interface CacheOptions {
     store?: CacheStore
 
@@ -14,7 +16,7 @@ declare namespace CacheHandler {
      * @see https://www.rfc-editor.org/rfc/rfc9111.html#name-invalidating-stored-respons
      * @see https://www.rfc-editor.org/rfc/rfc9110#section-9.2.1
      */
-    methods?: ('GET' | 'HEAD' | 'OPTIONS' | 'TRACE')[]
+    methods?: CacheMethods[]
   }
 
   /**

commit f2213801bde64193aca31a371f669150c8b3db39

fix: unsafe methods not causing cache purge
Fixes bug introduced in #3733 where unsafe methods no longer make it to
the cache handler and cause a cache purge for an origin.

Also removes a duplicate test file.

Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

lib/handler/cache-handler.js
@@ -17,11 +17,6 @@ class CacheHandler extends DecoratorHandler {
   #store
 
   /**
-   * @type {import('../../types/cache-interceptor.d.ts').default.CacheMethods}
-   */
-  #methods
-
-  /**
    * @type {import('../../types/dispatcher.d.ts').default.RequestOptions}
    */
   #requestOptions
@@ -42,14 +37,13 @@ class CacheHandler extends DecoratorHandler {
    * @param {import('../../types/dispatcher.d.ts').default.DispatchHandlers} handler
    */
   constructor (opts, requestOptions, handler) {
-    const { store, methods } = opts
+    const { store } = opts
 
     super(handler)
 
     this.#store = store
     this.#requestOptions = requestOptions
     this.#handler = handler
-    this.#methods = methods
   }
 
   /**
@@ -75,7 +69,7 @@ class CacheHandler extends DecoratorHandler {
     )
 
     if (
-      !this.#methods.includes(this.#requestOptions.method) &&
+      !util.safeHTTPMethods.includes(this.#requestOptions.method) &&
       statusCode >= 200 &&
       statusCode <= 399
     ) {

lib/interceptor/cache.js
@@ -32,7 +32,7 @@ module.exports = (opts = {}) => {
 
   return dispatch => {
     return (opts, handler) => {
-      if (!opts.origin || !methods.includes(opts.method)) {
+      if (!opts.origin || (opts.method in util.safeHTTPMethods && !methods.includes(opts.method))) {
         // Not a method we want to cache or we don't have the origin, skip
         return dispatch(opts, handler)
       }

test/cache-interceptor/interceptor.js
@@ -1,240 +1,0 @@
-'use strict'
-
-const { describe, test, after } = require('node:test')
-const { strictEqual, notEqual, fail } = require('node:assert')
-const { createServer } = require('node:http')
-const { once } = require('node:events')
-const { Client, interceptors, cacheStores } = require('../../index')
-
-describe('Cache Interceptor', () => {
-  test('doesn\'t cache request w/ no cache-control header', async () => {
-    let requestsToOrigin = 0
-
-    const server = createServer((_, res) => {
-      requestsToOrigin++
-      res.end('asd')
-    }).listen(0)
-
-    after(() => server.close())
-    await once(server, 'listening')
-
-    const client = new Client(`http://localhost:${server.address().port}`)
-      .compose(interceptors.cache())
-
-    strictEqual(requestsToOrigin, 0)
-
-    // Send initial request. This should reach the origin
-    let response = await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/'
-    })
-    strictEqual(requestsToOrigin, 1)
-    strictEqual(await response.body.text(), 'asd')
-
-    // Send second request that should be handled by cache
-    response = await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/'
-    })
-    strictEqual(requestsToOrigin, 2)
-    strictEqual(await response.body.text(), 'asd')
-  })
-
-  test('caches request successfully', async () => {
-    let requestsToOrigin = 0
-
-    const server = createServer((_, res) => {
-      requestsToOrigin++
-      res.setHeader('cache-control', 'public, s-maxage=10')
-      res.end('asd')
-    }).listen(0)
-
-    after(() => server.close())
-    await once(server, 'listening')
-
-    const client = new Client(`http://localhost:${server.address().port}`)
-      .compose(interceptors.cache())
-
-    strictEqual(requestsToOrigin, 0)
-
-    // Send initial request. This should reach the origin
-    let response = await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/'
-    })
-    strictEqual(requestsToOrigin, 1)
-    strictEqual(await response.body.text(), 'asd')
-
-    // Send second request that should be handled by cache
-    response = await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/'
-    })
-    strictEqual(requestsToOrigin, 1)
-    strictEqual(await response.body.text(), 'asd')
-    strictEqual(response.headers.age, '0')
-  })
-
-  test('respects vary header', async () => {
-    let requestsToOrigin = 0
-
-    const server = createServer((req, res) => {
-      requestsToOrigin++
-      res.setHeader('cache-control', 'public, s-maxage=10')
-      res.setHeader('vary', 'some-header, another-header')
-
-      if (req.headers['some-header'] === 'abc123') {
-        res.end('asd')
-      } else {
-        res.end('dsa')
-      }
-    }).listen(0)
-
-    after(() => server.close())
-    await once(server, 'listening')
-
-    const client = new Client(`http://localhost:${server.address().port}`)
-      .compose(interceptors.cache())
-
-    strictEqual(requestsToOrigin, 0)
-
-    // Send initial request. This should reach the origin
-    let response = await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/',
-      headers: {
-        'some-header': 'abc123',
-        'another-header': '123abc'
-      }
-    })
-    strictEqual(requestsToOrigin, 1)
-    strictEqual(await response.body.text(), 'asd')
-
-    // Make another request with changed headers, this should miss
-    const secondResponse = await client.request({
-      method: 'GET',
-      path: '/',
-      headers: {
-        'some-header': 'qwerty',
-        'another-header': 'asdfg'
-      }
-    })
-    strictEqual(requestsToOrigin, 2)
-    strictEqual(await secondResponse.body.text(), 'dsa')
-
-    // Resend the first request again which should still be cahced
-    response = await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/',
-      headers: {
-        'some-header': 'abc123',
-        'another-header': '123abc'
-      }
-    })
-    strictEqual(requestsToOrigin, 2)
-    strictEqual(await response.body.text(), 'asd')
-  })
-
-  test('revalidates request when needed', async () => {
-    let requestsToOrigin = 0
-
-    const server = createServer((req, res) => {
-      res.setHeader('cache-control', 'public, s-maxage=1, stale-while-revalidate=10')
-
-      requestsToOrigin++
-
-      if (requestsToOrigin > 1) {
-        notEqual(req.headers['if-modified-since'], undefined)
-
-        if (requestsToOrigin === 3) {
-          res.end('asd123')
-        } else {
-          res.statusCode = 304
-          res.end()
-        }
-      } else {
-        res.end('asd')
-      }
-    }).listen(0)
-
-    after(() => server.close())
-    await once(server, 'listening')
-
-    const client = new Client(`http://localhost:${server.address().port}`)
-      .compose(interceptors.cache())
-
-    strictEqual(requestsToOrigin, 0)
-
-    const request = {
-      origin: 'localhost',
-      method: 'GET',
-      path: '/'
-    }
-
-    // Send initial request. This should reach the origin
-    let response = await client.request(request)
-    strictEqual(requestsToOrigin, 1)
-    strictEqual(await response.body.text(), 'asd')
-
-    // Now we send two more requests. Both of these should reach the origin,
-    //  but now with a conditional header asking if the resource has been
-    //  updated. These need to be ran after the response is stale.
-    const completed = new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          // No update for the second request
-          response = await client.request(request)
-          strictEqual(requestsToOrigin, 2)
-          strictEqual(await response.body.text(), 'asd')
-
-          // This should be updated, even though the value isn't expired.
-          response = await client.request(request)
-          strictEqual(requestsToOrigin, 3)
-          strictEqual(await response.body.text(), 'asd123')
-
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
-      }, 1500)
-    })
-    await completed
-  })
-
-  test('respects cache store\'s isFull property', async () => {
-    const server = createServer((_, res) => {
-      res.end('asd')
-    }).listen(0)
-
-    after(() => server.close())
-    await once(server, 'listening')
-
-    const store = new cacheStores.MemoryCacheStore()
-    Object.defineProperty(store, 'isFull', {
-      value: true
-    })
-
-    store.createWriteStream = (...args) => {
-      fail('shouln\'t have reached this')
-    }
-
-    const client = new Client(`http://localhost:${server.address().port}`)
-      .compose(interceptors.cache({ store }))
-
-    await client.request({
-      origin: 'localhost',
-      method: 'GET',
-      path: '/',
-      headers: {
-        'some-header': 'abc123',
-        'another-header': '123abc'
-      }
-    })
-  })
-})

test/interceptors/cache.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { describe, test, after } = require('node:test')
-const { strictEqual, notEqual, fail } = require('node:assert')
+const { strictEqual, notEqual, fail, equal } = require('node:assert')
 const { createServer } = require('node:http')
 const { once } = require('node:events')
 const FakeTimers = require('@sinonjs/fake-timers')
@@ -249,5 +249,42 @@ describe('Cache Interceptor', () => {
         'another-header': '123abc'
       }
     })
+  })
+
+  test('unsafe methods call the store\'s deleteByOrigin function', async () => {
+    const server = createServer((_, res) => {
+      res.end('asd')
+    }).listen(0)
+
+    after(() => server.close())
+    await once(server, 'listening')
+
+    let deleteByOriginCalled = false
+    const store = new cacheStores.MemoryCacheStore()
+
+    const originalDeleteByOrigin = store.deleteByOrigin.bind(store)
+    store.deleteByOrigin = (origin) => {
+      deleteByOriginCalled = true
+      originalDeleteByOrigin(origin)
+    }
+
+    const client = new Client(`http://localhost:${server.address().port}`)
+      .compose(interceptors.cache({ store }))
+
+    await client.request({
+      origin: 'localhost',
+      method: 'GET',
+      path: '/'
+    })
+
+    equal(deleteByOriginCalled, false)
+
+    await client.request({
+      origin: 'localhost',
+      method: 'DELETE',
+      path: '/'
+    })
+
+    equal(deleteByOriginCalled, true)
   })
 })