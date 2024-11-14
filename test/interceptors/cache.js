799d7ea3f9be5f6cd82411f0a540ec762afd83b4
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-11-13 21:28:02 -0800

    feat: support request cache control directives
    Co-authored-by: Robert Nagy <ronagy@icloud.com>
    
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- test/interceptors/cache.js
+++ test/interceptors/cache.js
@@ -338,4 +338,408 @@ describe('Cache Interceptor', () => {
     const response = await client.request(request)
     strictEqual(await response.body.text(), 'asd')
   })
+
+  test('requests w/ unsafe methods never get cached', async () => {
+    const server = createServer((req, res) => {
+      res.setHeader('cache-control', 'public, s-maxage=1')
+      res.end('asd')
+    }).listen(0)
+
+    after(() => server.close())
+
+    const client = new Client(`http://localhost:${server.address().port}`)
+      .compose(interceptors.cache({
+        store: {
+          get () {
+            return undefined
+          },
+          createWriteStream (key) {
+            fail(key.method)
+          },
+          delete () {}
+        }
+      }))
+
+    for (const method of ['POST', 'PUT', 'PATCH', 'DELETE']) {
+      await client.request({
+        origin: 'localhost',
+        method,
+        path: '/'
+      })
+    }
+  })
+
+  describe('Client-side directives', () => {
+    test('max-age', async () => {
+      const clock = FakeTimers.install({
+        shouldClearNativeTimers: true
+      })
+      tick(0)
+
+      let requestsToOrigin = 0
+      const server = createServer((_, res) => {
+        requestsToOrigin++
+        res.setHeader('cache-control', 'public, s-maxage=100')
+        res.end('asd')
+      }).listen(0)
+
+      const client = new Client(`http://localhost:${server.address().port}`)
+        .compose(interceptors.cache())
+
+      after(async () => {
+        clock.uninstall()
+        server.close()
+        await client.close()
+      })
+
+      await once(server, 'listening')
+
+      strictEqual(requestsToOrigin, 0)
+
+      // Send initial request. This should reach the origin
+      let response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/'
+      })
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Send second request that should be handled by cache
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/'
+      })
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+      strictEqual(response.headers.age, '0')
+
+      // Send third request w/ the directive, this should be handled by the cache
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/',
+        headers: {
+          'cache-control': 'max-age=5'
+        }
+      })
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      clock.tick(6000)
+      tick(6000)
+
+      // Send fourth request w/ the directive, age should be 6 now so this
+      //  should hit the origin
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/',
+        headers: {
+          'cache-control': 'max-age=5'
+        }
+      })
+      strictEqual(requestsToOrigin, 2)
+      strictEqual(await response.body.text(), 'asd')
+    })
+
+    test('max-stale', async () => {
+      let requestsToOrigin = 0
+
+      const clock = FakeTimers.install({
+        shouldClearNativeTimers: true
+      })
+      tick(0)
+
+      const server = createServer((req, res) => {
+        res.setHeader('cache-control', 'public, s-maxage=1, stale-while-revalidate=10')
+
+        if (requestsToOrigin === 1) {
+          notEqual(req.headers['if-modified-since'], undefined)
+
+          res.statusCode = 304
+          res.end()
+        } else {
+          res.end('asd')
+        }
+
+        requestsToOrigin++
+      }).listen(0)
+
+      const client = new Client(`http://localhost:${server.address().port}`)
+        .compose(interceptors.cache())
+
+      after(async () => {
+        server.close()
+        await client.close()
+        clock.uninstall()
+      })
+
+      await once(server, 'listening')
+
+      strictEqual(requestsToOrigin, 0)
+
+      const request = {
+        origin: 'localhost',
+        method: 'GET',
+        path: '/'
+      }
+
+      // Send initial request. This should reach the origin
+      let response = await client.request(request)
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      clock.tick(1500)
+      tick(1500)
+
+      // Now we send a second request. This should be within the max stale
+      //  threshold, so a request shouldn't be made to the origin
+      response = await client.request({
+        ...request,
+        headers: {
+          'cache-control': 'max-stale=5'
+        }
+      })
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Send a third request. This shouldn't be within the max stale threshold
+      //  so a request should be made to the origin
+      response = await client.request({
+        ...request,
+        headers: {
+          'cache-control': 'max-stale=0'
+        }
+      })
+      strictEqual(requestsToOrigin, 2)
+      strictEqual(await response.body.text(), 'asd')
+    })
+
+    test('min-fresh', async () => {
+      let requestsToOrigin = 0
+
+      const clock = FakeTimers.install({
+        shouldClearNativeTimers: true
+      })
+      tick(0)
+
+      const server = createServer((req, res) => {
+        requestsToOrigin++
+        res.setHeader('cache-control', 'public, s-maxage=10')
+        res.end('asd')
+      }).listen(0)
+
+      const client = new Client(`http://localhost:${server.address().port}`)
+        .compose(interceptors.cache())
+
+      after(async () => {
+        server.close()
+        await client.close()
+        clock.uninstall()
+      })
+
+      await once(server, 'listening')
+
+      strictEqual(requestsToOrigin, 0)
+
+      const request = {
+        origin: 'localhost',
+        method: 'GET',
+        path: '/'
+      }
+
+      // Send initial request. This should reach the origin
+      let response = await client.request(request)
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Fast forward more. Response has 8sec TTL left after
+      clock.tick(2000)
+      tick(2000)
+
+      // Now we send a second request. This should be within the threshold, so
+      //  a request shouldn't be made to the origin
+      response = await client.request({
+        ...request,
+        headers: {
+          'cache-control': 'min-fresh=5'
+        }
+      })
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Fast forward more. Response has 2sec TTL left after
+      clock.tick(6000)
+      tick(6000)
+
+      // Send the second request again, this time it shouldn't be within the
+      //  threshold and a request should be made to the origin.
+      response = await client.request({
+        ...request,
+        headers: {
+          'cache-control': 'min-fresh=5'
+        }
+      })
+      strictEqual(requestsToOrigin, 2)
+      strictEqual(await response.body.text(), 'asd')
+    })
+
+    test('no-cache', async () => {
+      let requestsToOrigin = 0
+      const server = createServer((req, res) => {
+        if (requestsToOrigin === 1) {
+          notEqual(req.headers['if-modified-since'], undefined)
+          res.statusCode = 304
+          res.end()
+        } else {
+          res.setHeader('cache-control', 'public, s-maxage=100')
+          res.end('asd')
+        }
+
+        requestsToOrigin++
+      }).listen(0)
+
+      const client = new Client(`http://localhost:${server.address().port}`)
+        .compose(interceptors.cache())
+
+      after(async () => {
+        server.close()
+        await client.close()
+      })
+
+      await once(server, 'listening')
+
+      strictEqual(requestsToOrigin, 0)
+
+      // Send initial request. This should reach the origin
+      let response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/',
+        headers: {
+          'cache-control': 'no-cache'
+        }
+      })
+      strictEqual(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Send second request, a validation request should be sent
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/',
+        headers: {
+          'cache-control': 'no-cache'
+        }
+      })
+      strictEqual(requestsToOrigin, 2)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Send third request w/o no-cache, this should be handled by the cache
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/'
+      })
+      strictEqual(requestsToOrigin, 2)
+      strictEqual(await response.body.text(), 'asd')
+    })
+
+    test('no-store', async () => {
+      const server = createServer((req, res) => {
+        res.setHeader('cache-control', 'public, s-maxage=100')
+        res.end('asd')
+      }).listen(0)
+
+      const store = new cacheStores.MemoryCacheStore()
+      store.createWriteStream = (...args) => {
+        fail('shouln\'t have reached this')
+      }
+
+      const client = new Client(`http://localhost:${server.address().port}`)
+        .compose(interceptors.cache({ store }))
+
+      after(async () => {
+        server.close()
+        await client.close()
+      })
+
+      await once(server, 'listening')
+
+      // Send initial request. This should reach the origin
+      const response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/',
+        headers: {
+          'cache-control': 'no-store'
+        }
+      })
+      strictEqual(await response.body.text(), 'asd')
+    })
+
+    test('only-if-cached', async () => {
+      let requestsToOrigin = 0
+      const server = createServer((_, res) => {
+        res.setHeader('cache-control', 'public, s-maxage=100')
+        res.end('asd')
+        requestsToOrigin++
+      }).listen(0)
+
+      const client = new Client(`http://localhost:${server.address().port}`)
+        .compose(interceptors.cache())
+
+      after(async () => {
+        server.close()
+        await client.close()
+      })
+
+      await once(server, 'listening')
+
+      // Send initial request. This should reach the origin
+      let response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/'
+      })
+      equal(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Send second request, this shouldn't reach the origin
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/',
+        headers: {
+          'cache-control': 'only-if-cached'
+        }
+      })
+      equal(requestsToOrigin, 1)
+      strictEqual(await response.body.text(), 'asd')
+
+      // Send third request to an uncached resource, this should return a 504
+      response = await client.request({
+        origin: 'localhost',
+        method: 'GET',
+        path: '/bla',
+        headers: {
+          'cache-control': 'only-if-cached'
+        }
+      })
+      equal(response.statusCode, 504)
+
+      // Send fourth request to an uncached resource w/ a , this should return a 504
+      response = await client.request({
+        origin: 'localhost',
+        method: 'DELETE',
+        path: '/asd123',
+        headers: {
+          'cache-control': 'only-if-cached'
+        }
+      })
+      equal(response.statusCode, 504)
+    })
+  })
 })
