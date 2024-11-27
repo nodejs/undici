a30fa5ba9d6ccf38e32b9ee0b2df0fda52b78f9c
Matteo Collina <hello@matteocollina.com>
2024-11-27 14:51:21 +0100

    Correctly parse JSON contentType in responseError interceptor
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/interceptors/response-error.js
+++ test/interceptors/response-error.js
@@ -82,3 +82,89 @@ test('should not throw error for ok response', async () => {
   assert.equal(response.statusCode, 200)
   assert.equal(await response.body.text(), 'hello')
 })
+
+test('should throw error for error response, parsing JSON', async () => {
+  const server = createServer()
+
+  server.on('request', (req, res) => {
+    res.writeHead(400, { 'content-type': 'application/json; charset=utf-8' })
+    res.end(JSON.stringify({ message: 'Bad Request' }))
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(responseError())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  let error
+  try {
+    await client.request({
+      method: 'GET',
+      path: '/',
+      headers: {
+        'content-type': 'text/plain'
+      }
+    })
+  } catch (err) {
+    error = err
+  }
+
+  assert.equal(error.statusCode, 400)
+  assert.equal(error.message, 'Response Error')
+  assert.deepStrictEqual(error.body, {
+    message: 'Bad Request'
+  })
+})
+
+test('should throw error for error response, parsing JSON without charset', async () => {
+  const server = createServer()
+
+  server.on('request', (req, res) => {
+    res.writeHead(400, { 'content-type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Bad Request' }))
+  })
+
+  server.listen(0)
+
+  await once(server, 'listening')
+
+  const client = new Client(
+    `http://localhost:${server.address().port}`
+  ).compose(responseError())
+
+  after(async () => {
+    await client.close()
+    server.close()
+
+    await once(server, 'close')
+  })
+
+  let error
+  try {
+    await client.request({
+      method: 'GET',
+      path: '/',
+      headers: {
+        'content-type': 'text/plain'
+      }
+    })
+  } catch (err) {
+    error = err
+  }
+
+  assert.equal(error.statusCode, 400)
+  assert.equal(error.message, 'Response Error')
+  assert.deepStrictEqual(error.body, {
+    message: 'Bad Request'
+  })
+})
