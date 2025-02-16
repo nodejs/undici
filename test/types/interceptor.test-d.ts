a0e76c73a8ecb913beea7e2210e40d12b7c5cf69
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2025-02-16 10:03:54 +0100

    fix(types): add missing DNS interceptor (#4024)

--- test/types/interceptor.test-d.ts
+++ test/types/interceptor.test-d.ts
@@ -1,5 +1,9 @@
 import {expectAssignable} from "tsd";
 import Undici from "../..";
 import Dispatcher from "../../types/dispatcher";
+import Interceptors from "../../types/interceptors";
 
 expectAssignable<Dispatcher.DispatchInterceptor>(Undici.createRedirectInterceptor({ maxRedirections: 3 }))
+
+expectAssignable<Dispatcher.ComposedDispatcher>(new Dispatcher().compose([Interceptors.dns({maxTTL: 2_000})]));
+
