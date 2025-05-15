1e864c0a44f4ab2d54a2639a211af0e439f0ae40
1ly4s0 <67460389+1ly4s0@users.noreply.github.com>
2025-05-15 19:37:06 +0200

    Add tests

---
+++ test/jest/parser-timeout.test.js
@@ -0,0 +1,39 @@
+// test/jest/parser-timeout.test.js
+const EventEmitter = require('events')
+const connectH1 = require('../../lib/dispatcher/client-h1')
+const { kParser } = require('../../lib/core/symbols')
+
+// DummySocket extends EventEmitter to support .on/.off/.read()
+class DummySocket extends EventEmitter {
+    constructor() {
+        super()
+        this.destroyed = false
+        this.errored = null
+    }
+    read() {
+        return null
+    }
+}
+
+const dummyClient = {
+    [Symbol.for('kMaxHeadersSize')]: 1024,
+    [Symbol.for('kMaxResponseSize')]: 1024,
+    [Symbol.for('kQueue')]: [],
+    [Symbol.for('kRunningIdx')]: 0,
+    headersTimeout: 100,
+    bodyTimeout: 100
+}
+
+describe('Parser#setTimeout under fake timers', () => {
+    beforeEach(() => jest.useFakeTimers('modern'))
+    afterEach(() => jest.useRealTimers())
+
+    it('does not throw when calling setTimeout under fake timers', async () => {
+        const socket = new DummySocket()
+        // connectH1 will internally create Parser and assign it to socket[kParser]
+        await connectH1(dummyClient, socket)
+        const parser = socket[kParser]
+
+        expect(() => parser.setTimeout(200, 0)).not.toThrow()
+    })
+})
