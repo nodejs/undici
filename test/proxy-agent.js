4beaafdd710ebf58c85c7f4684e386bace475d7a
⭐caitp⭐ <caitp@users.noreply.github.com>
2025-05-20 06:47:37 +0000

    feat(ProxyAgent): match Curl behavior in HTTP->HTTP Proxy connections (#4180)
    * feat(ProxyAgent): match Curl behaviour for http-http Proxy connections
    
    Curl does not send a CONNECT request for to a Proxy server, by default,
    for cleartext communications to an endpoint, via a cleartext connection
    to a Proxy. It permits forcing a CONNECT request to be sent via the
    --tunnelproxy parameter.
    
    This change modifies ProxyAgent's constructor to accept a `tunnelProxy`
    option, sends a CONNECT if either `tunnelProxy` is true, or either the
    Proxy or endpoint use a non-http: protocol.
    
    Disabling tunneling for HTTP->HTTP by default would be a breaking change, so
    currently, the tunneling behaviour requires an opt-out. This may change depending
    on feedback during code review.
    
    This adds a new test case which explicitly disables tunneling for an HTTP->HTTP
    connection, and asserts that no CONNECT message is sent to the server or proxy,
    and that the expected HTTP request is sent to the proxy.
    
    Closes #4083
    
    * Part 2
    
    This version tries to expose less sketchiness -- it's not particularly well organized yet, and
    I'm sure it could be cleaned up a lot.
    
    Instead of adding the "rawSocket" stuff to RequestOptions, there's a new wrapper ProxyClient added,
    which intercepts the CONNECT message and prevents it from being dispatched.
    
    Unfortunately the wrapper client isn't quite written in a way to manage all of the client-ness,
    so ProxyAgent is still responsible for updating the PATH of HTTP->HTTP Proxy requests to include
    the endpoint domain.
    
    It is messy though, admittedly.
    
    * remove rawSocket from Dispatcher type definition
    
    * Add some docs
    
    * rename to proxyTunnel to match CURL
    
    * Rename  to  in the docs, too
    
    * Try to clarify the docs a bit
    
    initially just wanted to fix a typo, but thought maybe the original explanation wasn't great.
    
    (cherry picked from commit 95fd9d3feebcdede11a4e6b66f57aac34abf482f)

--- test/proxy-agent.js
+++ test/proxy-agent.js
@@ -793,6 +793,53 @@ test('Proxy via HTTP to HTTP endpoint', async (t) => {
   proxyAgent.close()
 })
 
+test('Proxy via HTTP to HTTP endpoint with tunneling disabled', async (t) => {
+  t = tspl(t, { plan: 3 })
+  const server = await buildServer()
+  const proxy = await buildProxy()
+
+  const serverUrl = `http://localhost:${server.address().port}`
+  const proxyUrl = `http://localhost:${proxy.address().port}`
+  const proxyAgent = new ProxyAgent({ uri: proxyUrl, proxyTunnel: false })
+
+  server.on('request', function (req, res) {
+    t.ok(!req.connection.encrypted)
+    const headers = { host: req.headers.host, connection: req.headers.connection }
+    res.end(JSON.stringify(headers))
+  })
+
+  server.on('secureConnection', () => {
+    t.fail('server is http')
+  })
+
+  proxy.on('secureConnection', () => {
+    t.fail('proxy is http')
+  })
+
+  proxy.on('connect', () => {
+    t.fail(true, 'connect to proxy should unreachable if proxyTunnel is false')
+  })
+
+  proxy.on('request', function (req) {
+    const bits = { method: req.method, url: req.url }
+    t.deepStrictEqual(bits, {
+      method: 'GET',
+      url: `${serverUrl}/`
+    })
+  })
+
+  const data = await request(serverUrl, { dispatcher: proxyAgent })
+  const json = await data.body.json()
+  t.deepStrictEqual(json, {
+    host: `localhost:${server.address().port}`,
+    connection: 'keep-alive'
+  })
+
+  server.close()
+  proxy.close()
+  proxyAgent.close()
+})
+
 test('Proxy via HTTPS to HTTP fails on wrong SNI', async (t) => {
   t = tspl(t, { plan: 2 })
   const server = await buildServer()
