d9a9921991c8d2f53daf0df911e02fc32bd99292
Felix Ezama-Vaughan <ezamabusiness@gmail.com>
2025-05-29 16:54:40 -0600

    snackin

---
+++ example-usage.js
@@ -0,0 +1,14 @@
+const { request } = require('undici')
+const createDecompressInterceptor = require('./lib/interceptor/decompress')
+
+// Create client with decompression
+const client = request.compose(createDecompressInterceptor())
+
+// Now requests will automatically decompress gzip/deflate/brotli responses
+const response = await client('https://httpbin.org/gzip', {
+  headers: {
+    'accept-encoding': 'gzip, deflate, br'
+  }
+})
+
+console.log(await response.body.text()) // Automatically decompressed 
