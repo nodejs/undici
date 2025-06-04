53fd1016a7f074b765d5a5c90db877c851e63768
tsctx <91457664+tsctx@users.noreply.github.com>
2025-06-04 12:56:48 +0000

    Fix several WPT failures

--- test/wpt/server/server.mjs
+++ test/wpt/server/server.mjs
@@ -43,10 +43,17 @@ const server = createServer({ joinDuplicateHeaders: true }, async (req, res) => 
       res.setHeader('content-type', 'text/html')
       // fall through
     }
-    case '/fetch/content-encoding/resources/big.text.gz':
+    case '/fetch/content-encoding/gzip/resources/big.text.gz':
+    case '/fetch/content-encoding/gzip/resources/foo.octetstream.gz':
+    case '/fetch/content-encoding/gzip/resources/foo.text.gz':
+    case '/fetch/content-encoding/br/resources/big.text.br':
+    case '/fetch/content-encoding/br/resources/foo.octetstream.br':
+    case '/fetch/content-encoding/br/resources/foo.text.br':
+    case '/fetch/content-encoding/zstd/resources/big.window.zst':
+    case '/fetch/content-encoding/zstd/resources/big.text.zst':
+    case '/fetch/content-encoding/zstd/resources/foo.octetstream.zst':
+    case '/fetch/content-encoding/zstd/resources/foo.text.zst':
     case '/service-workers/cache-storage/resources/simple.txt':
-    case '/fetch/content-encoding/resources/foo.octetstream.gz':
-    case '/fetch/content-encoding/resources/foo.text.gz':
     case '/fetch/api/resources/cors-top.txt':
     case '/fetch/api/resources/top.txt':
     case '/mimesniff/mime-types/resources/generated-mime-types.json':
@@ -327,9 +334,19 @@ const server = createServer({ joinDuplicateHeaders: true }, async (req, res) => 
 
       break
     }
-    case '/fetch/content-encoding/resources/bad-gzip-body.py': {
+    case '/fetch/content-encoding/gzip/resources/bad-gzip-body.py': {
       res.setHeader('Content-Encoding', 'gzip')
       res.end('not actually gzip')
+      break
+    }
+    case '/fetch/content-encoding/br/resources/bad-br-body.py': {
+      res.setHeader('Content-Encoding', 'br')
+      res.end('not actually br')
+      break
+    }
+    case '/fetch/content-encoding/zstd/resources/bad-zstd-body.py': {
+      res.setHeader('Content-Encoding', 'br')
+      res.end('not actually br')
       break
     }
     case '/fetch/api/resources/dump-authorization-header.py': {
