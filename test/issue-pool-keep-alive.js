3fcd2be3b0119b8fe1f3685c4d4ff1182fd0b9e4
Ethan Arrowood <ethan@arrowood.dev>
2025-07-14 09:14:52 -0600

    demo client working fine, but pool still fails

--- test/issue-pool-keep-alive.js
+++ test/issue-pool-keep-alive.js
@@ -2,9 +2,53 @@
 
 const { tspl } = require('@matteo.collina/tspl')
 const { test, after } = require('node:test')
-const { Pool } = require('..')
+const { Pool, Client } = require('..')
 const { createServer } = require('node:http')
 
+// Client passes okay
+test('respect keep-alive', async (t) => {
+  t = tspl(t, { plan: 1 })
+  const server = createServer({ joinDuplicateHeaders: true, keepAlive: true }, (req, res) => {
+    // Server always responds with a 20s keep-alive header
+    res.writeHead(200, { 'Content-Type': 'text/plain', Connection: 'keep-alive', 'Keep-Alive': 'timeout=20' })
+    res.end('foo')
+  })
+
+  let connections = 0
+  server.on('connection', () => {
+    connections++
+  })
+  after(() => server.close())
+  const expectedConnections = 1
+  const keepAliveTimeout = 20 * 1000 // 20 seconds
+  server.listen(0, () => {
+    const client = new Client(`http://localhost:${server.address().port}`, { keepAliveTimeout, keepAliveMaxTimeout: keepAliveTimeout * 2 })
+    after(() => client.close())
+
+    client.request({ path: '/', method: 'GET' }, (err, res) => {
+      if (err) {
+        t.fail(err)
+        return
+      }
+      res.body.on('end', () => {
+        client.request({ path: '/', method: 'GET' }, (err, res) => {
+          if (err) {
+            t.fail(err)
+            return
+          }
+          setTimeout(() => {
+            res.body.on('end', () => {
+              t.strictEqual(connections, expectedConnections, 'Client connections should have been reused')
+            }).resume()
+          }, keepAliveTimeout / 2)
+        })
+      }).resume()
+    })
+  })
+  await t.completed
+})
+
+// Pool fails
 test('pool respects keep-alive', async t => {
   t = tspl(t, { plan: 1 })
 
@@ -24,6 +68,10 @@ test('pool respects keep-alive', async t => {
   server.listen(0, () => {
     // Pool is set to keep connections alive for 20 seconds too
     const pool = new Pool(`http://localhost:${server.address().port}`, { connections: expectedConnections, keepAliveTimeout, keepAliveMaxTimeout: keepAliveTimeout * 2 })
+    pool.on('connectionError', (err) => {
+      console.error('Connection error');
+      console.error(err);
+    });
     after(() => pool.close())
 
     // Can also use `20`
