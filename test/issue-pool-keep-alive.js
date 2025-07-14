4104c668dbdd706b287daba2823f98910fb26c17
Ethan Arrowood <ethan@arrowood.dev>
2025-07-14 09:24:41 -0600

    add unbatched pool example

--- test/issue-pool-keep-alive.js
+++ test/issue-pool-keep-alive.js
@@ -6,7 +6,7 @@ const { Pool, Client } = require('..')
 const { createServer } = require('node:http')
 
 // Client passes okay
-test('respect keep-alive', async (t) => {
+test('client respects keep-alive', async (t) => {
   t = tspl(t, { plan: 1 })
   const server = createServer({ joinDuplicateHeaders: true, keepAlive: true }, (req, res) => {
     // Server always responds with a 20s keep-alive header
@@ -48,8 +48,52 @@ test('respect keep-alive', async (t) => {
   await t.completed
 })
 
+test.only('Pool respects keep-alive', async t => {
+  t = tspl(t, { plan: 1 })
+
+  const server = createServer({ joinDuplicateHeaders: true, keepAlive: true }, (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain', Connection: 'keep-alive', 'Keep-Alive': 'timeout=2' })
+    res.end('foo')
+  })
+  let connections = 0
+  server.on('connection', () => {
+    connections++
+  })
+  after(() => server.close())
+  const expectedConnections = 1
+  const keepAliveTimeout = 2 * 1000 // 2 seconds
+  server.listen(0, () => {
+    const pool = new Pool(`http://localhost:${server.address().port}`, { connections: expectedConnections, keepAliveTimeout, keepAliveMaxTimeout: keepAliveTimeout * 2 })
+    pool.on('connectionError', (err) => {
+      console.error('Connection error')
+      console.error(err)
+    })
+    after(() => pool.close())
+    pool.request({ path: '/', method: 'GET' }, (err, res) => {
+      if (err) {
+        t.fail(err)
+        return
+      }
+      res.body.on('end', () => {
+        setTimeout(() => {
+          pool.request({ path: '/', method: 'GET' }, (err, res) => {
+            if (err) {
+              t.fail(err)
+              return
+            }
+            res.body.on('end', () => {
+              t.strictEqual(connections, expectedConnections, 'Pool connections should have been reused between batches')
+            }).resume()
+          })
+        }, keepAliveTimeout / 2)
+      }).resume()
+    })
+  })
+  await t.completed
+})
+
 // Pool fails
-test('pool respects keep-alive', async t => {
+test('pool respects keep-alive batched requests', async t => {
   t = tspl(t, { plan: 1 })
 
   const server = createServer({ joinDuplicateHeaders: true, keepAlive: true }, (req, res) => {
@@ -69,9 +113,9 @@ test('pool respects keep-alive', async t => {
     // Pool is set to keep connections alive for 20 seconds too
     const pool = new Pool(`http://localhost:${server.address().port}`, { connections: expectedConnections, keepAliveTimeout, keepAliveMaxTimeout: keepAliveTimeout * 2 })
     pool.on('connectionError', (err) => {
-      console.error('Connection error');
-      console.error(err);
-    });
+      console.error('Connection error')
+      console.error(err)
+    })
     after(() => pool.close())
 
     // Can also use `20`
