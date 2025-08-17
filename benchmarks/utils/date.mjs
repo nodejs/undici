3e8f54fa2b0220e97d8f60a3cb905ef9fb3cbef2
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-17 20:39:42 +0200

    case sensitive

--- benchmarks/utils/date.mjs
+++ benchmarks/utils/date.mjs
@@ -6,7 +6,11 @@ const asctime = 'Sun Nov  6 08:49:37 1994'
 const rfc850 = 'Sunday, 06-Nov-94 08:49:37 GMT'
 const imf = 'Sun, 06 Nov 1994 08:49:37 GMT'
 
-const bench = new Bench({ name: 'simple benchmark', time: 100 })
+console.assert(parseHttpDate(asctime) instanceof Date, 'asctime should return a Date')
+console.assert(parseHttpDate(rfc850) instanceof Date, 'rfc850 should return a Date')
+console.assert(parseHttpDate(imf) instanceof Date, 'imf should return a Date')
+
+const bench = new Bench({ name: 'parseHttpDate' })
 
 bench
   .add('asctime', () => {
