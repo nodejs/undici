9a40a2a651202da5bf71a9a413f25c0912c61e0d
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-22 23:03:47 +0200

    add test

---
+++ test/fetch/count-promises.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const assert = require('node:assert')
+const { once } = require('node:events')
+const { createServer } = require('node:http')
+const { test } = require('node:test')
+
+const countPromises = require('count-promises')
+const { fetch } = require('../..')
+
+const { closeServerAsPromise } = require('../utils/node-http')
+
+const expectedCount = 1334
+const body = 'abcdefg'.repeat(1e6)
+
+test(`fetch instantiates ${expectedCount} Promises`, { timeout: 180_000 }, async (t) => {
+  const server = createServer({ joinDuplicateHeaders: true }, (req, res) => {
+    res.writeHead(200)
+      .end(body)
+  })
+  t.after(closeServerAsPromise(server))
+
+  await once(server.listen(0), 'listening')
+
+  const url = new URL(`http://127.0.0.1:${server.address().port}`)
+
+  const getPromiseCount = countPromises({ locations: true, continuation: false })
+  const response = await fetch(url)
+
+  await response.text()
+
+  const promiseCount = getPromiseCount()
+
+  // console.log(promiseCount)
+
+  const actualCount = Object.entries(promiseCount).reduce((acc, [key, value]) => acc + value, 0)
+
+  assert.strictEqual(actualCount, expectedCount, `Expected ${expectedCount} Promises, got ${promiseCount}`)
+})
