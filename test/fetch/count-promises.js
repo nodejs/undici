bebc098174b57ce9034ea258052ea7e0d381d8ce
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-23 21:45:54 +0200

    keep track on created fetch promises

--- test/fetch/count-promises.js
+++ test/fetch/count-promises.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const assert = require('node:assert')
 const { once } = require('node:events')
 const { createServer } = require('node:http')
 const { test } = require('node:test')
@@ -9,9 +10,11 @@ const { fetch } = require('../..')
 
 const { closeServerAsPromise } = require('../utils/node-http')
 
+const expectedCount = 25
+
 const body = 'abcdefgh'
 
-test('fetch: Determine amount of created Promises', { timeout: 180_000 }, async (t) => {
+test(`fetch: should create ${expectedCount} Promises for fetch-call and using .text() on request`, { timeout: 180_000 }, async (t) => {
   const server = createServer({ joinDuplicateHeaders: true }, (req, res) => {
     res.writeHead(200)
       .end(body)
@@ -23,15 +26,15 @@ test('fetch: Determine amount of created Promises', { timeout: 180_000 }, async 
   const url = new URL(`http://127.0.0.1:${server.address().port}`)
 
   const getPromiseCount = countPromises({ locations: true, continuation: false })
-  const response = await fetch(url)
 
+  const response = await fetch(url)
   await response.text()
 
-  const promiseCount = getPromiseCount()
+  const promiseCount = Object.fromEntries(Object.entries(getPromiseCount()).filter(([path, value]) => path.includes('undici') && !path.includes('node:internal') && !path.includes('undici/test')))
 
   // console.log(promiseCount)
 
   const actualCount = Object.entries(promiseCount).reduce((acc, [key, value]) => acc + value, 0)
 
-  console.log(`Promise count: ${actualCount}`)
+  assert.strictEqual(actualCount, expectedCount, `Expected ${expectedCount} promises to be created, got ${actualCount}`)
 })
