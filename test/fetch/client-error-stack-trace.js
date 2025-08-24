4210b3114ae9061c35f1c9b8a3b4b78bf4a4af47
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-24 19:31:30 +0200

    fetch: remove promise in exported fetch (#4452)
    * fetch: remove promise in exported fetch
    
    * fix windows
    
    * Refactor fetch function parameters and error handling

--- test/fetch/client-error-stack-trace.js
+++ test/fetch/client-error-stack-trace.js
@@ -2,6 +2,7 @@
 
 const { test } = require('node:test')
 const assert = require('node:assert')
+const { sep } = require('node:path')
 const { fetch, setGlobalDispatcher, Agent } = require('../..')
 const { fetch: fetchIndex } = require('../../index-fetch')
 
@@ -14,7 +15,11 @@ test('FETCH: request errors and prints trimmed stack trace', async (t) => {
   try {
     await fetch('http://a.com')
   } catch (error) {
-    assert.ok(error.stack.includes(`at async TestContext.<anonymous> (${__filename}`))
+    const stackLines = error.stack.split('\n')
+    assert.ok(stackLines[0].includes('TypeError: fetch failed'))
+    assert.ok(stackLines[1].includes(`undici${sep}index.js`))
+    assert.ok(stackLines[2].includes('at process.processTicksAndRejections'))
+    assert.ok(stackLines[3].includes(`at async TestContext.<anonymous> (${__filename}`))
   }
 })
 
@@ -22,6 +27,10 @@ test('FETCH-index: request errors and prints trimmed stack trace', async (t) => 
   try {
     await fetchIndex('http://a.com')
   } catch (error) {
-    assert.ok(error.stack.includes(`at async TestContext.<anonymous> (${__filename}`))
+    const stackLines = error.stack.split('\n')
+    assert.ok(stackLines[0].includes('TypeError: fetch failed'))
+    assert.ok(stackLines[1].includes(`undici${sep}index-fetch.js`))
+    assert.ok(stackLines[2].includes('at process.processTicksAndRejections'))
+    assert.ok(stackLines[3].includes(`at async TestContext.<anonymous> (${__filename}`))
   }
 })
