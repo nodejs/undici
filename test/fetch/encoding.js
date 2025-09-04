940f7f218094eff22299cbe4edac84879358951b
Aras Abbasi <aras.abbasi@googlemail.com>
2025-09-04 22:12:34 +0200

    set keepalive to false

--- test/fetch/encoding.js
+++ test/fetch/encoding.js
@@ -51,6 +51,7 @@ describe('content-encoding handling', () => {
     const { strictEqual } = tspl(t, { plan: 3 })
 
     const response = await fetch(`http://localhost:${server.address().port}`, {
+      keepalive: false,
       headers: { 'accept-encoding': 'deflate, gzip' }
     })
 
@@ -65,6 +66,7 @@ describe('content-encoding handling', () => {
     const { strictEqual } = tspl(t, { plan: 3 })
 
     const response = await fetch(`http://localhost:${server.address().port}`, {
+      keepalive: false,
       headers: { 'accept-encoding': 'DeFlAtE, GzIp' }
     })
 
@@ -81,6 +83,7 @@ describe('content-encoding handling', () => {
       const { strictEqual } = tspl(t, { plan: 3 })
 
       const response = await fetch(`http://localhost:${server.address().port}`, {
+        keepalive: false,
         headers: { 'accept-encoding': 'zstd' }
       })
 
