9b279c9c2b063100c2400597dda6bb9bdbd38175
Aras Abbasi <aras.abbasi@googlemail.com>
2025-09-24 08:56:14 +0200

    chore: use testcontest for subresource integrity tests

--- test/subresource-integrity/case-sensitive-match.js
+++ test/subresource-integrity/case-sensitive-match.js
@@ -1,74 +1,73 @@
 'use strict'
 
 const { test, describe } = require('node:test')
-const assert = require('node:assert')
 
 const { caseSensitiveMatch } = require('../../lib/web/subresource-integrity/subresource-integrity')
 
 describe('caseSensitiveMatch', () => {
-  test('identical strings', () => {
+  test('identical strings', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('identical strings, actualValue has one padding char', () => {
+  test('identical strings, actualValue has one padding char', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('identical strings, expectedValue has one padding char', () => {
+  test('identical strings, expectedValue has one padding char', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('identical strings, expectedValue has two padding chars', () => {
+  test('identical strings, expectedValue has two padding chars', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=='
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('identical strings, both have one padding char', () => {
+  test('identical strings, both have one padding char', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs='
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('identical strings, both have two padding chars', () => {
+  test('identical strings, both have two padding chars', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=='
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=='
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('identical strings, expectedValue has invalid third padding char', () => {
+  test('identical strings, expectedValue has invalid third padding char', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=='
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs==='
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue) === false)
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue) === false)
   })
 
-  test('expectedValue can be base64Url - match `_`', () => {
+  test('expectedValue can be base64Url - match `_`', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7_gUfE5yuYB3ha/uSLs'
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('expectedValue can be base64Url - match `+`', () => {
+  test('expectedValue can be base64Url - match `+`', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7+gUfE5yuYB3ha/uSLs'
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7-gUfE5yuYB3ha/uSLs'
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 
-  test('should be case sensitive', () => {
+  test('should be case sensitive', (t) => {
     const actualValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs'
     const expectedValue = 'ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLS'
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue) === false)
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue) === false)
   })
 
-  test('empty string should return true', () => {
+  test('empty string should return true', (t) => {
     const actualValue = ''
     const expectedValue = ''
-    assert.ok(caseSensitiveMatch(actualValue, expectedValue))
+    t.assert.ok(caseSensitiveMatch(actualValue, expectedValue))
   })
 })
