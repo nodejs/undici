f82e476492053b25aa05202f398af20da693e9f8
Khafra <maitken033380023@gmail.com>
2025-10-18 18:39:40 -0400

    this is worse than tspl but ok

--- test/websocket/issue-4628.js
+++ test/websocket/issue-4628.js
@@ -1,31 +1,28 @@
 'use strict'
 
 const assert = require('node:assert')
-const { tspl } = require('@matteo.collina/tspl')
 const { test } = require('node:test')
 const { WebSocket } = require('../..')
 
-test('closing before connection is established should only fire error and close events once', async (t) => {
-  const plan = tspl(t, { plan: 2 })
+test('closing before connection is established should only fire error and close events once', (t) => {
+  t.plan(2)
+
+  t.after(() => assert.deepStrictEqual(events, ['error', 'close']))
 
   const events = []
   const ws = new WebSocket('wss://example.com/')
 
-  ws.onopen = plan.fail
+  ws.onopen = t.assert.fail
 
   ws.addEventListener('error', () => {
-    plan.ok(true, 'error event fired')
+    t.assert.ok(true, 'error event fired')
     events.push('error')
   })
 
   ws.addEventListener('close', () => {
-    plan.ok(true, 'close event fired')
+    t.assert.ok(true, 'close event fired')
     events.push('close')
   })
 
   ws.close()
-
-  await plan.completed
-
-  assert.deepStrictEqual(events, ['error', 'close'])
 })
