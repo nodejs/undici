67943801ed14bb4cc2346dc959a3037109b3f7b5
Khafra <maitken033380023@gmail.com>
2026-01-25 12:57:54 -0500

    return response when receiving 401 instead of network error
    fixes https://github.com/nodejs/undici/issues/4767

--- test/fetch/401-statuscode-no-infinite-loop.js
+++ test/fetch/401-statuscode-no-infinite-loop.js
@@ -9,18 +9,14 @@ const assert = require('node:assert')
 const { closeServerAsPromise } = require('../utils/node-http')
 
 test('Receiving a 401 status code should not cause infinite retry loop', async (t) => {
-  let requestCount = 0
-
   const server = createServer({ joinDuplicateHeaders: true }, (req, res) => {
-    requestCount++
-    console.log({ requestCount })
     res.statusCode = 401
-    res.setHeader('WWW-Authenticate', 'Basic realm="test"')
     res.end('Unauthorized')
   }).listen(0)
 
   t.after(closeServerAsPromise(server))
   await once(server, 'listening')
 
-  await assert.rejects(() => fetch(`http://localhost:${server.address().port}`))
+  const response = await fetch(`http://localhost:${server.address().port}`)
+  assert.strictEqual(response.status, 401)
 })
