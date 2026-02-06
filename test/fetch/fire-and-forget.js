47f9b9664759d18d30c3c8f321425bee102e1df3
tsctx <91457664+tsctx@users.noreply.github.com>
2026-02-06 21:33:46 +0900

    fix: set finalizer only for fetch responses (#4803)

--- test/fetch/fire-and-forget.js
+++ test/fetch/fire-and-forget.js
@@ -46,6 +46,42 @@ test('test finalizer cloned response', async () => {
   await response.arrayBuffer() // check consume body
 })
 
+// https://github.com/nodejs/undici/pull/4803
+test('should not call cancel() during GC (new Response)', async () => {
+  if (!hasGC) {
+    throw new Error('gc is not available. Run with \'--expose-gc\'.')
+  }
+
+  let response = new Response(new ReadableStream({
+    start () {},
+
+    pull (ctrl) {
+      ctrl.enqueue(new Uint8Array([72, 101, 108, 108, 111])) // Hello
+    },
+
+    cancel () {
+      throw new Error('should be unreachable')
+    }
+  }))
+
+  let cloned = response.clone()
+
+  const body = response.body
+
+  await nextTick()
+  cloned.body.cancel()
+
+  cloned = null
+  response = null
+
+  await nextTick()
+  // eslint-disable-next-line no-undef
+  gc()
+
+  await nextTick(); // handle 'uncaughtException' event
+  (function () {})(body) // save a reference without triggering linter warnings
+})
+
 test('does not need the body to be consumed to continue', { timeout: 180_000 }, async (t) => {
   if (!hasGC) {
     throw new Error('gc is not available. Run with \'--expose-gc\'.')
