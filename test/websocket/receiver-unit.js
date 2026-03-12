7df6442194b7a54e9ac734335e6e0a56a9bc6666
Matteo Collina <hello@matteocollina.com>
2026-03-12 15:24:05 +0100

    fix: adapt websocket frame-limit handling for v6 parser
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/websocket/receiver-unit.js
+++ test/websocket/receiver-unit.js
@@ -2,37 +2,39 @@
 
 const { test } = require('node:test')
 const { ByteParser } = require('../../lib/web/websocket/receiver')
-const { states } = require('../../lib/web/websocket/constants')
+const { kController, kResponse } = require('../../lib/web/websocket/symbols')
 
 const invalidFrame = Buffer.from([0x82, 0x7F, 0x00, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01])
 
 test('ByteParser rejects 64-bit payload lengths with a non-zero upper word', (t) => {
   const calls = {
     abort: 0,
-    close: 0
+    destroy: 0
   }
 
-  const handler = {
-    readyState: states.CONNECTING,
-    controller: {
-      abort: () => {
-        calls.abort += 1
+  const ws = new EventTarget()
+  ws[kController] = {
+    abort: () => {
+      calls.abort += 1
+    }
+  }
+  ws[kResponse] = {
+    socket: {
+      destroyed: false,
+      destroy: () => {
+        calls.destroy += 1
       }
-    },
-    onSocketClose: () => {
-      calls.close += 1
-    },
-    closeState: new Set()
+    }
   }
 
-  const parser = new ByteParser(handler)
+  const parser = new ByteParser(ws)
 
   parser.write(invalidFrame)
 
   return new Promise((resolve) => {
     setImmediate(() => {
       t.assert.strictEqual(calls.abort, 1)
-      t.assert.strictEqual(calls.close, 1)
+      t.assert.strictEqual(calls.destroy, 1)
       parser.destroy()
       resolve()
     })
