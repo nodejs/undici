61c74f61676e95efa185befdc3e8d93202be4792
Maruthan G <maruthang4@gmail.com>
2026-04-19 17:46:41 +0530

    types(dispatcher): use OutgoingHttpHeaders for request headers
    Closes #3840

--- test/types/header.test-d.ts
+++ test/types/header.test-d.ts
@@ -1,6 +1,6 @@
-import { IncomingHttpHeaders as CoreIncomingHttpHeaders } from 'node:http'
+import { IncomingHttpHeaders as CoreIncomingHttpHeaders, OutgoingHttpHeaders as CoreOutgoingHttpHeaders } from 'node:http'
 import { expectAssignable, expectNotAssignable } from 'tsd'
-import { IncomingHttpHeaders } from '../../types/header'
+import { IncomingHttpHeaders, OutgoingHttpHeaders } from '../../types/header'
 
 const headers = {
   authorization: undefined,
@@ -14,3 +14,23 @@ expectNotAssignable<CoreIncomingHttpHeaders>({
   authorization: null,
   'content-type': 'application/json'
 })
+
+// `OutgoingHttpHeaders` accepts numeric values (e.g. `content-length`)
+// in addition to strings, string arrays, and undefined values.
+const outgoingHeaders = {
+  'content-length': 42,
+  'content-type': 'application/json',
+  'set-cookie': ['a=1', 'b=2'],
+  authorization: undefined
+} satisfies CoreOutgoingHttpHeaders
+
+expectAssignable<OutgoingHttpHeaders>(outgoingHeaders)
+expectAssignable<OutgoingHttpHeaders>({ 'content-length': 42 })
+
+// `IncomingHttpHeaders` should still be assignable to `OutgoingHttpHeaders`,
+// since every incoming header value (string | string[] | undefined) is also a
+// valid outgoing header value.
+expectAssignable<OutgoingHttpHeaders>(headers)
+
+// Numeric header values are not assignable to `IncomingHttpHeaders`.
+expectNotAssignable<IncomingHttpHeaders>({ 'content-length': 42 })
