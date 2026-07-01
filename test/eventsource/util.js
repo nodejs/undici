b1f36c2e9f6172932e6714221f36a85cd1b984c6
Ram-blip <ramcruze2000@gmail.com>
2026-07-01 21:41:23 +0530

    fix(eventsource): set use-URL-credentials flag
    Signed-off-by: Ram-blip <ramcruze2000@gmail.com>

--- test/eventsource/util.js
+++ test/eventsource/util.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { test } = require('node:test')
-const { isASCIINumber, isValidLastEventId } = require('../../lib/web/eventsource/util')
+const { isASCIINumber, isValidLastEventId, createPotentialCORSRequest } = require('../../lib/web/eventsource/util')
 
 test('isValidLastEventId', (t) => {
   t.assert.strictEqual(isValidLastEventId('valid'), true)
@@ -14,4 +14,42 @@ test('isASCIINumber', (t) => {
   t.assert.strictEqual(isASCIINumber('123'), true)
   t.assert.strictEqual(isASCIINumber(''), false)
   t.assert.strictEqual(isASCIINumber('123a'), false)
+})
+
+test('createPotentialCORSRequest', async (t) => {
+  const url = new URL('https://example.com/events')
+
+  const cases = [
+    {
+      name: 'anonymous',
+      state: 'anonymous',
+      mode: 'cors',
+      credentials: 'same-origin'
+    },
+    {
+      name: 'use credentials',
+      state: 'use-credentials',
+      mode: 'cors',
+      credentials: 'include'
+    }
+  ]
+
+  for (const item of cases) {
+    await t.test(item.name, (t) => {
+      const request = createPotentialCORSRequest(
+        url,
+        '',
+        item.state,
+        false
+      )
+
+      t.assert.strictEqual(request.mode, item.mode)
+      t.assert.strictEqual(request.credentials, item.credentials)
+      t.assert.strictEqual(request.useURLCredentials, true)
+      t.assert.strictEqual(request.destination, '')
+      t.assert.strictEqual(request.urlList.length, 1)
+      t.assert.strictEqual(request.urlList[0], url)
+      t.assert.strictEqual(request.url, url)
+    })
+  }
 })
