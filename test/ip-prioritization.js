fe68b0efdd161b40b20ef06997116f38f1ff7713
Matteo Collina <hello@matteocollina.com>
2026-07-11 08:05:32 +0000

    fix(h1): ignore type of service errors

--- test/ip-prioritization.js
+++ test/ip-prioritization.js
@@ -1,12 +1,14 @@
 'use strict'
 
+const assert = require('node:assert')
 const { test, after } = require('node:test')
 const { Client } = require('..')
 const { createServer } = require('node:http')
 const { once } = require('node:events')
+const net = require('node:net')
 
-test('HTTP/1.1 Request Prioritization', async (t) => {
-  let priority = null
+test('HTTP/1.1 Request Prioritization', async () => {
+  const priorities = []
 
   const server = createServer((req, res) => {
     res.end('ok')
@@ -17,7 +19,7 @@ test('HTTP/1.1 Request Prioritization', async (t) => {
 
   const client = new Client(`http://localhost:${server.address().port}`, {
     connect: (opts, cb) => {
-      const socket = require('node:net').connect({
+      const socket = net.connect({
         ...opts,
         host: opts.hostname,
         port: opts.port
@@ -25,7 +27,7 @@ test('HTTP/1.1 Request Prioritization', async (t) => {
         cb(null, socket)
       })
       socket.setTypeOfService = (p) => {
-        priority = p
+        priorities.push(p)
       }
       return socket
     }
@@ -33,20 +35,104 @@ test('HTTP/1.1 Request Prioritization', async (t) => {
   after(() => client.close())
   after(() => server.close())
 
-  await client.request({
+  const response = await client.request({
+    path: '/',
+    method: 'GET',
+    typeOfService: 42
+  })
+  await response.body.text()
+
+  const response2 = await client.request({
+    path: '/',
+    method: 'GET'
+  })
+  await response2.body.text()
+
+  assert.deepStrictEqual(priorities, [42, 0])
+})
+
+// https://github.com/nodejs/undici/issues/5544
+// The default request path should not touch setsockopt on a fresh socket.
+test('HTTP/1.1 Request Prioritization skips default ToS on fresh socket', async () => {
+  let calls = 0
+
+  const server = createServer((req, res) => {
+    res.end('ok')
+  })
+
+  server.listen(0)
+  await once(server, 'listening')
+
+  const client = new Client(`http://localhost:${server.address().port}`, {
+    connect: (opts, cb) => {
+      const socket = net.connect({
+        ...opts,
+        host: opts.hostname,
+        port: opts.port
+      }, () => {
+        cb(null, socket)
+      })
+      socket.setTypeOfService = () => {
+        calls++
+        throw new Error('setTypeOfService EINVAL')
+      }
+      return socket
+    }
+  })
+  after(() => client.close())
+  after(() => server.close())
+
+  const response = await client.request({
+    path: '/',
+    method: 'GET'
+  })
+  await response.body.text()
+
+  assert.strictEqual(calls, 0)
+})
+
+// https://github.com/nodejs/undici/issues/5544
+// setTypeOfService() is best-effort and must not make the request fail.
+test('HTTP/1.1 Request Prioritization ignores setTypeOfService errors', async () => {
+  const priorities = []
+
+  const server = createServer((req, res) => {
+    res.end('ok')
+  })
+
+  server.listen(0)
+  await once(server, 'listening')
+
+  const client = new Client(`http://localhost:${server.address().port}`, {
+    connect: (opts, cb) => {
+      const socket = net.connect({
+        ...opts,
+        host: opts.hostname,
+        port: opts.port
+      }, () => {
+        cb(null, socket)
+      })
+      socket.setTypeOfService = (p) => {
+        priorities.push(p)
+        throw new Error('setTypeOfService EINVAL')
+      }
+      return socket
+    }
+  })
+  after(() => client.close())
+  after(() => server.close())
+
+  const response = await client.request({
     path: '/',
     method: 'GET',
     typeOfService: 42
   })
 
-  // Check if priority was set
-  if (priority !== 42) {
-    throw new Error(`Expected priority 42, got ${priority}`)
-  }
+  assert.strictEqual(await response.body.text(), 'ok')
+  assert.deepStrictEqual(priorities, [42])
 })
 
 test('HTTP/2 Connection Prioritization', async (t) => {
-  const net = require('node:net')
   const buildConnector = require('../lib/core/connect')
 
   let receivedHints = null
