ea92b41d30dd8c9a04d0b27312d93cb273d6ee0e
arshiya tabasum <arshi@bugqore.com>
2026-08-24 15:19:42 +0530

    fix(websocket): strip surrounding whitespace in parseExtensions
    Signed-off-by: arshiya tabasum <arshi@bugqore.com>

--- test/websocket/util.js
+++ test/websocket/util.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { describe, test } = require('node:test')
-const { isValidSubprotocol } = require('../../lib/web/websocket/util')
+const { isValidSubprotocol, parseExtensions } = require('../../lib/web/websocket/util')
 
 describe('isValidSubprotocol', () => {
   test('empty string returns false', t => {
@@ -25,6 +25,26 @@ describe('isValidSubprotocol', () => {
 
     for (let i = 0; i < chars.length; ++i) {
       t.assert.strictEqual(isValidSubprotocol('valid' + chars[i]), false)
+    }
+  })
+})
+
+describe('parseExtensions', () => {
+  test('keeps whitespace out of parameter names and values', t => {
+    // Optional whitespace is allowed around the ";" and "=" delimiters of an
+    // extension header, so it must be stripped from both ends of every name
+    // and value before the map is keyed on them.
+    const cases = {
+      'permessage-deflate; client_max_window_bits': [['permessage-deflate', ''], ['client_max_window_bits', '']],
+      'permessage-deflate ; client_max_window_bits': [['permessage-deflate', ''], ['client_max_window_bits', '']],
+      'permessage-deflate;\tclient_max_window_bits': [['permessage-deflate', ''], ['client_max_window_bits', '']],
+      'permessage-deflate; server_max_window_bits = 10': [['permessage-deflate', ''], ['server_max_window_bits', '10']],
+      'permessage-deflate; server_max_window_bits=10 ': [['permessage-deflate', ''], ['server_max_window_bits', '10']]
+    }
+
+    t.plan(Object.keys(cases).length)
+    for (const [header, expected] of Object.entries(cases)) {
+      t.assert.deepStrictEqual([...parseExtensions(header).entries()], expected, header)
     }
   })
 })
